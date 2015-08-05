5dca9318cc469cf6f6f7f0084a653947d9ba2dec
Mikeal Rogers <mikeal.rogers@gmail.com>
2015-08-05 09:34:15 -0700

    Localizable build system.

--- build.js
+++ build.js
@@ -2,7 +2,6 @@
 
 'use strict';
 
-const Path = require('path');
 const Metalsmith = require('metalsmith');
 const autoprefixer = require('autoprefixer-stylus');
 const collections = require('metalsmith-collections');
@@ -13,68 +12,65 @@ const prism = require('metalsmith-prism');
 const serve = require('metalsmith-serve');
 const stylus = require('metalsmith-stylus');
 const permalinks = require('metalsmith-permalinks');
+const path = require('path');
+const fs = require('fs');
 
 const debug = require('./plugins/debug.js');
 const filterStylusPartials = require('./plugins/filter-stylus-partials.js');
 const mapHandlebarsPartials = require('./plugins/map-handlebars-partials.js');
 
+/** Build **/
 
-/**
- * Build.
- */
-console.time('[metalsmith] Build finished');
-let metalsmith = Metalsmith(__dirname);
-metalsmith
-    .metadata({
-        // TODO: Import iojs meta data (check iojs templates for index/download)
-        site: {
-            title  : 'Node.js',
-            author : 'Node.js Foundation',
-            url    : 'https://new.nodejs.org'
-        }
-    })
+fs.readdir(path.join(__dirname, 'locale'), function (e, locales) {
+  locales.forEach(function (locale) {
+    console.time('[metalsmith] build/'+locale+' finished');
+    let metalsmith = Metalsmith(__dirname);
+    metalsmith
+    .metadata({site:require(path.join(__dirname, 'locale', locale, 'site.json'))})
+    .source(path.join(__dirname, 'locale', locale))
     .use(collections({
-        blog : {
-            pattern: 'blog/**/*.md',
-            sortBy: 'date',
-            reverse: true,
-            refer: false
-        }
+      blog : {
+        pattern: 'blog/**/*.md',
+        sortBy: 'date',
+        reverse: true,
+        refer: false
+      }
     }))
     .use(markdown({ langPrefix: 'language-' }))
     .use(prism())
     .use(filterStylusPartials())
     .use(stylus({
-        compress: true,
-        use: [autoprefixer()]
+      compress: true,
+      use: [autoprefixer()]
     }))
     .use(permalinks())
     .use(layouts({
-        engine: 'handlebars',
-        partials: mapHandlebarsPartials(metalsmith, 'layouts', 'partials'),
-        helpers: {
-            equals: function (v1, v2, options) {
-
-                return (v1 === v2) ? options.fn(this) : options.inverse(this);
-            }
+      engine: 'handlebars',
+      partials: mapHandlebarsPartials(metalsmith, 'layouts', 'partials'),
+      helpers: {
+        equals: function (v1, v2, options) {
+          return (v1 === v2) ? options.fn(this) : options.inverse(this);
         }
+      }
     }))
     .use(feed({
-        collection: 'blog',
-        destionation: 'blog.xml'
-    }));
+      collection: 'blog',
+      destionation: 'blog.xml'
+    }))
+    .destination(path.join(__dirname, 'build', locale));
 
+    metalsmith.build(function (err) {
+      if (err) { throw err; }
+      console.timeEnd('[metalsmith] build/'+locale+' finished');
+    });
+  })
+})
 
-if (process.argv[2] === 'serve') {
-    metalsmith = metalsmith
-        .use(serve({
-            port: 8080,
-            verbose: true
-        }));
-}
-
-metalsmith.build(function (err) {
-
-    if (err) { throw err; }
-    console.timeEnd('[metalsmith] Build finished');
-});
+// TODO replace with st and a filewatcher.
+// if (process.argv[2] === 'serve') {
+//     metalsmith = metalsmith
+//         .use(serve({
+//             port: 8080,
+//             verbose: true
+//         }));
+// }
