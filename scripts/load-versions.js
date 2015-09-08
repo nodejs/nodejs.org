53a0dd38768c0b57f5b4365726d07fad68e7e562
Rod Vagg <rod@vagg.org>
2015-09-08 22:01:58 +1000

    updates for v4.0 and minor tweaks

--- scripts/load-versions.js
+++ scripts/load-versions.js
@@ -3,44 +3,50 @@
 'use strict';
 
 const fs = require('fs');
-const semver = require('semver')
+const semver = require('semver');
+const map = require('map-async');
 const https = require('https');
+
+map([ 'https://nodejs.org/dist/index.json', 'https://iojs.org/dist/index.json' ], download, munge);
 
 function download (url, cb) {
     let data = '';
     https.get(url, function (res) {
         res.on('data', function (chunk) { data += chunk; });
-        res.on('end', function () { cb(null, data); });
+        res.on('end', function () {
+            try {
+                cb(null, JSON.parse(data));
+            } catch (e) {
+                return cb(e);
+            }
+        });
     }).on('error', function (e) {
         console.error('Error downloading file from %s: %s', url, e.message);
         cb(e);
     });
 }
 
-download('https://new.nodejs.org/dist/index.json', function (nodeErr, nodeVersions) {
-    download('https://iojs.org/dist/index.json', function (iojsErr, iojsVersions) {
+function munge (err, versions) {
+    if (err) {
+        console.error('Aborting due to download error from node or iojs');
+        console.error(err.stack)
+        return process.exit(1);
+    }
 
-        if (nodeErr || iojsErr) {
-            console.error('Aborting due to download error from node or iojs');
-            return process.exit(1);
-        }
+    versions[0].forEach(function (v) {
+        v.url = 'https://nodejs.org/dist/' + v.version + '/'
+        v.name = 'Node.js'
+    });
+    versions[1].forEach(function (v) {
+        v.url = 'https://iojs.org/dist/' + v.version + '/'
+        v.name = 'io.js'
+    });
 
-        let allVersions = [];
-        try {
-            nodeVersions = JSON.parse(nodeVersions);
-            iojsVersions = JSON.parse(iojsVersions);
+    let allVersions = versions[0].concat(versions[1]);
 
-            allVersions = nodeVersions.concat(iojsVersions);
-        } catch(e) {
-            console.error('Error parsing json', e);
-            return process.exit(1);
-        }
+    allVersions.sort(function (a, b) {
+        return semver.compare(b.version, a.version);
+    });
 
-        allVersions.sort(function (a, b) {
-            return semver.compare(b.version, a.version);
-        });
-
-        fs.writeFileSync('../source/versions.json', JSON.stringify(allVersions, null, ' '));
-    })
-})
-
+    fs.writeFileSync(__dirname + '/../source/versions.json', JSON.stringify(allVersions, null, 2));
+}
