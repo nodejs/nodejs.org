9096e0516417183da739452ac32551cdde57c014
Elin <elinnet@gmail.com>
2015-09-12 14:34:26 +0100

    broken summary of first para from blog

--- scripts/helpers/summary.js
+++ scripts/helpers/summary.js
@@ -1,5 +1,16 @@
 'use strict'
 
+const cheerio = require('cheerio');
+
 module.exports = function (contents) {
-    return contents;
+    var $ = cheerio.load('<div>'+contents+'</div>');
+
+    var summary = $(':nth-child(1)').html();
+    console.log(contents);
+
+    console.log("hello");
+
+    return summary;
 };
+
+// module.exports('<h2 class="title">Hello world</h2><h2>asdfasd</h2>');
