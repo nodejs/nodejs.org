44a3e359ada6163752ce83c56f1d364b2a9d00fd
Frederic Hemberger <mail@frederic-hemberger.de>
2015-09-18 20:17:28 +0200

    Download: Switch Windows link between x64/x86
    The homepage (/) points to the x64 installer, but the link on /downloads
    (the one above the table of platforms/architectures) points to the x86 installer.
    
    Closes https://github.com/nodejs/nodejs.org/issues/123

--- static/js/download.js
+++ static/js/download.js
@@ -4,24 +4,35 @@
   var os = n.platform.match(/(Win|Mac|Linux)/);
   var x = n.userAgent.match(/x86_64|Win64|WOW64/) ||
     n.cpuClass === 'x64' ? 'x64' : 'x86';
+  var text = 'textContent' in d ? 'textContent' : 'innerText';
   var db = d.getElementById('home-downloadbutton');
-  if (!db) { return; }
-  var version = db.dataset.version;
-  var dlLocal = db.dataset.dlLocal;
-  var text = 'textContent' in db ? 'textContent' : 'innerText';
-  switch (os && os[1]) {
-    case 'Mac':
-      db.href += 'node-' + version + '.pkg';
-      db[text] = dlLocal + ' OS X (x64)';
-      break;
-    case 'Win':
-      // Windows 64-bit files for 0.x.x need to be prefixed with 'x64/'
-      db.href += (version[1] == '0' && x == 'x64' ? x + '/' : '') + 'node-' + version + '-' + x + '.msi';
-      db[text] = dlLocal + ' Windows (' + x +')';
-      break;
-    case 'Linux':
-      db.href += 'node-' + version + '-linux-' + x + '.tar.gz';
-      db[text] = dlLocal + ' Linux (' + x + ')';
-      break;
+  var version;
+  if (db) {
+    version = db.dataset.version;
+    var dlLocal = db.dataset.dlLocal;
+    switch (os && os[1]) {
+      case 'Mac':
+        db.href += 'node-' + version + '.pkg';
+        db[text] = dlLocal + ' OS X (x64)';
+        break;
+      case 'Win':
+        // Windows 64-bit files for 0.x.x need to be prefixed with 'x64/'
+        db.href += (version[1] == '0' && x == 'x64' ? x + '/' : '') + 'node-' + version + '-' + x + '.msi';
+        db[text] = dlLocal + ' Windows (' + x +')';
+        break;
+      case 'Linux':
+        db.href += 'node-' + version + '-linux-' + x + '.tar.gz';
+        db[text] = dlLocal + ' Linux (' + x + ')';
+        break;
+    }
+  }
+
+  // Windows button on download page
+  var winButton = d.getElementById('windows-downloadbutton');
+  if (winButton && os && os[1] === 'Win') {
+    var winText = winButton.getElementsByTagName('p')[0];
+    version = winButton.dataset.version;
+    winButton.href = winButton.href.replace(/x(86|64)/, x);
+    winText[text] = winText[text].replace(/x(86|64)/, x);
   }
 })(document, navigator);
