590f3126e0c4f7ca4e806c6f62ea433681e97bbe
Phillip Johnsen <johphi@gmail.com>
2015-11-18 22:40:02 +0100

    Fix lint issue in changeloglink.js.
    Introduced in 2252323e4.

--- scripts/helpers/changeloglink.js
+++ scripts/helpers/changeloglink.js
@@ -5,8 +5,9 @@ const semver = require('semver')
 module.exports = function (version) {
   if (!version) { return '' }
 
-  if (semver.satisfies(version, '>= 1.0.0'))
+  if (semver.satisfies(version, '>= 1.0.0')) {
     return `https://github.com/nodejs/node/blob/${version}/CHANGELOG.md`
+  }
 
   return `https://github.com/nodejs/node-v0.x-archive/blob/${version}/ChangeLog`
 }
