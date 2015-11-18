2252323e4adaa5f341c42e123d5fd6f8c71eabfa
thefourtheye <thefourtheye@users.noreply.github.com>
2015-11-18 22:53:58 +0530

    simplify the link selection logic

--- scripts/helpers/changeloglink.js
+++ scripts/helpers/changeloglink.js
@@ -5,11 +5,8 @@ const semver = require('semver')
 module.exports = function (version) {
   if (!version) { return '' }
 
-  const legacyLink = semver.satisfies(version, '>=1.0.0 <4.0.0')
-    ? `https://github.com/nodejs/node/blob/${version}/CHANGELOG.md`
-    : `https://github.com/nodejs/node-v0.x-archive/blob/${version}/ChangeLog`
+  if (semver.satisfies(version, '>= 1.0.0'))
+    return `https://github.com/nodejs/node/blob/${version}/CHANGELOG.md`
 
-  return semver.gte(version, '4.0.0')
-    ? `https://github.com/nodejs/node/blob/${version}/CHANGELOG.md`
-    : legacyLink
+  return `https://github.com/nodejs/node-v0.x-archive/blob/${version}/ChangeLog`
 }
