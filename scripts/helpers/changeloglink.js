9d3a781ec3fe05f9c7e5b9f03d14719d461f3fda
Jeremiah Senkpiel <fishrock123@rocketmail.com>
2016-02-03 12:47:46 -0500

    fix changelog links for new 0.12 and 0.10 releases
    Fixes: https://github.com/nodejs/nodejs.org/issues/504
    PR-URL: https://github.com/nodejs/nodejs.org/pull/505

--- scripts/helpers/changeloglink.js
+++ scripts/helpers/changeloglink.js
@@ -9,5 +9,10 @@ module.exports = function (version) {
     return `https://github.com/nodejs/node/blob/${version}/CHANGELOG.md`
   }
 
+  // 0.12.8+ and 0.10.41+ releases come from the new repo
+  if (semver.satisfies(version, '~0.12.8 || ~0.10.41')) {
+    return `https://github.com/nodejs/node/blob/${version}/ChangeLog`
+  }
+
   return `https://github.com/nodejs/node-v0.x-archive/blob/${version}/ChangeLog`
 }
