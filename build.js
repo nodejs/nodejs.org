e552ce42ede5fe45d8cbaefaa9eb24359e0998a8
Michael Dawson <michael_dawson@ca.ibm.com>
2017-12-04 14:28:28 -0500

    sec: upcoming security announcement
    PR-URL: https://github.com/nodejs/nodejs.org/pull/1496
    Reviewed-By: Myles Borins <myles.borins@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Evan Lucas <evanlucas@me.com>

--- build.js
+++ build.js
@@ -270,7 +270,7 @@ function getSource (callback) {
         },
         banner: {
           visible: true,
-          content: 'Important <a href="https://nodejs.org/en/blog/vulnerability/oct-2017-dos/">security releases</a>, please update now!'
+          content: 'Important <a href="https://nodejs.org/en/blog/vulnerability/december-2017-security-releases/">upcoming security releases</a>, please review'
         }
       }
     }
