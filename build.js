577d3f94361cb7bef4839f5621db4f8f6fad8105
Michael Dawson <michael_dawson@ca.ibm.com>
2018-06-05 23:25:28 -0400

    doc: june 2018 sec pre-announce
    Add blog for June 2018 pre-announce
    Update header to point to pre-announce

--- build.js
+++ build.js
@@ -254,9 +254,9 @@ function getSource (callback) {
           lts: latestVersion.lts(versions)
         },
         banner: {
-          visible: false,
-          text: 'Important March 2018 security upgrades now available',
-          link: '/en/blog/vulnerability/march-2018-security-releases/'
+          visible: true,
+          text: 'June 2018 Security Releases',
+          link: '/en/blog/vulnerability/june-2018-security-releases/'
         }
       }
     }
