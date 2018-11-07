dbb6080d76d0faf8c8f42cb13e492b291ebb2461
Frederic Hemberger <mail@frederic-hemberger.de>
2018-11-07 09:17:47 +0100

    Add Release Schedule
    Pulls data from https://github.com/nodejs/Release/pull/380
    
    Fixes: #1889

---
+++ locale/en/about/release-schedule.md
@@ -0,0 +1,18 @@
+---
+layout: about-release-schedule.hbs
+title: Release Schedule
+statuses:
+  maintenance: 'Maintenance LTS'
+  active: 'Active LTS'
+  current: 'Current'
+  pending: 'Pending'
+columns:
+  - 'Release'
+  - 'Status'
+  - 'Codename'
+  - 'Initial Release'
+  - 'Active LTS Start'
+  - 'Maintenance LTS Start'
+  - 'End-of-life'
+schedule-footer: Dates are subject to change.
+---
---
+++ scripts/helpers/release-schedule.js
@@ -0,0 +1,62 @@
+'use strict'
+
+const schedule = require('../../source/schedule.json')
+
+const today = new Date()
+const datify = (release, key) => new Date(schedule[release][key])
+
+module.exports = context => {
+  const statuses = context.data.root.statuses
+
+  const header = context.data.root.columns
+    .map(column => `<th>${column}</th>\n`)
+    .join('')
+
+  let content = ''
+
+  Object.keys(schedule)
+    .filter(release => datify(release, 'end') > today)
+    .forEach(release => {
+      const codename = schedule[release].codename
+      const codenameLink = codename
+        ? `<a href="https://nodejs.org/download/release/latest-${codename.toLowerCase()}/">${codename}</a>`
+        : ''
+
+      const releaseLink = datify(release, 'start') < today
+        ? `<a href="https://nodejs.org/download/release/latest-${release}.x/">${release}</a>`
+        : release
+
+      let status
+
+      if (datify(release, 'start') > today) {
+        status = statuses.pending
+      } else if (!schedule[release].lts || datify(release, 'lts') > today) {
+        status = statuses.current
+      } else if (datify(release, 'maintenance') < today) {
+        status = statuses.maintenance
+      } else if (datify(release, 'lts') < today) {
+        status = statuses.active
+      }
+
+      content += `<tr>
+        <td>${releaseLink}</td>
+        <td>${status}</td>
+        <td>${codenameLink}</td>
+        <td>${schedule[release].start || ''}</td>
+        <td>${schedule[release].lts || ''}</td>
+        <td>${schedule[release].maintenance || ''}</td>
+        <td>${schedule[release].end || ''}</td>
+      </tr>`
+    })
+
+  return `
+    <table class="release-schedule">
+      <thead>
+        <tr>${header}</tr>
+      </thead>
+      <tbody>
+        ${content}
+      </tbody>
+    </table>
+  `
+}
