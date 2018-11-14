064220a281a0cd109e7f86d71e7b7a50779d2a2f
Tobias Bernting <tobiasbernting@gmail.com>
2018-11-14 20:54:48 +0100

    test ci

--- tests/scripts/anchor-mardown-headings.test.js
+++ tests/scripts/anchor-mardown-headings.test.js
@@ -6,7 +6,7 @@ test('anchorMarkdownHeadings', (t) => {
   const anchorMarkdownHeadings = require('../../scripts/plugins/anchor-markdown-headings')
 
   t.plan(4)
-  t.test('correctly pharses markdown heading without links', (t) => {
+  t.test('Correctly pharses markdown heading without links', (t) => {
     const text = 'Simple title'
     const level = 1
     const raw = 'Simple title'
@@ -19,7 +19,7 @@ test('anchorMarkdownHeadings', (t) => {
     t.equal(output, expected)
   })
 
-  t.test('correctly pharses markdown heading with a single link', (t) => {
+  t.test('Correctly pharses markdown heading with a single link', (t) => {
     const text = 'Title with <a href="#">link</a>'
     const level = 3
     const raw = 'Title with [link](#)'
@@ -33,7 +33,7 @@ test('anchorMarkdownHeadings', (t) => {
     t.equal(output, expected)
   })
 
-  t.test('correctly pharses markdown heading with multiple links', (t) => {
+  t.test('Correctly pharses markdown heading with multiple links', (t) => {
     const text = 'a <a href="b">b</a> c<a href="d">d</a>e'
     const level = 2
     const raw = 'a [b](b) c[d](d)e'
@@ -47,7 +47,7 @@ test('anchorMarkdownHeadings', (t) => {
     t.equal(output, expected)
   })
 
-  t.test('makes pretty slugs', (t) => {
+  t.test('Makes pretty slugs', (t) => {
     const text = '$$$ WIN BIG! $$$'
     const level = 4
     const raw = '$$$ WIN BIG! $$$'
