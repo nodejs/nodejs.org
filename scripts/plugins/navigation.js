e3d3c232e59445285403ae01c85b00f62f7f798c
MaleDong <maledong_github@outlook.com>
2019-04-19 11:22:03 +0800

    chore: update functions to ES6
    In 'navigation.js', there're some functions in the old style, union them
    by re-writing them into ES6.

--- scripts/plugins/navigation.js
+++ scripts/plugins/navigation.js
@@ -3,7 +3,7 @@
 // Extracts the main menu and sub-menu links form locale's site.json and
 // adds them to the metadata. This data is used in the navigation template
 module.exports = function buildNavigation (latestVersions) {
-  return function (files, metalsmith, done) {
+  return (files, metalsmith, done) => {
     const meta = metalsmith.metadata()
     meta.nav = {}
     generateNavigation(meta.site, 'main')
@@ -14,7 +14,7 @@ module.exports = function buildNavigation (latestVersions) {
       if (obj.link && obj.text) {
         parent.push(obj)
       }
-      Object.keys(obj).forEach(function (key) {
+      Object.keys(obj).forEach((key) => {
         if (obj[key].link && obj[key].text) {
           // Insert latest versions for API docs
           if (key === 'api-current') {
