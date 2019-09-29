eca9e98a485a358bbcc0a4cf6bd636306bf97995
XhmikosR <xhmikosr@gmail.com>
2019-09-29 09:21:42 +0300

    Move `githubLinks()` to scripts/plugins/githubLinks.js

--- build.js
+++ build.js
@@ -23,6 +23,7 @@ const stylus = require('stylus')
 const ncp = require('ncp')
 const junk = require('junk')
 
+const githubLinks = require('./scripts/plugins/githubLinks')
 const navigation = require('./scripts/plugins/navigation')
 const anchorMarkdownHeadings = require('./scripts/plugins/anchor-markdown-headings')
 const loadVersions = require('./scripts/load-versions')
@@ -206,33 +207,6 @@ function withPreserveLocale (preserveLocale) {
     } else {
       next()
     }
-  }
-}
-
-// This middleware adds "Edit on GitHub" links to every editable page
-function githubLinks (options) {
-  return (files, m, next) => {
-    // add suffix (".html" or "/" or "\" for windows) to each part of regex
-    // to ignore possible occurrences in titles (e.g. blog posts)
-    const isEditable = /security\.html|about(\/|\\)|docs(\/|\\)|foundation(\/|\\)|get-involved(\/|\\)|knowledge(\/|\\)/
-
-    Object.keys(files).forEach((path) => {
-      if (!isEditable.test(path)) {
-        return
-      }
-
-      const file = files[path]
-      const url = `https://github.com/nodejs/nodejs.org/edit/master/locale/${options.locale}/${path.replace('.html', '.md').replace(/\\/g, '/')}`
-      const editText = options.site.editOnGithub || 'Edit on GitHub'
-
-      const contents = file.contents.toString().replace(/<h1(.*?)>(.*?)<\/h1>/, (match, $1, $2) => {
-        return `<a class="edit-link" href="${url}">${editText}</a> <h1${$1}>${$2}</h1>`
-      })
-
-      file.contents = Buffer.from(contents)
-    })
-
-    next()
   }
 }
 
