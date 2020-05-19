c8c68e235b2477725dbeee3643b738d3364632e8
José J. Pérez Rivas <me@josejpr.com>
2020-05-19 12:46:00 +0200

    Include proctect file into test/scripts and resolver bug with relative path

--- tests/scripts/proctect-i18n.js
+++ tests/scripts/proctect-i18n.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 
 /**
- * What is this? With this small JavaScript file we want 
- * to block the upload of modified files in the "locale" 
- * folder to the repository through a git hook generated 
+ * What is this? With this small JavaScript file we want
+ * to block the upload of modified files in the "locale"
+ * folder to the repository through a git hook generated
  * with the Husky library.
- * 
+ *
  * Usage: $ node .git-hook/locale.js
- * 
+ *
  * Happy releasing!
  */
 
@@ -27,30 +27,30 @@ const arrayResults = results.toString().trim().split('\n')
 // Define array files to not change.
 const changedLocalizedFiles = []
 
-function gitHook(expresion) {
-
+function gitHook () {
   // Define RegExp with reg param.
-  const reg = new RegExp(expresion, 'g')
+  const reg = new RegExp('/locale/', 'g')
 
   // Iterate in array for search if RegExp is true or not.
   arrayResults.forEach(file => {
     // Control for search if exist or not file.
-    const filepath = path.join(__dirname, '..', file);
+    const filepath = path.join(__dirname, '../..', file)
+    console.log(filepath)
     if (fs.existsSync(filepath) && reg.test(filepath)) {
       // Include path reference into array for resolve result console.
-      filesNoChange.push({
+      changedLocalizedFiles.push({
         Path: filepath
       })
     }
   })
 
   // Show an error and log table with the list of modified files in the "locale" folder.
-  if (filesNoChange.length !== 0) {
+  if (changedLocalizedFiles.length !== 0) {
     console.error('\x1b[31m', '\n ❌ You can not commit changes of the following files:', '\x1b[0m')
-    console.table(filesNoChange)
+    console.table(changedLocalizedFiles)
     process.exit(1)
   }
 }
 
 // Launch for locale folder.
-gitHook('/locale/')
+gitHook()
