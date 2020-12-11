23119cb524de3be88c4245f4c31bf86348b4010f
Michael Dawson <michael_dawson@ca.ibm.com>
2020-12-11 11:12:46 -0800

    doc: Jan security release pre-announce (#3572)
    * doc: Jan security release pre-announce
    
    Signed-off-by: Michael Dawson <mdawson@devrus.com>

--- build.js
+++ build.js
@@ -278,8 +278,8 @@ function getSource (callback) {
         },
         banner: {
           visible: true,
-          text: 'Node.js Certifications and Trainings are on sale, now until Dec. 8',
-          link: 'https://nodejs.medium.com/cyber-monday-sale-save-up-to-65-on-node-js-certifications-and-training-810a20bad64e'
+          text: 'New security releases to be made available January 4, 2021',
+          link: '/en/blog/vulnerability/january-2021-security-releases/'
         }
       }
     }
