c53098cdd2085eea0d61911ecf4ca20b49dd50bb
thomas <moyelysian@icloud.com>
2021-11-14 13:44:53 +0100

    static/js/main.js Deprecated OS detection

--- static/js/main.js
+++ static/js/main.js
@@ -242,12 +242,11 @@
 })()
 ;(function () {
   'use strict'
-
-  var osMatch = navigator.platform.match(/(Win|Mac|Linux)/)
+  var userAgent = navigator.userAgent
+  var osMatch = userAgent.match(/(Win|Mac|Linux)/)
   var os = (osMatch && osMatch[1]) || ''
   var arch =
-    navigator.userAgent.match(/x86_64|Win64|WOW64/) ||
-    navigator.cpuClass === 'x64'
+    userAgent.match(/x86_64|Win64|WOW64/) || navigator.cpuClass === 'x64'
       ? 'x64'
       : 'x86'
   var buttons = document.querySelectorAll('.home-downloadbutton')
