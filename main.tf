a087a3f835e6d7d751407fb8399b9952cc5a5550
ErçetinGüler <93947784+biyonik-ci@users.noreply.github.com>
2021-12-10 13:43:16 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,5 @@
+  terraform plan
+  shell: /usr/bin/bash --noprofile --norc -e -o pipefail {0}
+  env:
+    TERRAFORM_CLI_PATH: /home/runner/work/_temp/8d90b57e-18ce-4240-b384-4f8c9a566d26
+/home/runner/work/_temp/8d90b57e-18ce-4240-b384-4f8c9a566d26/terraform-bin plan
