6880d0f1c0785bfe9f221fce0b959d48b72ee184
Claudio Wunder <cwunder@hubspot.com>
2023-01-09 17:54:31 +0100

    chore(header): added i18n config

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,8 @@ import Head from 'next/head';
 import { Source_Sans_Pro } from '@next/font/google';
 
 import { useSiteConfig } from '../hooks/useSiteConfig';
-import { LegacyFrontMatter } from '../types';
+import { useLocale } from '../hooks/useLocale';
+import type { LegacyFrontMatter } from '../types';
 
 const sourceSansPro = Source_Sans_Pro({
   weight: ['400', '600'],
@@ -11,8 +12,12 @@ const sourceSansPro = Source_Sans_Pro({
 
 type HeaderProps = { frontMatter: LegacyFrontMatter };
 
+// @TODO: Generate RSS feeds and include them here
 const Header = ({ frontMatter }: HeaderProps) => {
   const siteConfig = useSiteConfig();
+  const { currentLocale, getLocaleConfig } = useLocale();
+
+  const currentLocaleConfig = getLocaleConfig(currentLocale);
 
   const pageTitle = frontMatter.title || siteConfig.title;
 
@@ -41,6 +46,8 @@ const Header = ({ frontMatter }: HeaderProps) => {
       <link rel="manifest" href="/static/manifest.json" />
 
       <link rel="icon" href={siteConfig.favicon} type="image/png" />
+
+      <html lang={currentLocaleConfig.code} dir={currentLocaleConfig.langDir} />
 
       <meta name="robots" content={frontMatter.robots || 'index, follow'} />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
