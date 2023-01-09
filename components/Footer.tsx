1c957b872d86ec9825d59631ec8e9f51127bc8c9
Claudio Wunder <cwunder@hubspot.com>
2023-01-09 17:57:29 +0100

    feat(footer): added scroll-to-top

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,40 +1,49 @@
+import { FormattedMessage } from 'react-intl';
+
 type FooterProps = { className?: string };
 
 // Note.: We don't expect to translate these items as we're going to replace with `nodejs/nodejs.dev` footer
 const Footer = ({ className }: FooterProps) => (
-  <footer className={className}>
-    <div className="container">
-      <div className="openjsfoundation-footer">
-        <p>
-          Copyright <a href="https://openjsf.org">OpenJS Foundation</a> and
-          Node.js contributors. All rights reserved. The{' '}
-          <a href="https://openjsf.org">OpenJS Foundation</a> has registered
-          trademarks and uses trademarks. For a list of trademarks of the{' '}
-          <a href="https://openjsf.org">OpenJS Foundation</a>, please see our{' '}
-          <a href="https://trademark-policy.openjsf.org">Trademark Policy</a>{' '}
-          and <a href="https://trademark-list.openjsf.org">Trademark List</a>.
-          Trademarks and logos not indicated on the{' '}
-          <a href="https://trademark-list.openjsf.org">
-            list of OpenJS Foundation trademarks
-          </a>{' '}
-          are trademarks&trade; or registered&reg; trademarks of their
-          respective holders. Use of them does not imply any affiliation with or
-          endorsement by them.
-        </p>
-        <p className="openjsfoundation-footer-links">
-          <a href="https://openjsf.org">The OpenJS Foundation</a> |
-          <a href="https://terms-of-use.openjsf.org">Terms of Use</a> |
-          <a href="https://privacy-policy.openjsf.org">Privacy Policy</a> |
-          <a href="https://bylaws.openjsf.org">Bylaws</a> |
-          <a href="https://code-of-conduct.openjsf.org">Code of Conduct</a> |
-          <a href="https://trademark-policy.openjsf.org">Trademark Policy</a> |
-          <a href="https://trademark-list.openjsf.org">Trademark List</a> |
-          <a href="https://www.linuxfoundation.org/cookies">Cookie Policy</a>
-        </p>
-        <div className="openjsfoundation-footer-edit"></div>
+  <>
+    <a href="#" id="scroll-to-top">
+      <span>
+        &uarr; <FormattedMessage id="site.scrollToTop" />
+      </span>
+    </a>
+    <footer className={className}>
+      <div className="container">
+        <div className="openjsfoundation-footer">
+          <p>
+            Copyright <a href="https://openjsf.org">OpenJS Foundation</a> and
+            Node.js contributors. All rights reserved. The{' '}
+            <a href="https://openjsf.org">OpenJS Foundation</a> has registered
+            trademarks and uses trademarks. For a list of trademarks of the{' '}
+            <a href="https://openjsf.org">OpenJS Foundation</a>, please see our{' '}
+            <a href="https://trademark-policy.openjsf.org">Trademark Policy</a>{' '}
+            and <a href="https://trademark-list.openjsf.org">Trademark List</a>.
+            Trademarks and logos not indicated on the{' '}
+            <a href="https://trademark-list.openjsf.org">
+              list of OpenJS Foundation trademarks
+            </a>{' '}
+            are trademarks&trade; or registered&reg; trademarks of their
+            respective holders. Use of them does not imply any affiliation with
+            or endorsement by them.
+          </p>
+          <p className="openjsfoundation-footer-links">
+            <a href="https://openjsf.org">The OpenJS Foundation</a> |
+            <a href="https://terms-of-use.openjsf.org">Terms of Use</a> |
+            <a href="https://privacy-policy.openjsf.org">Privacy Policy</a> |
+            <a href="https://bylaws.openjsf.org">Bylaws</a> |
+            <a href="https://code-of-conduct.openjsf.org">Code of Conduct</a> |
+            <a href="https://trademark-policy.openjsf.org">Trademark Policy</a>{' '}
+            |<a href="https://trademark-list.openjsf.org">Trademark List</a> |
+            <a href="https://www.linuxfoundation.org/cookies">Cookie Policy</a>
+          </p>
+          <div className="openjsfoundation-footer-edit"></div>
+        </div>
       </div>
-    </div>
-  </footer>
+    </footer>
+  </>
 );
 
 export default Footer;
