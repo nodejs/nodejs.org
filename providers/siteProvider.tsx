03e4c834e7170cf8da80e05502c50f0c90c82609
Claudio Wunder <cwunder@hubspot.com>
2023-01-09 17:55:19 +0100

    refactor(provider): cleaned-up the site provider

--- providers/siteProvider.tsx
+++ providers/siteProvider.tsx
@@ -1,13 +1,11 @@
 import { createContext } from 'react';
-import type { FC, PropsWithChildren } from 'react';
+import type { PropsWithChildren } from 'react';
 import type { SiteConfig } from '../types';
 
 import siteConfig from '../site.json';
 
 export const SiteContext = createContext<SiteConfig>(siteConfig);
 
-export const SiteProvider: FC<PropsWithChildren> = props => (
-  <SiteContext.Provider value={siteConfig}>
-    {props.children}
-  </SiteContext.Provider>
+export const SiteProvider = ({ children }: PropsWithChildren) => (
+  <SiteContext.Provider value={siteConfig}>{children}</SiteContext.Provider>
 );
