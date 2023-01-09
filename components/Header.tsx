00d07c675f90e3876574e779da48cc3b7cc28a50
Claudio Wunder <cwunder@hubspot.com>
2023-01-09 16:22:33 +0100

    feat(header): added all header meta tags

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,22 +2,52 @@ import Head from 'next/head';
 import { Source_Sans_Pro } from '@next/font/google';
 
 import { useSiteConfig } from '../hooks/useSiteConfig';
+import { LegacyFrontMatter } from '../types';
 
 const sourceSansPro = Source_Sans_Pro({
   weight: ['400', '600'],
   display: 'fallback',
 });
 
-const Header = () => {
+interface HeaderProps {
+  frontMatter: LegacyFrontMatter;
+}
+
+const Header = ({ frontMatter }: HeaderProps) => {
   const siteConfig = useSiteConfig();
+
+  const pageTitle = frontMatter.title || siteConfig.title;
 
   return (
     <Head>
-      <style jsx global>{`
-        font: 400 20px/1.5 ${sourceSansPro.style.fontFamily}, 'Open Sans',
-          Roboto, 'San Francisco', Helvetica, Arial, sans-serif;
-      `}</style>
+      <style jsx global>
+        {`
+          body {
+            font: 400 20px/1.5 ${sourceSansPro.style.fontFamily}, 'Open Sans',
+              Roboto, 'San Francisco', Helvetica, Arial, sans-serif;
+          }
+        `}
+      </style>
+
       <title>{siteConfig.title}</title>
+
+      <meta name="robots" content={frontMatter.robots || 'index, follow'} />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="description" content={siteConfig.description} />
+
+      <meta property="og:title" content={pageTitle} />
+      <meta property="og:site_name" content={siteConfig.title} />
+      <meta property="og:description" content={siteConfig.description} />
+      <meta property="og:image" content={siteConfig.featuredImage} />
+      <meta property="og:image:type" content={siteConfig.og.imgType} />
+      <meta property="og:image:width" content={siteConfig.og.imgWidth} />
+      <meta property="og:image:height" content={siteConfig.og.imgHeight} />
+
+      <meta name="twitter:card" content={siteConfig.twitter.card} />
+      <meta name="twitter:site" content={siteConfig.twitter.username} />
+      <meta name="twitter:title" content={pageTitle} />
+      <meta name="twitter:image" content={siteConfig.twitter.img} />
+      <meta name="twitter:image:alt" content={siteConfig.twitter.imgAlt} />
     </Head>
   );
 };
