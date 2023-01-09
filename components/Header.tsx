2a4d8831cd8afaf6574f612a723edcf1bf560bb6
Claudio Wunder <cwunder@hubspot.com>
2023-01-09 15:51:25 +0100

    chore(fonts): added source sans pro font

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,22 @@
 import Head from 'next/head';
+import { Source_Sans_Pro } from '@next/font/google';
+
 import { useSiteConfig } from '../hooks/useSiteConfig';
+
+const sourceSansPro = Source_Sans_Pro({
+  weight: ['400', '600'],
+  display: 'fallback',
+});
 
 const Header = () => {
   const siteConfig = useSiteConfig();
 
   return (
     <Head>
+      <style jsx global>{`
+        font: 400 20px/1.5 ${sourceSansPro.style.fontFamily}, 'Open Sans',
+          Roboto, 'San Francisco', Helvetica, Arial, sans-serif;
+      `}</style>
       <title>{siteConfig.title}</title>
     </Head>
   );
