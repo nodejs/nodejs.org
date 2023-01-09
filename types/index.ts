c7d8eadc44001e022df3a24e83a1072a3065e7c7
Claudio Wunder <cwunder@hubspot.com>
2023-01-09 15:52:16 +0100

    feat(types): introduced frontmatter types

--- types/index.ts
+++ types/index.ts
@@ -1,3 +1,5 @@
+// @TODO: These types will be splitted on individual files for better organisation in the future
+
 export interface RSSFeed {
   link: string;
   title: string;
@@ -29,4 +31,34 @@ export interface SiteConfig {
   twitterImgAlt: string;
   rssFeeds: Array<RSSFeed>;
   websiteBanners: Record<string, WebsiteBanner>;
+}
+
+// @TODO: These are legacy layouts that are going to be replaced with the `nodejs/nodejs.dev` Layouts in the future
+export type LegacyLayouts =
+  | 'about.hbs'
+  | 'blog-index.hbs'
+  | 'blog-post.hbs'
+  | 'category-index.hbs'
+  | 'contribute.hbs'
+  | 'index.hbs'
+  | 'docs.hbs'
+  | 'download.hbs'
+  | 'download-current.hbs'
+  | 'download-releases.hbs'
+  | 'page.hbs';
+
+// @TODO: This is the legacy frontmatter configuration going to be replaced in the future with the `ndoejs/nodejs.dev` one
+// this is going to be done via a script that replaces layouts
+// Note.: The current legacy pages have other frontmatter entries but they're irrelevant
+export interface LegacyFrontmatter {
+  title: string;
+  layout: LegacyLayouts;
+  robots?: string;
+}
+
+// @TODO: This is the legacy frontmatter configuration going to be replaced in the future with the `ndoejs/nodejs.dev` one
+// this is going to be done via a script that replaces layouts
+export interface LegacyBlogFrontmatter extends LegacyFrontmatter {
+  author: string;
+  date: string;
 }
