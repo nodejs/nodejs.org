bc270b04af10cfde3ad7342bbf03b782dc341022
Claudio Wunder <cwunder@hubspot.com>
2023-01-10 12:20:44 +0100

    feat(header): renamed header into htmlhead

---
+++ components/HtmlHead.tsx
@@ -0,0 +1,50 @@
+import Head from 'next/head';
+
+import { useSiteConfig } from '../hooks/useSiteConfig';
+import { useLocale } from '../hooks/useLocale';
+import type { LegacyFrontMatter } from '../types';
+
+type HeaderProps = { frontMatter: LegacyFrontMatter };
+
+// @TODO: Generate RSS feeds and include them here
+const HtmlHead = ({ frontMatter }: HeaderProps) => {
+  const siteConfig = useSiteConfig();
+  const { currentLocale } = useLocale();
+
+  const pageTitle = frontMatter.title || siteConfig.title;
+
+  return (
+    <Head>
+      <title>{siteConfig.title}</title>
+
+      <meta name="theme-color" content={siteConfig.accentColor}></meta>
+
+      {/* @TODO: This should be generated during build with the site config */}
+      <link rel="manifest" href="/manifest.json" />
+
+      <link rel="icon" href={siteConfig.favicon} type="image/png" />
+
+      <html lang={currentLocale.code} dir={currentLocale.langDir} />
+
+      <meta name="robots" content={frontMatter.robots || 'index, follow'} />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="description" content={siteConfig.description} />
+
+      <meta property="og:title" content={pageTitle} />
+      <meta property="og:site_name" content={siteConfig.title} />
+      <meta property="og:description" content={siteConfig.description} />
+      <meta property="og:image" content={siteConfig.featuredImage} />
+      <meta property="og:image:type" content={siteConfig.og.imgType} />
+      <meta property="og:image:width" content={siteConfig.og.imgWidth} />
+      <meta property="og:image:height" content={siteConfig.og.imgHeight} />
+
+      <meta name="twitter:card" content={siteConfig.twitter.card} />
+      <meta name="twitter:site" content={siteConfig.twitter.username} />
+      <meta name="twitter:title" content={pageTitle} />
+      <meta name="twitter:image" content={siteConfig.twitter.img} />
+      <meta name="twitter:image:alt" content={siteConfig.twitter.imgAlt} />
+    </Head>
+  );
+};
+
+export default HtmlHead;
