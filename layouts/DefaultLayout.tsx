2d158d7f6978ca5134b25244eba4dca85398825c
Claudio Wunder <cwunder@hubspot.com>
2023-01-10 12:22:09 +0100

    chore(layouts): added header to default layout

--- layouts/DefaultLayout.tsx
+++ layouts/DefaultLayout.tsx
@@ -1,9 +1,11 @@
 import type { PropsWithChildren } from 'react';
 
 import Footer from '../components/Footer';
+import Header from '../components/Header';
 
 const DefaultLayout = ({ children }: PropsWithChildren) => (
   <>
+    <Header />
     <main id="main">
       <div className="container">{children}</div>
     </main>
