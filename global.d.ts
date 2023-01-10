b4e8f7214e74254191cb97357665209807bb9240
Claudio Wunder <cwunder@hubspot.com>
2023-01-10 17:01:21 +0100

    chore(types): added types for node-version-data

--- global.d.ts
+++ global.d.ts
@@ -5,3 +5,12 @@ declare global {
 }
 
 export default global;
+
+declare module 'node-version-data' {
+  import type { NodeVersionData } from './types';
+
+  export default function nodeVersionData(
+    // eslint-disable-next-line no-unused-vars
+    callback: (err: Error | null, data: NodeVersionData) => void
+  ): void;
+}
