057cd9deb02bc7af86d789fd427bf87b33dc9aed
Claudio Wunder <cwunder@hubspot.com>
2023-01-10 17:04:00 +0100

    chore(app): introduced node data provider usage

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app';
 import type { ReactNode } from 'react';
 import { Source_Sans_Pro } from '@next/font/google';
 
+import { NodeDataProvider } from '../providers/nodeDataProvider';
 import { LocalProvider } from '../providers/localeProvider';
 import { SiteProvider } from '../providers/siteProvider';
 
@@ -23,16 +24,18 @@ const sourceSansPro = Source_Sans_Pro({
 const Nextra = ({ Component, pageProps }: NextraAppProps) => (
   <SiteProvider>
     <LocalProvider>
-      {/* @TODO: This is a temporary solution. We might want to adopt Emotion/StyledComponents here */}
-      <style jsx global>
-        {`
-          body {
-            font: 400 20px/1.5 ${sourceSansPro.style.fontFamily}, 'Open Sans',
-              Roboto, 'San Francisco', Helvetica, Arial, sans-serif;
-          }
-        `}
-      </style>
-      <Component {...pageProps} />
+      <NodeDataProvider>
+        {/* @TODO: This is a temporary solution. We might want to adopt Emotion/StyledComponents here */}
+        <style jsx global>
+          {`
+            body {
+              font: 400 20px/1.5 ${sourceSansPro.style.fontFamily}, 'Open Sans',
+                Roboto, 'San Francisco', Helvetica, Arial, sans-serif;
+            }
+          `}
+        </style>
+        <Component {...pageProps} />
+      </NodeDataProvider>
     </LocalProvider>
   </SiteProvider>
 );
