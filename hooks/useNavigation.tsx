843c6009be9d1f401aa6474934c8228789a79829
Claudio Wunder <cwunder@hubspot.com>
2023-01-10 17:02:15 +0100

    feat(navigation): introduced a navigation hook

---
+++ hooks/useNavigation.tsx
@@ -0,0 +1,33 @@
+import { FormattedMessage } from 'react-intl';
+
+import type { NavigationEntry, NavigationKeys } from '../types';
+
+import navigation from '../navigation.json';
+
+// Translation Context for FormattedMessage
+type Context = Record<string, Record<string, string>>;
+
+export const useNavigation = () => {
+  const mapNavigationEntries = (
+    entries: Record<string, NavigationEntry>,
+    context?: Context
+  ) => {
+    const getFormattedMessage = (translationId: string, key: string) => (
+      <FormattedMessage
+        id={translationId}
+        values={(context && context[key]) || {}}
+      />
+    );
+
+    return Object.entries(entries).map(([key, item]) => ({
+      text: getFormattedMessage(item.translationId, key),
+      link: item.link,
+    }));
+  };
+
+  return {
+    navigationItems: mapNavigationEntries(navigation),
+    getSideNavigation: (section: NavigationKeys, context?: Context) =>
+      mapNavigationEntries(navigation[section].items, context),
+  };
+};
