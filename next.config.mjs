b37815912abe2dc8f751aec6cda0b74b4901e73f
Claudio Wunder <cwunder@hubspot.com>
2023-01-11 19:58:32 +0100

    feat(next): adopted new data fetching mechanism

--- next.config.mjs
+++ next.config.mjs
@@ -1,37 +1,12 @@
 import nextra from 'nextra';
 import remarkMdxDisableExplicitJsx from 'remark-mdx-disable-explicit-jsx';
-import fs from 'fs';
 
-// Note: we currently can't import .json in a mjs file.
-const i18nConfig = JSON.parse(fs.readFileSync('./i18n/config.json').toString());
+import getNextData from './next.data.mjs';
 
 const withNextra = nextra({
   theme: './theme.tsx',
-  remarkPlugins: [remarkMdxDisableExplicitJsx],
-  mdxOptions: {
-    format: 'md',
-  },
-  transform: (content, { route }) => {
-    const code = route.split('/')[1];
-    const locale = i18nConfig.find(c => c.code === code)?.code;
-
-    if (locale) {
-      return (
-        content +
-        `
-        export const getStaticProps = () => {
-          const localeMessages = require('i18n/locales/${locale}.json')
-          return {
-            props: {
-              localeMessages
-            },
-          }
-        }
-      `
-      );
-    }
-    return content;
-  },
+  mdxOptions: { format: 'md', remarkPlugins: [remarkMdxDisableExplicitJsx] },
+  transform: getNextData,
 });
 
 export default withNextra({
