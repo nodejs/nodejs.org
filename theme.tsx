fd6ddcddacd04a475fd9b0433993ec601ffa4d78
Claudio Wunder <cwunder@hubspot.com>
2023-01-18 19:22:53 +0100

    chore(theme): adopted anchored component

--- theme.tsx
+++ theme.tsx
@@ -1,18 +1,29 @@
 import React, { useEffect } from 'react';
 import { MDXProvider } from '@mdx-js/react';
-import type { NextraThemeLayoutProps } from 'nextra';
 import highlightJs from 'highlight.js/lib/common';
+import type { NextraThemeLayoutProps } from 'nextra';
+import type { MDXComponents } from 'mdx/types';
 
 import HtmlHead from './components/HtmlHead';
+import AnchoredHeading from './components/AnchoredHeading';
 import NodeApiVersionLinks from './components/Docs/NodeApiVersionLinks';
 import { LayoutProvider } from './providers/layoutProvider';
+
 import type { LegacyFrontMatter } from './types';
 
 type LayoutProps = React.PropsWithChildren<{
   pageOpts: NextraThemeLayoutProps['pageOpts'];
 }>;
 
-const mdxComponents = { NodeApiVersionLinks: NodeApiVersionLinks };
+const mdxComponents: MDXComponents = {
+  NodeApiVersionLinks: NodeApiVersionLinks,
+  h1: props => <AnchoredHeading level={1} {...props} />,
+  h2: props => <AnchoredHeading level={2} {...props} />,
+  h3: props => <AnchoredHeading level={3} {...props} />,
+  h4: props => <AnchoredHeading level={4} {...props} />,
+  h5: props => <AnchoredHeading level={5} {...props} />,
+  h6: props => <AnchoredHeading level={6} {...props} />,
+};
 
 const Content = ({ children }: LayoutProps) => {
   useEffect(() => highlightJs.highlightAll(), []);
