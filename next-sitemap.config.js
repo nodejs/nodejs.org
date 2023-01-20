982179bfc1180ef2f9f4e521d0a5f062ab6c85ce
Claudio Wunder <cwunder@hubspot.com>
2023-01-20 02:58:11 +0100

    chore(sitemap): added sitemap data

---
+++ next-sitemap.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.SITE_URL || 'https://nodejs.org',
+  generateRobotsTxt: true,
+  trailingSlash: false,
+  generateIndexSitemap: false,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        disallow: ['/dist/', '/docs/'],
+        allow: ['/dist/latest/', '/dist/latest/docs/api/', '/api/'],
+      },
+    ],
+  },
+};
