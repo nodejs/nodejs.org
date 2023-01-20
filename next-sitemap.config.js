efc6fd8f238583e44b89a940563c53d784437538
Claudio Wunder <cwunder@hubspot.com>
2023-01-20 16:35:07 +0100

    chore(sitemap): removed non-existent env

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://nodejs.org',
+  siteUrl: 'https://nodejs.org',
   generateRobotsTxt: true,
   trailingSlash: true,
   generateIndexSitemap: false,
