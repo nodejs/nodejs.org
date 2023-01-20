8c1f8f5be7f3ea6c4b26528868300c34020076fb
Claudio Wunder <cwunder@hubspot.com>
2023-01-20 12:50:20 +0100

    chore(next): minor refactor of helper

--- scripts/next-data/getLocalisationData.mjs
+++ scripts/next-data/getLocalisationData.mjs
@@ -3,12 +3,13 @@ import { readFile } from 'fs/promises';
 import { basename, extname, join } from 'path';
 import { fileURLToPath } from 'url';
 
+// this allows us to get the current module working directory
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+
+// gets the current i18n path based on local module path
+const i18nPath = join(__dirname, '../../i18n');
+
 const getLocalisationData = () => {
-  // this allows us to get the current module working directory
-  const __dirname = fileURLToPath(new URL('.', import.meta.url));
-
-  const i18nPath = join(__dirname, '../../i18n');
-
   // read the locale config file as a JSON object without using imports
   const localePromise = readFile(join(i18nPath, 'config.json')).then(
     JSON.parse
@@ -21,13 +22,14 @@ const getLocalisationData = () => {
   ];
 
   // dynamically load all locale files into json data and converts into an object
+  // we read the directory asynchronously as we want all the filenames ahead of time
   const allLocaleMessages = Object.fromEntries(
     readdirSync(join(i18nPath, 'locales'))
       .filter(f => extname(f) === '.json')
       .map(mapLocaleMessages)
   );
 
-  return (route, defaultLocale = 'en') => {
+  return (route = '/', defaultLocale = 'en') => {
     const localeCode = route.split('/')[1] || defaultLocale;
 
     return localePromise.then(localeConfig => {
