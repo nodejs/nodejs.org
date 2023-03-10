b9d9ea8e947c7da030e507a58ea6e93afb826e79
DongWei <maledong_public@foxmail.com>
2023-03-10 20:31:15 +0800

    fix: AnchorLinks don't work properly
    This fixture mainly focuses on two bugs:
    
    1. For Heading <h1>, it doesn't render as what we see for "h2" ,
       "h3"..,and the "id" is always null. So the solution is to render the
       the children (here it must be a string) and return it in default.
    
    2. For very long English titles or non-English titles in md files,
    the generated anchor name in default will be very hard to read/write in
    md files if we want to refer. What we used to do is to give it an alian
    name in short, Take Chinese generated HTML files will be a good example
    to explain.

--- components/AnchoredHeading.tsx
+++ components/AnchoredHeading.tsx
@@ -7,11 +7,42 @@ type AnchorHeadingProps = PropsWithChildren<{
   id?: string;
 }>;
 
+/**
+ * This module will automatically replace the header style words
+ * (such as `#`,`##`...ect) to an anchor name.
+ *
+ * This processor will make all the English characters (26 letters)
+ * into their lower case, for others in the middle of words,
+ * they will become `-` in default. For more than one `-` together,
+ * there's only one left.
+ *
+ * If your title has some non-English characters, please use
+ * `<!---->` to quote your English anchor name inside, with your
+ * own title beside it. Otherwises it will return you what it is.
+ */
+
+// React will ignore "<!" and ">" in the string,
+// so we can just use '-- --' to quote the anchor name inside it.
+const COMMENT_FOR_HEADANCHOR = /--\x20?([\w\x20-]+)\x20?--/;
+
 const AnchoredHeading = ({ children, level, id }: AnchorHeadingProps) => {
   const HeadingLevelTag = `h${level}` as any;
 
-  // This regex replacement is used for <!-- --> comments within the heading
-  const sanitizedId = id?.replace(/---(.+)---(.*)/, (_, f) => f);
+  let sanitizedId =
+    id ?? children?.toLocaleString().toLocaleLowerCase().replace(/\x20/g, '-');
+
+  if (sanitizedId) {
+    const foundAnchorAndTitle = COMMENT_FOR_HEADANCHOR.exec(sanitizedId);
+
+    if (foundAnchorAndTitle) {
+      sanitizedId = foundAnchorAndTitle[1]
+        .replace(/(\[([^\]]+)]\([^)]+\))/g, '$2')
+        .replace(/[^\w]+/g, '-')
+        .replace(/[\x20]+/g, '-')
+        .replace(/-{2,}/g, '-')
+        .replace(/(^-|-$)/g, '');
+    }
+  }
 
   return (
     <HeadingLevelTag id={sanitizedId}>
