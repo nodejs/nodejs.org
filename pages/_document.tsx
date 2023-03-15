af50ef4e8b95755481917ddc4a1b76a468061255
Claudio Wunder <cwunder@hubspot.com>
2023-03-15 01:27:04 +0100

    fix(lint): fixed lint due to previous commit

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,22 +7,19 @@ const basePath = process.env.NEXT_BASE_PATH || '';
 
 // @TODO: The custom scripts should be removed in the future when switching over `nodejs/nodejs.dev` codebase
 // Note.: Some of these scripts will also be completely removed from the codebase such as jQuery
-// eslint-disable-next-line react/function-component-definition
-const Document = () => {
-  return (
-    <Html>
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-        <Script
-          strategy="beforeInteractive"
-          src={`${basePath}/static/js/legacyMain.js`}
-        />
-        <a rel="me" href="https://social.lfx.dev/@nodejs" />
-      </body>
-    </Html>
-  );
-};
+const Document = () => (
+  <Html>
+    <Head />
+    <body>
+      <Main />
+      <NextScript />
+      <Script
+        strategy="beforeInteractive"
+        src={`${basePath}/static/js/legacyMain.js`}
+      />
+      <a rel="me" href="https://social.lfx.dev/@nodejs" />
+    </body>
+  </Html>
+);
 
 export default Document;
