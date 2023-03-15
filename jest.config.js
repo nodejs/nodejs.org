05f57f56c3c940af5a9664748ebd02763f6f9a85
Augustin Mauroy <97875033+AugustinMauroy@users.noreply.github.com>
2023-03-15 18:57:09 +0100

    feat(unit test): introduce unit test

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+/** @type {import('jest').Config} */
+const customJestConfig = {
+  testPathIgnorePattern: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
+  testMatch: ['**__tests__/**/*.test.[jt]s?(x)'],
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  testEnvironment: 'jest-environment-jsdom',
+};
+
+module.exports = createJestConfig(customJestConfig);
