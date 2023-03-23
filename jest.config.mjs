1ec7c30a4b63e3807b6362d1cb0db373bd7cfe0d
Augustin Mauroy <97875033+AugustinMauroy@users.noreply.github.com>
2023-03-23 12:05:23 +0100

    feat(unit-test): introduce unit test on website redesign branch (#5178
    feat(unit-test): introduce unit test

--- jest.config.mjs
+++ jest.config.mjs
@@ -7,8 +7,6 @@ const createJestConfig = nextJest({
 /** @type {import('jest').Config} */
 const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  moduleDirectories: ['node_modules', '<rootDir>/'],
-  testEnvironment: 'jest-environment-jsdom',
 };
 
 export default createJestConfig(customJestConfig);
