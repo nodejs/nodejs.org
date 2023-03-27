0202d39b05dff8fb9f98a3b5ea6bcf7fdae89556
Guilherme Araújo <arauujogui@gmail.com>
2023-03-27 19:21:12 -0300

    chore: set up storybook

---
+++ .storybook/main.js
@@ -0,0 +1,17 @@
+/** @type { import('@storybook/nextjs').StorybookConfig } */
+const config = {
+  stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+};
+export default config;
