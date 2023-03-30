6b049363aaebb853144541a82c0869659a0c7182
Guilherme Araújo <arauujogui@gmail.com>
2023-03-30 17:17:15 -0300

    chore: set up app

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,9 @@
+import * as NextImage from 'next/image';
+import App from '../pages/_app.mdx';
+import { pageProps } from './constants';
+import '../styles/styles.scss';
+import '../styles/tokens.scss';
+
 /** @type { import('@storybook/react').Preview } */
 const preview = {
   parameters: {
@@ -8,7 +14,23 @@ const preview = {
         date: /Date$/,
       },
     },
+    nextjs: {
+      router: {
+        basePath: '',
+      },
+    },
   },
 };
+
+export const decorators = [
+  Story => <App Component={Story} pageProps={pageProps} />,
+];
+
+const OriginalNextImage = NextImage.default;
+
+Object.defineProperty(NextImage, 'default', {
+  configurable: true,
+  value: props => <OriginalNextImage {...props} unoptimized />,
+});
 
 export default preview;
