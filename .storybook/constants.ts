36d831c7e6fdaa10e6e24c9ee6ba5cd3dd083ac0
Guilherme Araújo <guilherme.araujo@maxxidata.com>
2023-04-01 06:03:11 -0300

    chore: set up storybook (#5191) (#5214)

--- .storybook/constants.ts
+++ .storybook/constants.ts
@@ -1,7 +1,6 @@
-import type { AppProps, NodeVersionData } from '../types';
-import englishMessages from '../i18n/locales/en.json';
+import en from '../i18n/locales/en.json';
 
-const i18nData: AppProps['i18nData'] = {
+const i18nData = {
   currentLocale: {
     code: 'en',
     localName: 'English',
@@ -11,24 +10,29 @@ const i18nData: AppProps['i18nData'] = {
     hrefLang: 'en-US',
     enabled: true,
   },
-  localeMessages: englishMessages,
+  localeMessages: en,
 };
 
-const nodeVersionData: NodeVersionData[] = [
-  {
-    node: 'v19.8.1',
-    nodeNumeric: '19.8.1',
-    nodeMajor: 'v19.x',
-    npm: '9.5.1',
-    isLts: false,
-  },
-  {
-    node: 'v18.15.0',
-    nodeNumeric: '18.15.0',
-    nodeMajor: 'v18.x',
-    npm: '9.5.0',
-    isLts: true,
-  },
-];
+const nodeVersionData = {
+  nodeVersionData: [
+    {
+      node: 'v19.8.1',
+      nodeNumeric: '19.8.1',
+      nodeMajor: 'v19.x',
+      npm: '9.5.1',
+      isLts: false,
+    },
+    {
+      node: 'v18.15.0',
+      nodeNumeric: '18.15.0',
+      nodeMajor: 'v18.x',
+      npm: '9.5.0',
+      isLts: true,
+    },
+  ],
+};
 
-export const pageProps = { i18nData, nodeVersionData };
+export const pageProps = {
+  i18nData,
+  nodeVersionData,
+};
