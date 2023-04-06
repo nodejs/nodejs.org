c2fe0cd6e92ce8e8be82a641ff3df01e51087964
Guilherme Araújo <guilherme.araujo@maxxidata.com>
2023-04-06 06:09:31 -0300

    chore: set up testing-library jest extend (#5231)
    Co-authored-by: Manish Kumar ⛄ <manishprivet@protonmail.com>

--- jest.config.mjs
+++ jest.config.mjs
@@ -6,6 +6,7 @@ const createJestConfig = nextJest({
 
 /** @type {import('jest').Config} */
 const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleDirectories: ['node_modules', '<rootDir>/'],
   testEnvironment: 'jest-environment-jsdom',
 };
