d3b9008d97ba8d8f8e726b2f9802d2ea49bd49cc
Shanmughapriyan S <priyanshan03@gmail.com>
2023-04-08 17:42:28 +0530

    chore: sass addon

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,7 +6,6 @@ const config: StorybookConfig = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
-    'storybook-addon-sass-postcss',
   ],
   framework: {
     name: '@storybook/nextjs',
