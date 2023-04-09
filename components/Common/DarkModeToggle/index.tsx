ee48936d9065a5b3e13bf54c7923332ca1aaafa4
Shanmughapriyan S <priyanshan03@gmail.com>
2023-04-10 00:40:58 +0530

    chore: lint

--- components/Common/DarkModeToggle/index.tsx
+++ components/Common/DarkModeToggle/index.tsx
@@ -26,6 +26,7 @@ const DarkModeToggle = () => {
 
   useEffect(() => {
     if (theme) updateBodyClass(theme);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   function toggleTheme(isKeyPress?: boolean) {
