1d07ef593d8feec61b5891dce950dc5576251fa4
Claudio Wunder <cwunder@hubspot.com>
2023-04-17 10:30:58 +0200

    chore: storybook remove gfm and ts for constants

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,7 +6,6 @@ const config: StorybookConfig = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
-    '@storybook/addon-mdx-gfm',
   ],
   framework: {
     name: '@storybook/nextjs',
