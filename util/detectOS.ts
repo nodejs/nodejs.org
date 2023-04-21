d8926e639f734102cfe467dd03f1764e07633f7f
FHachez <floran.hachez@gmail.com>
2023-04-21 18:47:12 +0200

    feat(detectOs): detect based on the userAgent or appVersion

--- util/detectOS.ts
+++ util/detectOS.ts
@@ -37,39 +37,3 @@ function detectOsInString(userAgent: string): UserOS {
       return UserOS.UNKNOWN;
   }
 }
-// eslint-disable-next-line no-shadow
-export enum UserOS {
-  MAC = 'MAC',
-  WIN = 'WIN',
-  UNIX = 'UNIX',
-  LINUX = 'LINUX',
-  MOBILE = 'MOBILE',
-  UNKNOWN = 'UNKNOWN',
-}
-
-export function detectOS(): UserOS {
-  let OS = UserOS.UNKNOWN;
-  if (typeof window !== 'undefined') {
-    switch (typeof navigator !== 'undefined') {
-      case navigator.appVersion.includes('Win'):
-        OS = UserOS.WIN;
-        break;
-      case navigator.appVersion.includes('Mac'):
-        OS = UserOS.MAC;
-        break;
-      case navigator.appVersion.includes('X11'):
-        OS = UserOS.UNIX;
-        break;
-      case navigator.appVersion.includes('Linux'):
-        OS = UserOS.LINUX;
-        break;
-      case navigator.appVersion.includes('Mobi'):
-        OS = UserOS.MOBILE;
-        break;
-      default:
-        OS = UserOS.UNKNOWN;
-        break;
-    }
-  }
-  return OS;
-}
