fe333da6bc1d151fb147653d6204966481d5f49e
Uria Rogel <uria_rogel@hotmail.com>
2023-04-24 23:52:59 +0200

    import new-header

--- layouts/BaseLayout.tsx
+++ layouts/BaseLayout.tsx
@@ -1,5 +1,5 @@
 import Footer from '../components/Footer';
-import Header from '../components/Header';
+import Header from '../components/NewHeader';
 import type { FC, PropsWithChildren } from 'react';
 
 const BaseLayout: FC<PropsWithChildren> = ({ children }) => (
