61f1eca83edf028b107a45d6f8a446b89e510981
Guilherme Araújo <arauujogui@gmail.com>
2023-04-27 10:48:04 -0300

    chore: create inlinecode story

--- components/Article/Codebox/index.stories.tsx
+++ components/Article/Codebox/index.stories.tsx
@@ -1,11 +1,22 @@
+import InlineCode from './InlineCode';
 import Codebox from './index';
-
-export default { component: Codebox };
+import type { StoryObj } from '@storybook/react';
 
 const code = 'const a = 1;';
 
-export const Default = {
+export default { component: Codebox };
+
+type Story = StoryObj<typeof Codebox>;
+
+export const Default: Story = {
   args: {
     children: <pre className="language-js">{code}</pre>,
+  },
+};
+
+export const Inline: Story = {
+  render: ({ children }) => <InlineCode>{children}</InlineCode>,
+  args: {
+    children: <code>{code}</code>,
   },
 };
