9852061d7ac6b7a1e70a64a82c1152bca1e0b692
Augustin Mauroy <augustin.mauroy@outlook.fr>
2023-04-28 07:57:36 +0000

    feat(NodeFeat): introduce

---
+++ components/Home/NodeFeat/index.tsx
@@ -0,0 +1,60 @@
+import { cloneElement } from 'react';
+import { IoLogoNodejs, IoMdGitPullRequest, IoMdRocket } from 'react-icons/io';
+import { FormattedMessage } from 'react-intl';
+import styles from './index.module.scss';
+import type { ReactElement } from 'react';
+
+type NodeFeatureProps = {
+  icon: ReactElement;
+  heading: ReactElement;
+  description: ReactElement;
+};
+
+const styled = (icon: ReactElement): ReactElement =>
+  cloneElement(icon, {
+    alt: 'Node Feature',
+    className: styles.featureIcon,
+  });
+
+const features = [
+  {
+    icon: styled(<IoLogoNodejs />),
+    heading: 'pages.index.features.javascript.title',
+    description: 'pages.index.features.javascript.description',
+  },
+  {
+    icon: styled(<IoMdGitPullRequest />),
+    heading: 'pages.index.features.openSource.title',
+    description: 'pages.index.features.openSource.description',
+  },
+  {
+    icon: styled(<IoMdRocket />),
+    heading: 'pages.index.features.everywhere.title',
+    description: 'pages.index.features.everywhere.description',
+  },
+];
+
+const NodeFeature = ({ icon, heading, description }: NodeFeatureProps) => (
+  <div className={styles.text}>
+    {icon}
+    <h2 className="t-subheading2">{heading}</h2>
+    <p>{description}</p>
+  </div>
+);
+
+const NodeFeatures = () => {
+  return (
+    <section className={styles.nodeFeatures}>
+      {features.map(feature => (
+        <NodeFeature
+          key={feature.heading}
+          icon={feature.icon}
+          heading={<FormattedMessage id={feature.heading} />}
+          description={<FormattedMessage id={feature.description} />}
+        />
+      ))}
+    </section>
+  );
+};
+
+export default NodeFeatures;
