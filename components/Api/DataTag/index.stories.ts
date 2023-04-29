364c30318610716ca617228c46c999f15b6fdb18
Harkunwar Kochar <10580591+Harkunwar@users.noreply.github.com>
2023-04-29 15:38:28 -0700

    test(snapshot): Migrate workflow

--- components/Api/DataTag/index.stories.ts
+++ components/Api/DataTag/index.stories.ts
@@ -4,6 +4,7 @@ import type { Meta as MetaObj, StoryObj } from '@storybook/react';
 type Story = StoryObj<typeof DataTag>;
 type Meta = MetaObj<typeof DataTag>;
 
+
 export const Red: Story = {
   args: {
     tag: 'E',
