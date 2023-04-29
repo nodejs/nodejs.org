7a87dfcc099818f668efa396d3a44712e2da9182
Harkunwar Kochar <10580591+Harkunwar@users.noreply.github.com>
2023-04-29 15:44:49 -0700

    test(snapshot): Fix file

--- components/Api/DataTag/index.stories.ts
+++ components/Api/DataTag/index.stories.ts
@@ -4,7 +4,6 @@ import type { Meta as MetaObj, StoryObj } from '@storybook/react';
 type Story = StoryObj<typeof DataTag>;
 type Meta = MetaObj<typeof DataTag>;
 
-
 export const Red: Story = {
   args: {
     tag: 'E',
