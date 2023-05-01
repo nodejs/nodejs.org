b38a3b4cdd09ab7342d2a909c8bb3b52385117d2
Augustin Mauroy <augustin.mauroy@outlook.fr>
2023-05-01 18:41:16 +0200

    Feat(DownloadHeader): introduce component

---
+++ components/Downloads/DownloadHeader/index.stories.tsx
@@ -0,0 +1,9 @@
+import DownloadHeader from '.';
+import type { Meta as MetaObj, StoryObj } from '@storybook/react';
+
+type Story = StoryObj<typeof DownloadHeader>;
+type Meta = MetaObj<typeof DownloadHeader>;
+
+export const Default: Story = {};
+
+export default { component: DownloadHeader } as Meta;
---
+++ components/Downloads/DownloadHeader/index.tsx
@@ -0,0 +1,40 @@
+import { FormattedMessage, injectIntl } from 'react-intl';
+import styles from './index.module.scss';
+import SectionTitle from '../../Common/SectionTitle';
+import type { WrappedComponentProps } from 'react-intl';
+
+type Props = {
+  // release?: NodeReleaseData;
+  release?: any;
+};
+
+const DownloadHeader = ({ release, intl }: Props & WrappedComponentProps) => (
+  <>
+    <div className={styles.downloadHeader}>
+      <SectionTitle
+        path={[
+          'home',
+          intl.formatMessage({
+            id: 'components.downloadHeader.navigation.activeSection',
+          }),
+        ]}
+      />
+      <div>
+        <FormattedMessage
+          id="components.downloadHeader.navigation.nodeVersion"
+          values={{ lts: release?.isLts, nodeVersion: release?.fullVersion }}
+        />
+      </div>
+    </div>
+    <div className={styles.downloadHeader}>
+      <div className={styles.title}>
+        <FormattedMessage id="components.downloadHeader.navigation.title" />
+      </div>
+      <div className={styles.npm}>
+        <FormattedMessage id="components.downloadHeader.navigation.npmVersion" />
+      </div>
+    </div>
+  </>
+);
+
+export default injectIntl(DownloadHeader);
