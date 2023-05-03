816f1af2d642f3358ad8296624100e9a9303fb6b
Augustin Mauroy <augustin.mauroy@outlook.fr>
2023-05-03 12:39:30 +0200

    feat(useClickOutside): introduce

---
+++ hooks/useClickOutside.ts
@@ -0,0 +1,27 @@
+import { useEffect } from 'react';
+import type { RefObject, MouseEvent, TouchEvent } from 'react';
+
+// eslint-disable-next-line no-unused-vars
+type TypeHandler = (event: MouseEvent | TouchEvent) => void;
+
+export const useClickOutside = (
+  ref: RefObject<HTMLElement>,
+  handler: TypeHandler
+) => {
+  useEffect(() => {
+    const listener = (event: MouseEvent | TouchEvent) => {
+      if (!ref.current || ref.current.contains(event.target as Node)) {
+        return;
+      }
+      handler(event);
+    };
+
+    document.addEventListener('mousedown', listener);
+    document.addEventListener('touchstart', listener);
+
+    return () => {
+      document.removeEventListener('mousedown', listener);
+      document.removeEventListener('touchstart', listener);
+    };
+  }, [ref, handler]);
+};
