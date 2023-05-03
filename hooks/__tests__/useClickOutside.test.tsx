591b70e517d837b135c6239341b2733458a74b43
Augustin Mauroy <augustin.mauroy@outlook.fr>
2023-05-03 15:33:28 +0200

    feat(useClickOutside): fix and twix

--- hooks/__tests__/useClickOutside.test.tsx
+++ hooks/__tests__/useClickOutside.test.tsx
@@ -1,26 +1,30 @@
-import { render, fireEvent, screen } from '@testing-library/react';
 import { useRef } from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { useClickOutside } from '../useClickOutside';
 
-describe('test hook useClickOutside', () => {
-  const handler = jest.fn();
-  const Component = ({ handler }: { handler: () => void }) => {
+describe('useClickOutside', () => {
+  const mockFn = jest.fn(() => console.log('click outside'));
+  const SubComponent = ({ onClickOutside }: { onClickOutside: () => void }) => {
     const ref = useRef(null);
-    useClickOutside(ref, handler);
-
-    return (
-      <>
-        <div data-testid="outside" />
-        <div ref={ref} data-testid="inside" />
-      </>
-    );
+    useClickOutside(ref, onClickOutside);
+    return <div ref={ref}>inside</div>;
   };
+  const Component = () => (
+    <div>
+      outside
+      <SubComponent onClickOutside={mockFn} />
+    </div>
+  );
 
-  it('calls handler when clicking outside ref', () => {
-    render(<Component handler={handler} />);
-    fireEvent.click(screen.getByTestId('outside'));
-    expect(handler).toHaveBeenCalled();
-    fireEvent.click(screen.getByTestId('inside'));
-    expect(handler).toHaveBeenCalledTimes(1);
+  it('should call onClickOutside when click outside', () => {
+    render(<Component />);
+    fireEvent.click(screen.getByText('outside'));
+    expect(mockFn).toHaveBeenCalled();
+  });
+
+  it('should not call onClickOutside when click inside', () => {
+    render(<Component />);
+    fireEvent.click(screen.getByText('inside'));
+    expect(mockFn).not.toHaveBeenCalled();
   });
 });
