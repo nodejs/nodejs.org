acbf58fd336e5f6d6766d2e0ae7ab0a629aa0c73
Augustin Mauroy <augustin.mauroy@outlook.fr>
2023-05-03 11:29:11 +0200

    (website redesign) Feat(shellbox): migration (#5234)
    Co-authored-by: Manish Kumar ⛄ <manishprivet@protonmail.com>
    Co-authored-by: Claudio Wunder <cwunder@gnome.org>
    Co-authored-by: Shanmughapriyan S <priyanshan03@gmail.com>
    Co-authored-by: Michael Esteban <mickel13@gmail.com>
    Co-authored-by: Wai.Tung <maledong_public@foxmail.com>

--- components/Common/ShellBox/__tests__/index.test.tsx
+++ components/Common/ShellBox/__tests__/index.test.tsx
@@ -23,6 +23,15 @@ describe('ShellBox', () => {
     });
   });
 
+  it('should render', () => {
+    const { container } = render(
+      <IntlProvider locale="en" onError={() => {}}>
+        <ShellBox>test</ShellBox>
+      </IntlProvider>
+    );
+    expect(container).toMatchSnapshot();
+  });
+
   it('should call clipboard API with `test` once', async () => {
     const user = userEvent.setup();
     const navigatorClipboardWriteTextSpy = jest
