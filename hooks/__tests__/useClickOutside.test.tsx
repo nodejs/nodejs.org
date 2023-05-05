91e6609b1d58d16eed0c2e0289b4923f9792f723
Augustin Mauroy <augustin.mauroy@outlook.fr>
2023-05-05 10:54:20 +0200

    better unit test

--- hooks/__tests__/useClickOutside.test.tsx
+++ hooks/__tests__/useClickOutside.test.tsx
@@ -1,15 +1,40 @@
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
+import { useState } from 'react';
 import { useClickOutside } from '../useClickOutside';
 
 describe('useClickOutside', () => {
   const handler = jest.fn();
   const Component = () => {
-    const ref = useClickOutside<HTMLDivElement>(handler);
-    return <div ref={ref}>Hello</div>;
+    const [state, setState] = useState(false);
+    const ref = useClickOutside<HTMLDivElement>(() => {
+      setState(false);
+      handler();
+    });
+
+    return (
+      <div>
+        <h1>Page</h1>
+        <div>
+          <button onClick={() => setState(!state)}>Open</button>
+          {state && (
+            <div ref={ref}>
+              <h2>Modal</h2>
+            </div>
+          )}
+        </div>
+      </div>
+    );
   };
-  it('should call the handler function when clicking outside of the element', () => {
+  it('should call handler when click outside', () => {
     render(<Component />);
-    fireEvent.click(document.body);
-    expect(handler).toHaveBeenCalled();
+    fireEvent.click(screen.getByText('Open'));
+    fireEvent.click(screen.getByText('Page'));
+    expect(screen.queryByText('Modal')).not.toBeInTheDocument();
+  });
+  it('should not call handler when click inside', () => {
+    render(<Component />);
+    fireEvent.click(screen.getByText('Open'));
+    fireEvent.click(screen.getByText('Modal'));
+    expect(screen.getByText('Modal')).toBeInTheDocument();
   });
 });
