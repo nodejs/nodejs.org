78f0a1c63f4898acaccdaaf8b15235972b44c902
Augustin Mauroy <augustin.mauroy@outlook.fr>
2023-05-13 12:34:42 +0200

    Update useClickOutside.ts

--- hooks/useClickOutside.ts
+++ hooks/useClickOutside.ts
@@ -7,15 +7,14 @@ export const useClickOutside = <T extends HTMLElement>(
 ) => {
   const ref = useRef<T>(null);
   useEffect(() => {
-    const clickEvent = 'ontouchstart' in window ? 'touchstart' : 'click';
     const listener = (event: MouseEvent | TouchEvent) => {
       if (ref.current && !ref.current.contains(event.target as Node)) {
         handler(event);
       }
     };
 
-    document.addEventListener(clickEvent, listener, true);
-    return () => document.removeEventListener(clickEvent, listener);
+    document.addEventListener('click', listener, true);
+    return () => document.removeEventListener('click', listener);
   }, [ref, handler]);
   return ref;
 };
