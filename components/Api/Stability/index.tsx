9c2b8696ba13a53034814f071d3af895005e5665
Augustin Mauroy <augustin.mauroy@outlook.fr>
2023-05-13 17:45:42 +0200

    code format

--- components/Api/Stability/index.tsx
+++ components/Api/Stability/index.tsx
@@ -4,11 +4,10 @@ import type { PropsWithChildren, FC } from 'react';
 
 type StabilityProps = PropsWithChildren<{ stability: number }>;
 
- const getStabilityClass = (stability: number) => {
-    const style = styles[`stabilityLevel${stability}`]
-    if(!style)
-        throw new Error(`Unknown stability level: ${stability}`);
- };
+const getStabilityClass = (stability: number) => {
+  const style = styles[`stabilityLevel${stability}`];
+  if (!style) throw new Error(`Unknown stability level: ${stability}`);
+};
 
 const Stability: FC<StabilityProps> = ({ stability, children }) => (
   <div className={`${styles.stability} ${getStabilityClass(stability)}`}>
