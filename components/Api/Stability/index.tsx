28d370cc570dc4a218a74a7fd760e1c60d0234b0
Augustin Mauroy <augustin.mauroy@outlook.fr>
2023-05-13 15:34:27 +0200

    Update components/Api/Stability/index.tsx
    Co-authored-by: Jithil P Ponnan <MrJithil@users.noreply.github.com>
    Signed-off-by: Augustin Mauroy <augustin.mauroy@outlook.fr>

--- components/Api/Stability/index.tsx
+++ components/Api/Stability/index.tsx
@@ -4,20 +4,11 @@ import type { PropsWithChildren, FC } from 'react';
 
 type StabilityProps = PropsWithChildren<{ stability: number }>;
 
-const getStabilityClass = (stability: number) => {
-  switch (stability) {
-    case 0:
-      return styles.stabilityLevel0;
-    case 1:
-      return styles.stabilityLevel1;
-    case 2:
-      return styles.stabilityLevel2;
-    case 3:
-      return styles.stabilityLevel3;
-    default:
-      throw new Error(`Unknown stability level: ${stability}`);
-  }
-};
+ const getStabilityClass = (stability: number) => {
+    const style = styles[`stabilityLevel${stability}`]
+    if(!style)
+        throw new Error(`Unknown stability level: ${stability}`);
+ };
 
 const Stability: FC<StabilityProps> = ({ stability, children }) => (
   <div className={`${styles.stability} ${getStabilityClass(stability)}`}>
