547b5b7521ed29e2ba13c085ee9917acec58a14a
Shiba Rin <hinatakaho517@gmail.com>
2023-05-14 16:33:21 +0800

    chore: add Node releases data fixture

--- .storybook/constants.ts
+++ .storybook/constants.ts
@@ -1,5 +1,6 @@
 import type { AppProps, NodeReleaseData } from '../types';
 import englishMessages from '../i18n/locales/en.json';
+import { createNodeReleasesData } from '../__fixtures__/nodeReleasesData';
 
 const i18nData: AppProps['i18nData'] = {
   currentLocale: {
@@ -14,8 +15,7 @@ const i18nData: AppProps['i18nData'] = {
   localeMessages: englishMessages,
 };
 
-// TODO: Add fixture
-// See https://github.com/nodejs/nodejs.org/blob/f0c376bf4404ca2751b24a96681f6ed5edf65b37/__fixtures__/page.tsx
-const nodeReleasesData: NodeReleaseData[] = [];
-
-export const pageProps = { i18nData, nodeReleasesData };
+export const pageProps = {
+  i18nData,
+  nodeReleasesData: createNodeReleasesData(),
+};
