2ec854534d83c2b3786b43f3c07d7b91b363f0f6
Claudio Wunder <cwunder@gnome.org>
2023-05-14 18:14:35 +0200

    Apply suggestions from code review
    Signed-off-by: Claudio Wunder <cwunder@gnome.org>

--- hooks/useClickOutside.ts
+++ hooks/useClickOutside.ts
@@ -6,6 +6,7 @@ export const useClickOutside = <T extends HTMLElement>(
   handler: useClickOutsideType
 ) => {
   const ref = useRef<T>(null);
+
   useEffect(() => {
     const listener = (event: MouseEvent | TouchEvent) => {
       if (ref.current && !ref.current.contains(event.target as Node)) {
@@ -14,6 +15,7 @@ export const useClickOutside = <T extends HTMLElement>(
     };
 
     document.addEventListener('click', listener, true);
+
     return () => document.removeEventListener('click', listener);
   }, [ref, handler]);
   return ref;
