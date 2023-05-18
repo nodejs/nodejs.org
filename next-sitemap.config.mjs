774442fa6da20e12fedb9e0d655dced2e664fb5a
Claudio Wunder <cwunder@hubspot.com>
2023-05-18 18:42:30 +0200

    chore: updated next-sitemap config

---
+++ next-sitemap.config.mjs
@@ -0,0 +1,24 @@
+// This is used for telling Next.js to to a Static Export Build of the Website
+// We use this within this config file to determine the output directory of this generated sitemap files
+const enableStaticExport = process.env.NEXT_STATIC_EXPORT === 'true';
+
+/** @type {import('next-sitemap').IConfig} */
+const sitemapConfig = {
+  siteUrl: 'https://nodejs.org',
+  changefreq: 'always',
+  trailingSlash: false,
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+  outDir: enableStaticExport ? 'build' : 'public',
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        disallow: ['/dist/', '/docs/'],
+        allow: ['/dist/latest/', '/dist/latest/docs/api/', '/api/'],
+      },
+    ],
+  },
+};
+
+export default sitemapConfig;
