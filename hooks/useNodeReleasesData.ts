d6087ea963b238f0155c0f3b71c6d604b3678336
Shiba Rin <hinatakaho517@gmail.com>
2023-05-21 18:25:10 +0800

    refactor: simplify useNodeReleasesData hooks

--- hooks/useNodeReleasesData.ts
+++ hooks/useNodeReleasesData.ts
@@ -1,12 +1,4 @@
 import { useContext } from 'react';
 import { NodeReleasesDataContext } from '../providers/nodeReleasesDataProvider';
 
-export const useNodeReleasesData = () => {
-  const { releases, lts, current } = useContext(NodeReleasesDataContext);
-
-  return {
-    releases: releases,
-    lts: lts,
-    current: current,
-  };
-};
+export const useNodeReleasesData = () => useContext(NodeReleasesDataContext);
