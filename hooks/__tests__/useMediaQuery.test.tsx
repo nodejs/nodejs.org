640ad8e164b8466bb7caf259e5f95881106d952a
u-rogel <18611576+u-rogel@users.noreply.github.com>
2023-05-23 11:09:25 +0200

    feat: Migrate Header (#5367)
    * refactor: Migrate Header and ActiveLocalizedLink
    
    * test: Added storybook
    
    * fix: Adding missing i18n entries
    
    * refactor: Added hook useMediaQuery
    
    * test: Migrated tests
    
    * test: Storybook snapshot added
    
    * refactor: Moving i18n new entries to bottom
    
    * refactor: Using i18n under nested section
    
    * test: Updated snapshots after i18n keys update
    
    * test: Migrated tests for useMediaQuery
    
    * test: Snapshot removed
    
    * test: Added for ActiveLocalizedLink
    
    * style: Changes after pr review
    
    * test: Changed theme toggle test to matching logo image
    
    * test: Remove render for hook test
    
    * style: Using closest available css var
    
    * test: Removed not needed tests
    
    * chore: updated translation keys
    
    * refactor: Media Mixins in same file
    
    * refactor: Media Mixins in same file
    
    * refactor: Mini pr review suggestions
    
    * refactor: Mixins import
    
    * chore: reorganised scss modules
    
    * chore: tinye refactor of component
    
    ---------
    
    Signed-off-by: Claudio Wunder <cwunder@gnome.org>
    Co-authored-by: Claudio Wunder <cwunder@gnome.org>
    Co-authored-by: Claudio Wunder <cwunder@hubspot.com>
    Co-authored-by: Shanmughapriyan S <priyanshan03@gmail.com>

---
+++ hooks/__tests__/useMediaQuery.test.tsx
@@ -0,0 +1,77 @@
+import { renderHook } from '@testing-library/react';
+import { useMediaQuery } from '../useMediaQuery';
+
+describe('useMediaQuery', () => {
+  it('should check for matchMedia support', () => {
+    const { result } = renderHook(() => useMediaQuery('media-query-mock'));
+
+    expect(result.current).toBe(undefined);
+  });
+
+  it('should return true for matched query', () => {
+    Object.defineProperty(window, 'matchMedia', {
+      writable: true,
+      value: () => ({
+        matches: true,
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+      }),
+    });
+
+    const { result } = renderHook(() => useMediaQuery('media-query-mock'));
+
+    expect(result.current).toBe(true);
+  });
+
+  it('should return false for not-matched query', () => {
+    Object.defineProperty(window, 'matchMedia', {
+      writable: true,
+      value: () => ({
+        matches: false,
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+      }),
+    });
+
+    const { result } = renderHook(() => useMediaQuery('media-query-mock'));
+
+    expect(result.current).toBe(false);
+  });
+
+  it('should subscribe for media changes', () => {
+    const listenerMock = jest.fn().mockImplementation((_, handler) => {
+      handler();
+    });
+
+    Object.defineProperty(window, 'matchMedia', {
+      writable: true,
+      value: () => ({
+        matches: false,
+        addEventListener: listenerMock,
+        removeEventListener: jest.fn(),
+      }),
+    });
+
+    renderHook(() => useMediaQuery('media-query-mock'));
+
+    expect(listenerMock).toHaveBeenCalledTimes(1);
+  });
+
+  it("should support MediaQueryList's old event listeners", () => {
+    const listenerMock = jest.fn().mockImplementation(handler => {
+      handler();
+    });
+
+    Object.defineProperty(window, 'matchMedia', {
+      writable: true,
+      value: () => ({
+        matches: false,
+        addListener: listenerMock,
+        removeListener: jest.fn(),
+      }),
+    });
+
+    renderHook(() => useMediaQuery('media-query-mock'));
+    expect(listenerMock).toHaveBeenCalledTimes(1);
+  });
+});
