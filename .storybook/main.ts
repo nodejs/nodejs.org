8971470b0cc89e0a0ca10deaa112b49f8ce50965
Claudio Wunder <cwunder@gnome.org>
2023-05-24 23:42:26 +0200

    chore: remove unecessary packages improved commands (#5406)
    * chore: remove unecessary packages improved commands
    
    * chore: dependencies only for runtime
    
    * chore: fix concurrently

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,12 +1,8 @@
 import type { StorybookConfig } from '@storybook/nextjs';
 
 const config: StorybookConfig = {
-  stories: ['../components/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-  ],
+  stories: ['../components/**/*.stories.@(ts|tsx)'],
+  addons: ['@storybook/addon-controls', '@storybook/addon-interactions'],
   framework: {
     name: '@storybook/nextjs',
     options: {},
--- scripts/next-data/_helpers.mjs
+++ scripts/next-data/_helpers.mjs
@@ -1,3 +1,5 @@
+'use strict';
+
 import { readdir } from 'fs/promises';
 import { fileURLToPath } from 'url';
 
