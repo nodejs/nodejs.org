cf37e7aaa365e2e90ddc3399f512b5db940c0575
Claudio Wunder <cwunder@hubspot.com>
2023-05-24 22:43:46 +0200

    chore: remove unecessary packages improved commands

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,11 +2,7 @@ import type { StorybookConfig } from '@storybook/nextjs';
 
 const config: StorybookConfig = {
   stories: ['../components/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-  ],
+  addons: ['@storybook/addon-controls', '@storybook/addon-interactions'],
   framework: {
     name: '@storybook/nextjs',
     options: {},
