10e06389ca4ee0ce320525679d9cc78fdc433587
Claudio Wunder <cwunder@hubspot.com>
2023-05-30 13:50:56 +0200

    chore: rename component name

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -4,7 +4,7 @@ import { ThemeProvider } from 'next-themes';
 import { NodeDataProvider } from '../providers/nodeDataProvider';
 import { LocaleProvider } from '../providers/localeProvider';
 import { openSans } from '../util/nextFonts';
-import NextApp, { setAppFont } from '../next.app';
+import BaseApp, { setAppFont } from '../next.app';
 import { pageProps } from './constants';
 
 import '../styles/index.scss';
@@ -31,7 +31,7 @@ setAppFont(openSans.style.fontFamily);
 
 export const decorators = [
   Story => (
-    <NextApp>
+    <BaseApp>
       <ThemeProvider>
         <LocaleProvider>
           <NodeDataProvider nodeVersionData={pageProps.nodeVersionData}>
@@ -41,7 +41,7 @@ export const decorators = [
           </NodeDataProvider>
         </LocaleProvider>
       </ThemeProvider>
-    </NextApp>
+    </BaseApp>
   ),
 ];
 
