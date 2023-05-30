e446fa6b85da032a6d1c6bee79219cb428c864a4
Shanmughapriyan S <priyanshan03@gmail.com>
2023-05-31 02:19:53 +0530

    Update hooks/useRouter.ts
    Co-authored-by: Rodion Abdurakhimov <rodion-arr@users.noreply.github.com>
    Signed-off-by: Shanmughapriyan S <priyanshan03@gmail.com>

--- hooks/useRouter.ts
+++ hooks/useRouter.ts
@@ -3,7 +3,7 @@ import { useRouter as useNextRouter } from 'next/router';
 import * as nextLocales from '../next.locales.mjs';
 import type { NextRouter } from 'next/router';
 
-// Maps all available locales by only their Language COde
+// Maps all available locales by only their Language Code
 const mappedLocalesByCode = nextLocales.availableLocales.map(l => l.code);
 
 export const useRouter = (): NextRouter => {
