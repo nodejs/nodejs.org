f34e7aa2835f69d789f6163a7038d946a7d2fc2d
Claudio Wunder <cwunder@hubspot.com>
2023-06-29 11:02:10 +0200

    feat: ssr sitemap

--- app/en/feed/[feed]/route.ts
+++ app/en/feed/[feed]/route.ts
@@ -9,7 +9,8 @@ type StaticParams = { params: { feed: string } };
 
 // This is the Route Handler for the `GET` method which handles the request
 // for Blog Feeds within the Node.js Website
-export async function GET(_: Request, { params }: StaticParams) {
+// @see https://nextjs.org/docs/app/building-your-application/routing/router-handlers
+export const GET = (_: Request, { params }: StaticParams) => {
   if (params.feed.includes('.xml') && websiteFeeds.has(params.feed)) {
     return new NextResponse(websiteFeeds.get(params.feed)?.rss2(), {
       headers: { 'Content-Type': 'application/xml' },
@@ -17,7 +18,7 @@ export async function GET(_: Request, { params }: StaticParams) {
   }
 
   return new NextResponse(null, { status: 404 });
-}
+};
 
 // This function generates the static paths that come from the dynamic segments
 // `en/feeds/[feed]` and returns an array of all available static paths
@@ -26,3 +27,7 @@ export async function GET(_: Request, { params }: StaticParams) {
 // only if the export is already set for static export
 export const generateStaticParams = () =>
   [...websiteFeeds.keys()].map(feed => ({ feed }));
+
+// Enforces that this route is used as static rendering
+// @see https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config#dynamic
+export const dynamic = 'error';
