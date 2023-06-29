503dbe22efb0193744d754e2f85feb37bc0ca232
Claudio Wunder <cwunder@gnome.org>
2023-06-29 10:59:10 +0200

    fix: fixes feed routes dynamic segments (#5458)

--- app/en/feed/[feed]/route.ts
+++ app/en/feed/[feed]/route.ts
@@ -1,4 +1,3 @@
-import { basename } from 'node:path';
 import { NextResponse } from 'next/server';
 import * as nextJson from '@/next.json.mjs';
 import * as nextData from '@/next-data/index.mjs';
@@ -6,16 +5,24 @@ import * as nextData from '@/next-data/index.mjs';
 // loads all the data from the blog-posts-data.json file
 const websiteFeeds = nextData.generateWebsiteFeeds(nextJson.blogData);
 
-export async function GET(request: Request) {
-  const { pathname } = new URL(request.url);
+type StaticParams = { params: { feed: string } };
 
-  const feed = basename(pathname);
-
-  if (websiteFeeds.has(feed)) {
-    return new NextResponse(websiteFeeds.get(feed)?.rss2(), {
+// This is the Route Handler for the `GET` method which handles the request
+// for Blog Feeds within the Node.js Website
+export async function GET(_: Request, { params }: StaticParams) {
+  if (params.feed.includes('.xml') && websiteFeeds.has(params.feed)) {
+    return new NextResponse(websiteFeeds.get(params.feed)?.rss2(), {
       headers: { 'Content-Type': 'application/xml' },
     });
   }
 
   return new NextResponse(null, { status: 404 });
 }
+
+// This function generates the static paths that come from the dynamic segments
+// `en/feeds/[feed]` and returns an array of all available static paths
+// this is useful for static exports, for example.
+// Note that differently from the App Router these don't get built at the build time
+// only if the export is already set for static export
+export const generateStaticParams = () =>
+  [...websiteFeeds.keys()].map(feed => ({ feed }));
