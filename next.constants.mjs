b3b162c127dc051cc33c45f92d438f3160cfdd10
Claudio Wunder <cwunder@gnome.org>
2023-07-06 11:42:03 +0000

    chore: improved inline code docs

--- next.constants.mjs
+++ next.constants.mjs
@@ -7,6 +7,8 @@ import * as nextLocales from './next.locales.mjs';
  * This is used for telling Next.js if the Website is deployed on Vercel
  *
  * Can be used for conditionally enabling features that we know are Vercel only
+ *
+ * @see https://vercel.com/docs/concepts/projects/environment-variables/system-environment-variables#framework-environment-variables
  */
 export const VERCEL_ENV = process.env.NEXT_PUBLIC_VERCEL_ENV || undefined;
 
@@ -15,15 +17,20 @@ export const VERCEL_ENV = process.env.NEXT_PUBLIC_VERCEL_ENV || undefined;
  *
  * This is used for static/without a Node.js server hosting, such as on our
  * legacy Website Build Environment on Node.js's DigitalOcean Droplet.
+ *
+ * Note that this is a manual Environment Variable defined by us during `npm run deploy`
  */
 export const ENABLE_STATIC_EXPORT =
   process.env.NEXT_STATIC_EXPORT === 'true' ||
   process.env.NEXT_STATIC_EXPORT === true;
 
 /**
- * This is used for any place that requires the full canonical URL path for the Node.js Website (and its deployment)
+ * This is used for any place that requires the full canonical URL path for the Node.js Website (and its deployment), such as for example, the Node.js RSS Feed.
  *
- * Such as for example, the Node.js RSS Feed
+ * This variable can either come from the Vercel Deployment as `NEXT_PUBLIC_VERCEL_URL` or from the `NEXT_BASE_URL` environment variable that is manually defined
+ * by us if necessary. Otherwise it will fallback to the default Node.js Website URL.
+ *
+ * @see https://vercel.com/docs/concepts/projects/environment-variables/system-environment-variables#framework-environment-variables
  */
 export const BASE_URL = process.env.NEXT_PUBLIC_VERCEL_URL
   ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
@@ -34,6 +41,8 @@ export const BASE_URL = process.env.NEXT_PUBLIC_VERCEL_URL
  *
  * This is useful when running the deployment on a subdirectory
  * of a domain, such as when hosted on GitHub Pages.
+ *
+ * Note that this is a manual Environment Variable defined by us if necessary.
  */
 export const BASE_PATH = process.env.NEXT_BASE_PATH || '';
 
