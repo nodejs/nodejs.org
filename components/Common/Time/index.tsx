fbb5a014066318a93ee193154dcd711d5e5e877e
Claudio Wunder <cwunder@gnome.org>
2023-07-09 14:04:38 +0000

    chore: updated time implementation to use formatted-date

--- components/Common/Time/index.tsx
+++ components/Common/Time/index.tsx
@@ -1,27 +1,14 @@
-import { useMemo } from 'react';
-import { useLocale } from '@/hooks/useLocale';
 import type { FC } from 'react';
+import { FormattedDate } from 'react-intl';
 
 type TimeProps = { date: string | Date; format: Intl.DateTimeFormatOptions };
 
 export const Time: FC<TimeProps> = ({ date, format }) => {
-  const { currentLocale } = useLocale();
+  const dateObject = new Date(date);
 
-  const { isoDate, formatedDate } = useMemo(() => {
-    const dateObject = new Date(date);
-
-    return {
-      isoDate: dateObject.toISOString(),
-      formatedDate: dateObject.toLocaleString(currentLocale.hrefLang, {
-        ...format,
-        timeZone: 'UTC',
-      }),
-    };
-    // We assume that the `format` options will not change and be static
-    // and we calculate the date difference by comparing its string value
-    // since its ISO format is what matters for us the most.
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [date.toString(), currentLocale.hrefLang]);
-
-  return <time dateTime={isoDate}>{formatedDate}</time>;
+  return (
+    <time dateTime={dateObject.toISOString()}>
+      <FormattedDate value={dateObject} {...format} timeZone="UTC" />
+    </time>
+  );
 };
