0fada61aa44f67d4c07d1c72e584071290cc4e1b
Umair Raza <umair.raza666@gmail.com>
2023-07-13 01:51:56 +0500

    fix: map directory with alias instead of hard code

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -10,17 +10,11 @@ const config: StorybookConfig = {
   staticDirs: ['../public'],
   core: { disableTelemetry: true },
   webpackFinal: async (config, { configType }) => {
-    config.resolve!.modules = [path.resolve(__dirname, '..'), 'node_modules'];
+    config.resolve!.modules = [resolve(__dirname, '..'), 'node_modules'];
 
     config.resolve!.alias = {
       ...config.resolve!.alias,
-      '@/components': path.resolve(__dirname, '../components'),
-      '@/util': path.resolve(__dirname, '../util'),
-      '@/hooks': path.resolve(__dirname, '../hooks'),
-      '@/navigation.json': path.resolve(__dirname, '../navigation.json'),
-      '@/providers': path.resolve(__dirname, '../providers'),
-      '@/next.locales.mjs': path.resolve(__dirname, '../next.locales.mjs'),
-      '@/next.json.mjs': path.resolve(__dirname, '../next.json.mjs'),
+      '@': resolve(__dirname, '../'),
     };
 
     return config;
