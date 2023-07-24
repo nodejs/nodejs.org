b245caa598bfae9e6bcf2c9d14f786d44a566c79
Shiba Rin <hinatakaho517@gmail.com>
2023-07-24 18:36:52 +0800

    chore: install storybook-addon-module-mock

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,7 +3,11 @@ import type { StorybookConfig } from '@storybook/nextjs';
 
 const config: StorybookConfig = {
   stories: ['../components/**/*.stories.tsx'],
-  addons: ['@storybook/addon-controls', '@storybook/addon-interactions'],
+  addons: [
+    '@storybook/addon-controls',
+    '@storybook/addon-interactions',
+    'storybook-addon-module-mock',
+  ],
   framework: { name: '@storybook/nextjs', options: {} },
   features: { storyStoreV7: true },
   docs: { autodocs: 'tag' },
