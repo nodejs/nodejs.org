d9fccd47913cff13fe32297bc4ff24d03ab75ca9
Claudio Wunder <cwunder@hubspot.com>
2023-07-28 22:36:11 +0200

    chore: optimised test-runner config

--- .storybook/test-runner.ts
+++ .storybook/test-runner.ts
@@ -1,26 +1,35 @@
 import type { TestRunnerConfig } from '@storybook/test-runner';
 
-const STORYBOOK_ELEMENT_ID = '[data-test-id="story-root"]';
+// This is the PlayWright Selector for getting the first Element from
+// the Storybook Root Element. Which allows us to focus directly on the main Element
+const STORYBOOK_ELEMENT_ID = '[data-test-id="story-root"] > *';
 
 const config: TestRunnerConfig = {
   // This method replaces Storybook's Test Runner default `prepare` method
   // This is a lightweight method with extra checks of content loading
   prepare: async ({ page }) => {
+    // We want a small viewport as we don't need something big for our Components
+    await page.setViewportSize({ width: 768, height: 1024 });
+
+    // We get the URL of the current Story from the current process.env variable
     const targetURL = process.env.TARGET_URL;
+
+    // We append the targetURL to the iframeURL and then we navigate to the Iframe
     const iframeURL = new URL('iframe.html', targetURL).toString();
 
+    // Navigates to the Story's Iframe and waits for the page to render
     await page.goto(iframeURL);
 
     // We wait for the document to have its initial looading finished
     await page.waitForLoadState('domcontentloaded');
     await page.waitForLoadState('load');
   },
-  postRender: async page => {
+  postRender: async (page, context) => {
     // This is the minimum time we've noticed for the content to safely be rendered
     await page.waitForTimeout(1000);
 
     // We attempt to get the Storybook root Element
-    const rootElement = await page.locator(STORYBOOK_ELEMENT_ID);
+    const rootElement = await page.$(STORYBOOK_ELEMENT_ID);
 
     // Then we rewrite the inner HTML content
     const content = await rootElement.innerHTML();
