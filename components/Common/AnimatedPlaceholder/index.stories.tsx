4c06e41e1ffe3e0af93d416a41ab77e825ac29a3
Claudio Wunder <cwunder@hubspot.com>
2023-07-28 22:37:28 +0200

    chore: removed broken story

--- components/Common/AnimatedPlaceholder/index.stories.tsx
+++ components/Common/AnimatedPlaceholder/index.stories.tsx
@@ -6,10 +6,4 @@ type Meta = MetaObj<typeof AnimatedPlaceholder>;
 
 export const Default: Story = {};
 
-export const WithLoaderSkeleton: Story = {
-  args: {
-    children: <div className="animated-placeholder__image" />,
-  },
-};
-
 export default { component: AnimatedPlaceholder } as Meta;
