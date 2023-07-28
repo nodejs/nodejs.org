bc7e3e21845c6f3c9d93e18f282a4f37c1f39459
Claudio Wunder <cwunder@hubspot.com>
2023-07-28 13:35:43 +0200

    chore: added new pull request configs and storybook

--- .storybook/test-runner.ts
+++ .storybook/test-runner.ts
@@ -6,7 +6,7 @@ const config: TestRunnerConfig = {
   postRender: async page => {
     // We wait for the page to load for at least one second
     // as there's no reliable way globally to ensure everything loaded correctly
-    await page.waitForTimeout(1000);
+    await page.waitForTimeout(500);
 
     // We attempt to get the Storybook root Element
     const rootElement = await page.locator(STORYBOOK_ELEMENT_ID);
@@ -18,7 +18,7 @@ const config: TestRunnerConfig = {
 
     // We strip the `class` tags from the HTML content as we do not want
     // to pollute the snapshots with generated class names
-    expect(content.replace(/class="(.*?)"/gm, '')).toMatchSnapshot();
+    expect(content.replace(/(class|src|href)="(.*?)"/gm, '')).toMatchSnapshot();
   },
 };
 
