6fe7942041d8fbaf1232f5948f2dfdb3152379b2
Claudio Wunder <cwunder@hubspot.com>
2023-07-28 16:49:36 +0200

    chore: optimised storybook tr config

--- .storybook/test-runner.ts
+++ .storybook/test-runner.ts
@@ -3,11 +3,22 @@ import type { TestRunnerConfig } from '@storybook/test-runner';
 const STORYBOOK_ELEMENT_ID = '[data-test-id="story-root"]';
 
 const config: TestRunnerConfig = {
-  postRender: async page => {
-    // We wait for the page to load for at least one second
-    // as there's no reliable way globally to ensure everything loaded correctly
-    await page.waitForTimeout(1000);
+  // This method replaces Storybook's Test Runner default `prepare` method
+  // This is a lightweight method with extra checks of content loading
+  prepare: async ({ page }) => {
+    const targetURL = process.env.TARGET_URL;
+    const iframeURL = new URL('iframe.html', targetURL).toString();
 
+    await page.goto(iframeURL);
+
+    // We wait for the document to have its initial looading finished
+    await page.waitForLoadState('domcontentloaded');
+    await page.waitForLoadState('load');
+
+    // This is the minimum time we've noticed for the content to safely be rendered
+    await page.waitForTimeout(1000);
+  },
+  postRender: async page => {
     // We attempt to get the Storybook root Element
     const rootElement = await page.locator(STORYBOOK_ELEMENT_ID);
 
@@ -16,8 +27,8 @@ const config: TestRunnerConfig = {
 
     expect(content).toBeDefined();
 
-    // We strip the `class` tags from the HTML content as we do not want
-    // to pollute the snapshots with generated class names
+    // We stripe specific HTML tags whose values are auto-generated or can change from deployment to deployment
+    // this reduces the effectiveness of the stories but it's a trade-off we have to make
     expect(content.replace(/(class|src|href)="(.*?)"/gm, '')).toMatchSnapshot();
   },
 };
