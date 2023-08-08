06a5ae2d96b875806d24fe7a92bbe1a082d2db0a
Claudio Wunder <cwunder@gnome.org>
2023-08-08 23:45:22 +0200

    Update middleware.ts
    Co-authored-by: Steven <steven@ceriously.com>
    Signed-off-by: Claudio Wunder <cwunder@gnome.org>

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 import { availableLocales } from './next.locales.mjs';
 import type { NextRequest } from 'next/server';
 
-// This Middleware is responsible for handling automatic language detection from an user's Browser
+// This Middleware is responsible for handling automatic language detection from a user's Browser
 // This middleware should only run on "/" requests coming to the Website
 export const middleware = async (request: NextRequest) => {
   const { pathname, search } = request.nextUrl;
