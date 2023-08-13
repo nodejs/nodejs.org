b02f81a902f9320e656f78f0a2111294173c3ada
Claudio Wunder <cwunder@gnome.org>
2023-08-13 13:38:25 +0000

    feat: sitemap with alternate hreflangs

--- app/sitemap.xml/route.ts
+++ app/sitemap.xml/route.ts
@@ -1,20 +1,26 @@
 import { getServerSideSitemap } from 'next-sitemap';
-import * as nextDynamic from '@/next.dynamic.mjs';
-import * as nextConstants from '@/next.constants.mjs';
+import { allPaths } from '@/next.dynamic.mjs';
+import { defaultLocale, availableLocales } from '@/next.locales.mjs';
+import {
+  STATIC_ROUTES_IGNORES,
+  DYNAMIC_GENERATED_ROUTES,
+  BASE_PATH,
+  BASE_URL,
+} from '@/next.constants.mjs';
 
 // This is the combination of the Application Base URL and Base PATH
-const canonicalUrl = `${nextConstants.BASE_URL}${nextConstants.BASE_PATH}`;
+const canonicalUrl = `${BASE_URL}${BASE_PATH}`;
 
 // This method populates and generates the Website Sitemap by using `next-sitemap` SSR functionality
 // @see https://nextjs.org/docs/app/building-your-application/routing/router-handlers
 export const GET = () => {
   // Retrieves all the dynamic generated paths
-  const dynamicRoutes = nextConstants.DYNAMIC_GENERATED_ROUTES();
+  const dynamicRoutes = DYNAMIC_GENERATED_ROUTES();
 
-  // Retrieves all the static paths (from next.dynamic.mjs)
-  const staticPaths = [...nextDynamic.allPaths.values()]
-    .flat()
-    .filter(route => nextConstants.STATIC_ROUTES_IGNORES.every(e => !e(route)))
+  // Retrieves all the static paths for the default locale (English)
+  // and filter out the routes that should be ignored
+  const staticPaths = [...allPaths.get(defaultLocale.code)!]
+    .filter(route => STATIC_ROUTES_IGNORES.every(e => !e(route)))
     .map(route => route.routeWithLocale);
 
   // The current date of this request
@@ -25,6 +31,16 @@ export const GET = () => {
       loc: `${canonicalUrl}/${route}`,
       lastmod: currentDate,
       changefreq: 'always',
+      // We build the alternate languages based on the source pages
+      // This allows Google to correctly index these pages
+      alternateRefs: availableLocales.map(locale => ({
+        hreflang: locale.code,
+        hrefIsAbsolute: true,
+        href: `${canonicalUrl}/${route.replace(
+          `${defaultLocale.code}/`,
+          `${locale.code}/`
+        )}`,
+      })),
     }))
   );
 };
