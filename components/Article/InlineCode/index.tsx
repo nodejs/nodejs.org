3b42c765d148fe9e0b1c9081c5aed921ac3bf6da
Guilherme Araújo <arauujogui@gmail.com>
2023-08-28 11:28:31 -0300

    feat: remove inlineCode component

--- components/Article/InlineCode/index.tsx
+++
@@ -1,8 +0,0 @@
-import styles from './index.module.scss';
-import type { FC, PropsWithChildren } from 'react';
-
-const InlineCode: FC<PropsWithChildren> = ({ children }) => (
-  <code className={styles.code}>{children}</code>
-);
-
-export default InlineCode;
