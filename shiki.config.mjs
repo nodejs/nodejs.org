e2f8a21a565f71e77d463dee8ea2c906ebe09c4a
Claudio Wunder <cwunder@hubspot.com>
2023-09-01 13:07:02 +0200

    chore: manuall import grammar

--- shiki.config.mjs
+++ shiki.config.mjs
@@ -1,49 +1,58 @@
 'use strict';
 
+import cLanguage from 'shiki/languages/c.tmLanguage.json' assert { type: 'json' };
+import cppLanguage from 'shiki/languages/cpp.tmLanguage.json' assert { type: 'json' };
+import javaScriptLanguage from 'shiki/languages/javascript.tmLanguage.json' assert { type: 'json' };
+import jsonLanguage from 'shiki/languages/json.tmLanguage.json' assert { type: 'json' };
+import jsxLanguage from 'shiki/languages/jsx.tmLanguage.json' assert { type: 'json' };
+import typeScriptLanguage from 'shiki/languages/typescript.tmLanguage.json' assert { type: 'json' };
+import xmlLanguage from 'shiki/languages/xml.tmLanguage.json' assert { type: 'json' };
+import yamlLanguage from 'shiki/languages/yaml.tmLanguage.json' assert { type: 'json' };
+
 /** @type {import('shiki').ILanguageRegistration[]} */
 export const SUPPORTED_LANGUAGES = [
   {
     id: 'c',
     scopeName: 'source.c',
-    path: require.resolve('shiki/languages/c.tmLanguage.json'),
+    grammar: cLanguage,
   },
   {
     id: 'cpp',
     scopeName: 'source.cpp',
     embeddedLangs: ['sql'],
-    path: require.resolve('shiki/languages/cpp.tmLanguage.json'),
+    grammar: cppLanguage,
   },
   {
     id: 'javascript',
     scopeName: 'source.js',
     aliases: ['js'],
-    path: require.resolve('shiki/languages/javascript.tmLanguage.json'),
+    grammar: javaScriptLanguage,
   },
   {
     id: 'json',
     scopeName: 'source.json',
-    path: require.resolve('shiki/languages/json.tmLanguage.json'),
+    grammar: jsonLanguage,
   },
   {
     id: 'jsx',
     scopeName: 'source.js.jsx',
-    path: require.resolve('shiki/languages/jsx.tmLanguage.json'),
+    grammar: jsxLanguage,
   },
   {
     id: 'typescript',
     scopeName: 'source.ts',
     aliases: ['ts'],
-    path: require.resolve('shiki/languages/typescript.tmLanguage.json'),
+    grammar: typeScriptLanguage,
   },
   {
     id: 'xml',
     scopeName: 'text.xml',
     embeddedLangs: ['java'],
-    path: require.resolve('shiki/languages/xml.tmLanguage.json'),
+    grammar: xmlLanguage,
   },
   {
     id: 'yaml',
     scopeName: 'source.yaml',
-    path: require.resolve('shiki/languages/yaml.tmLanguage.json'),
+    grammar: yamlLanguage,
   },
 ];
