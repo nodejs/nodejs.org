b24676278c8250342560f835fbd1602758c63fe5
Claudio Wunder <cwunder@hubspot.com>
2023-09-05 11:34:09 +0200

    chore: remove remainder old components

--- components/Article/Alert/index.tsx
+++
@@ -1,8 +0,0 @@
-import styles from './index.module.scss';
-import type { FC, PropsWithChildren } from 'react';
-
-const Alert: FC<PropsWithChildren> = ({ children }) => (
-  <div className={styles.alert}>{children}</div>
-);
-
-export default Alert;
