18a4ad3dee36db79ca8065dddcee3b351758e342
Claudio Wunder <cwunder@hubspot.com>
2023-09-05 11:18:32 +0200

    meta: cleaned up design components

--- components/design/color.stories.tsx
+++
@@ -1,9 +0,0 @@
-import BrandColor from './color';
-import type { Meta as MetaObj, StoryObj } from '@storybook/react';
-
-type Story = StoryObj<typeof BrandColor>;
-type Meta = MetaObj<typeof BrandColor>;
-
-export const Default: Story = {};
-
-export default { component: BrandColor } as Meta;
--- components/design/text.stories.tsx
+++
@@ -1,9 +0,0 @@
-import Text from './text';
-import type { Meta as MetaObj, StoryObj } from '@storybook/react';
-
-type Story = StoryObj<typeof Text>;
-type Meta = MetaObj<typeof Text>;
-
-export const Default: Story = {};
-
-export default { component: Text } as Meta;
