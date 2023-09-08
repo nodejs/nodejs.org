4202b3626ee61c43e84d584c0509f91b018d2039
Augustin Mauroy <augustin.mauroy@outlook.fr>
2023-09-08 16:35:25 +0200

    feat(banner): introduce new design
    related issue: https://github.com/nodejs/nodejs.org/issues/5760

---
+++ components/Common/Banner/index.stories.tsx
@@ -0,0 +1,31 @@
+import type { Meta as MetaObj, StoryObj } from '@storybook/react';
+import Banner from './';
+
+type Story = StoryObj<typeof Banner>;
+type Meta = MetaObj<typeof Banner>;
+
+export const Default: Story = {
+  args: {
+    text: 'Nodejs collaborator summitNode.js Collaborator Summit 2023 - Bilbao, Spain (OpenJS World EU) 2023',
+    type: 'default',
+    url: 'https://github.com/openjs-foundation/summit/issues/360',
+  },
+};
+
+export const Error: Story = {
+  args: {
+    text: 'STOP creating issue for error 500 on download',
+    type: 'error',
+    url: 'https://github.com/nodejs/nodejs.org/issues/4495',
+  },
+};
+
+export const Warning: Story = {
+  args: {
+    text: 'STOP creating issue for error 500 on download',
+    type: 'warning',
+    url: 'https://github.com/nodejs/nodejs.org/issues/4495',
+  },
+};
+
+export default { component: Banner } as Meta;
