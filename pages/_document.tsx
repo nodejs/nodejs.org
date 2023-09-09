9a075a48d0347118690f8aa6840624a5ce30785b
Augustin Mauroy <augustin.mauroy@outlook.fr>
2023-09-09 11:07:38 +0200

    fix(SEO): html lang

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,13 @@
 import Script from 'next/script';
 import { Html, Head, Main, NextScript } from 'next/document';
 import { LEGACY_JAVASCRIPT_FILE } from '@/next.constants.mjs';
+import type { DocumentProps } from 'next/document';
 
-const Document = () => (
-  <Html>
+const Document = ({ __NEXT_DATA__ }: DocumentProps) => (
+  <Html lang={__NEXT_DATA__.query.path ? __NEXT_DATA__.query.path[0] : 'en'}>
     <Head />
     <body>
       <Main />
-
       <NextScript />
 
       <Script strategy="beforeInteractive" src={LEGACY_JAVASCRIPT_FILE} />
