465091a4bffb3061af148ba757b2156fb3cd30f0
Caner Akdas <canerakdas@gmail.com>
2023-09-11 22:01:47 +0300

    refactor: classnames replaced with template literals

--- components/Common/Badge/index.tsx
+++ components/Common/Badge/index.tsx
@@ -1,10 +1,7 @@
-import ArrowRightIcon from '@heroicons/react/24/solid/ArrowRightIcon';
 import type { ComponentProps, FC, PropsWithChildren } from 'react';
 import type Link from 'next/link';
-
+import ArrowRightIcon from '@heroicons/react/24/solid/ArrowRightIcon';
 import LocalizedLink from '@/components/LocalizedLink';
-import classNames from 'classnames';
-
 import styles from './index.module.scss';
 
 type BadgeProps = {
@@ -19,7 +16,7 @@ const Badge: FC<PropsWithChildren<BadgeProps>> = ({
   ...args
 }) => {
   return (
-    <LocalizedLink className={classNames(styles.badge, styles[kind])} {...args}>
+    <LocalizedLink className={`${styles.badge} ${styles[kind]}`} {...args}>
       {badgeText && <span className={styles.text}>{badgeText}</span>}
       <span className={styles.message}>{children}</span>
       <ArrowRightIcon className={styles.icon} />
