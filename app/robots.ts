dfe6daff951606d4b07edd312b463dd178d6bc3c
Brian Muenzenmeyer <brian.muenzenmeyer@gmail.com>
2023-09-19 16:55:51 -0500

    Update app/robots.ts
    Signed-off-by: Brian Muenzenmeyer <brian.muenzenmeyer@gmail.com>

--- app/robots.ts
+++ app/robots.ts
@@ -1,6 +1,6 @@
 import type { MetadataRoute } from 'next';
 
-// This allows us to generate a `robots.txt` file dynamically based on the needs of the Noe.js Website
+// This allows us to generate a `robots.txt` file dynamically based on the needs of the Node.js Website
 // @see https://nextjs.org/docs/app/api-reference/file-conventions/metadata/robots
 const robots = (): MetadataRoute.Robots => ({
   rules: [
