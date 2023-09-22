4d934b9790ee59cc7408fa1805b35eed8cdf2083
Shristi Suman <shristi@Shristis-MacBook-Air.local>
2023-09-22 15:07:03 -0700

    Added platform placeholder

---
+++ components/__design__/platform-logos.stories.tsx
@@ -0,0 +1,22 @@
+import Image from 'next/image';
+import type { Meta as MetaObj, StoryObj } from '@storybook/react';
+
+export const PlatformLogos: StoryObj = {};
+
+export default {
+  title: 'Design System/Logos',
+  component: () => {
+    return (
+      <div>
+        <div>
+          <Image
+            src="/static/images/logos/platform-placeholder.svg"
+            alt="Placeholder Logo"
+            width={64}
+            height={64}
+          />
+        </div>
+      </div>
+    );
+  },
+} as MetaObj;
