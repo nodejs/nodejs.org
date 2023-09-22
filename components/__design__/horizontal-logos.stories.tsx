daccaa9931d2c75846d5048393295f48c75a78da
Niya Shiyas <niyashiyas03@gmail.com>
2023-09-23 03:16:20 +0530

    chore: Added horizontal-logo-light and Storybook, #issue 27

---
+++ components/__design__/horizontal-logos.stories.tsx
@@ -0,0 +1,29 @@
+import Image from 'next/image';
+import type { Meta as MetaObj, StoryObj } from '@storybook/react';
+
+export const HorizontalLogos: StoryObj = {};
+
+export default {
+  title: 'Design System/Logos',
+  component: () => {
+    return (
+    <div>
+      <div>
+        <Image
+            src={`/static/images/logos/horizontal-light.svg`}
+            alt="Node.js"
+            width={267}
+            height={80}
+          />
+      </div>
+      <div>
+        <Image
+            src={`/static/images/logos/horizontal-dark.svg`}
+            alt="Node.js"
+            width={267}
+            height={80}
+          />
+      </div>
+    </div>
+  )},
+} as MetaObj;
