c2f2f255aa2dd8e5150e2b64ccf6b795638a98c1
sridevi <sridevi.sn>
2023-09-22 14:24:18 -0700

    chore:Added social icon LinkedIn #40

---
+++ components/__design__/social-logos.stories.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image';
+import type { Meta as MetaObj, StoryObj } from '@storybook/react';
+
+export const SocialLogos: StoryObj = {};
+
+export default {
+  title: 'Design System/Logos',
+  component: () => {
+    return (
+    <div>
+      <div>
+        <Image
+            src="/static/images/logos/social-linkedin.svg"
+            alt="LinkedIn Logo"
+            width={64}
+            height={64}
+          />
+      </div>
+    </div>
+  )},
+} as MetaObj;
