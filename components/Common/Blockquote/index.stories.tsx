a4bf182e3d70a2451836c2d951e62cbf2eb3702c
Caner Akdas <canerakdas@gmail.com>
2023-09-24 02:18:51 +0300

    refactor: attribution prop replaced with an HTML cite child

--- components/Common/Blockquote/index.stories.tsx
+++ components/Common/Blockquote/index.stories.tsx
@@ -7,13 +7,14 @@ type Meta = MetaObj<typeof Blockquote>;
 export const Default: Story = {
   args: {
     children: (
-      <p>
-        “Words can be like X-rays, if you use them properly—they’ll go through
-        anything. You read and you’re pierced.”
-      </p>
+      <>
+        <p>
+          “An expert is a person who has made all the mistakes that can be made
+          in a very narrow field.”
+        </p>
+        <cite>Niels Bohr</cite>
+      </>
     ),
-    cite: 'https://www.huxley.net/bnw/four.html',
-    attribution: 'Aldous Huxley, Brave New World',
   },
 };
 
@@ -21,8 +22,10 @@ export const Without_Attribution: Story = {
   args: {
     children: (
       <p>
-        “Words can be like X-rays, if you use them properly—they’ll go through
-        anything. You read and you’re pierced.”
+        “Commander Keen soared through the stars, leaving behind a galaxy of
+        memories, adventure, and boundless imagination. In the realm of gaming,
+        his farewell echoes as a timeless voyage into the hearts of those who
+        dare to dream and explore the unknown.”
       </p>
     ),
   },
@@ -36,17 +39,17 @@ export const Nested: Story = {
           “Words can be like X-rays, if you use them properly—they’ll go through
           anything. You read and you’re pierced.”
         </p>
-        <Blockquote attribution="Nested Blockquote, Storybook">
+        <Blockquote>
           <p>
-            It showed her glimpses of the past, revealing that the anomaly had
-            been triggered by an experiment gone awry—a desperate attempt to
-            harness unlimited energy.
+            “A thinker sees his own actions as experiments and questions--as
+            attempts to find out something. Success and failure are for him
+            answers above all.”
           </p>
+          <cite>Friedrich Nietzsche</cite>
         </Blockquote>
+        <cite>Aldous Huxley, Brave New World</cite>
       </>
     ),
-    cite: 'https://www.huxley.net/bnw/four.html',
-    attribution: 'Aldous Huxley, Brave New World',
   },
 };
 
--- components/Common/Blockquote/index.tsx
+++ components/Common/Blockquote/index.tsx
@@ -2,22 +2,14 @@ import type { ComponentProps, FC, PropsWithChildren } from 'react';
 
 import styles from './index.module.scss';
 
-type BlockquoteProps = {
-  attribution?: string;
-} & ComponentProps<'blockquote'>;
+type BlockquoteProps = ComponentProps<'blockquote'>;
 
 const Blockquote: FC<PropsWithChildren<BlockquoteProps>> = ({
   children,
   cite,
-  attribution,
 }) => (
   <blockquote className={styles.wrapper} cite={cite}>
     {children}
-    {attribution && (
-      <footer>
-        <cite>{attribution}</cite>
-      </footer>
-    )}
   </blockquote>
 );
 
