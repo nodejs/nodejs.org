23638817b3a466acf283069bb980eb36700dca31
Umair Raza <umair.raza666@gmail.com>
2023-09-23 14:22:41 +0400

    feat(button): extract backdrop glow to a mixin

--- components/Common/Button/index.stories.tsx
+++ components/Common/Button/index.stories.tsx
@@ -1,6 +1,5 @@
 import type { Meta as MetaObj, StoryObj } from '@storybook/react';
 import Button from './';
-import classNames from 'classnames';
 
 type Story = StoryObj<typeof Button>;
 type Meta = MetaObj<typeof Button>;
@@ -11,18 +10,5 @@ export const Default: Story = {
     children: 'Download Node (LTS)',
     disabled: false,
   },
-  decorators: [
-    (Story, { args }) => (
-      <>
-        <div
-          className={classNames({
-            'bg-[#000000] w-fit rounded-md': args.variant === 'special',
-          })}
-        >
-          <Story />
-        </div>
-      </>
-    ),
-  ],
 };
 export default { component: Button } as Meta;
