1773006be9059c3bfbb06dcf69fa14c40b93893c
Claudio W <cwunder@gnome.org>
2023-09-24 13:37:37 +0000

    chore: improved viewports vars and styles

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -3,22 +3,16 @@ import { withThemeByDataAttribute } from '@storybook/addon-themes';
 import { SiteProvider } from '../providers/siteProvider';
 import { ThemeProvider } from '../providers/themeProvider';
 import { LocaleProvider } from '../providers/localeProvider';
-import { OPEN_SANS_FONT, STORYBOOK_MODES, COMMON_CLASSES } from './constants';
+import { OPEN_SANS_FONT, STORYBOOK_MODES, STORYBOOK_SIZES } from './constants';
 import type { Preview, ReactRenderer } from '@storybook/react';
 
 import '../styles/new/index.css';
 
 const preview: Preview = {
   parameters: {
-    viewport: {
-      defaultViewport: 'large',
-      viewports: {
-        small: { name: 'Small', styles: { width: '414px', height: '896px' } },
-        large: { name: 'Large', styles: { width: '1024px', height: '768px' } },
-      },
-    },
     nextjs: { router: { basePath: '' } },
     chromatic: { modes: STORYBOOK_MODES },
+    viewport: { defaultViewport: 'large', viewports: STORYBOOK_SIZES },
   },
   // These are extra Storybook Decorators applied to all stories
   // that introduce extra functionality such as Theme Switching
@@ -28,7 +22,7 @@ const preview: Preview = {
       <SiteProvider>
         <LocaleProvider>
           <ThemeProvider>
-            <div className={`${OPEN_SANS_FONT.variable} ${COMMON_CLASSES}`}>
+            <div className={`${OPEN_SANS_FONT.variable}`}>
               <Story />
             </div>
           </ThemeProvider>
