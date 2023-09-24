52abf1c9c42caf53c0555fa118d1a2a29d011296
Pooja Patel <87775823+Pooja-Codes@users.noreply.github.com>
2023-09-24 16:55:12 -0400

    Added green hex logo svg (#5859)
    * Altered JS green svg logo
    
    * Added JS logos stories component
    
    * Added hex green logo svg file
    
    * Added hex logos stories component
    
    * Fixed commited not belonged changes

---
+++ components/__design__/hex-logos.stories.tsx
@@ -0,0 +1,38 @@
+import Image from 'next/image';
+import type { Meta as MetaObj, StoryObj } from '@storybook/react';
+
+export const HexLogos: StoryObj = {};
+
+export default {
+  title: 'Design System/Logos',
+  component: () => {
+    return (
+      <div>
+        <div>
+          <Image
+            src={`/static/images/logos/hex-white.svg`}
+            alt="Node.js"
+            width={64}
+            height={64}
+          />
+        </div>
+        <div>
+          <Image
+            src={`/static/images/logos/hex-green.svg`}
+            alt="Node.js"
+            width={64}
+            height={64}
+          />
+        </div>
+        <div>
+          <Image
+            src={`/static/images/logos/hex-black.svg`}
+            alt="Node.js"
+            width={64}
+            height={64}
+          />
+        </div>
+      </div>
+    );
+  },
+} as MetaObj;
