38e4b7f71717e4a4c88224554cfcc212860250ff
Caner Akdas <canerakdas@gmail.com>
2023-09-25 01:34:51 +0300

    refactor: text stylings moved to wrapper

--- components/Common/Blockquote/index.stories.tsx
+++ components/Common/Blockquote/index.stories.tsx
@@ -8,10 +8,8 @@ export const Default: Story = {
   args: {
     children: (
       <>
-        <p>
-          “An expert is a person who has made all the mistakes that can be made
-          in a very narrow field.”
-        </p>
+        “An expert is a person who has made all the mistakes that can be made in
+        a very narrow field.”
         <cite>Niels Bohr</cite>
       </>
     ),
@@ -20,14 +18,8 @@ export const Default: Story = {
 
 export const Without_Attribution: Story = {
   args: {
-    children: (
-      <p>
-        “Commander Keen soared through the stars, leaving behind a galaxy of
-        memories, adventure, and boundless imagination. In the realm of gaming,
-        his farewell echoes as a timeless voyage into the hearts of those who
-        dare to dream and explore the unknown.”
-      </p>
-    ),
+    children:
+      '“Commander Keen soared through the stars, leaving behind a galaxy of memories, adventure, and boundless imagination. In the realm of gaming, his farewell echoes as a timeless voyage into the hearts of those who dare to dream and explore the unknown.”',
   },
 };
 
@@ -35,16 +27,12 @@ export const Nested: Story = {
   args: {
     children: (
       <>
-        <p>
-          “Words can be like X-rays, if you use them properly—they’ll go through
-          anything. You read and you’re pierced.”
-        </p>
+        “Words can be like X-rays, if you use them properly—they’ll go through
+        anything. You read and you’re pierced.”
         <Blockquote>
-          <p>
-            “A thinker sees his own actions as experiments and questions--as
-            attempts to find out something. Success and failure are for him
-            answers above all.”
-          </p>
+          “A thinker sees his own actions as experiments and questions--as
+          attempts to find out something. Success and failure are for him
+          answers above all.”
           <cite>Friedrich Nietzsche</cite>
         </Blockquote>
         <cite>Aldous Huxley, Brave New World</cite>
