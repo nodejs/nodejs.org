72d9e0eacd5200d106d253f938f86de839ef0677
Claudio Wunder <cwunder@hubspot.com>
2023-09-28 16:32:01 +0200

    refactor: code review changes

--- components/Common/Button/index.stories.tsx
+++ components/Common/Button/index.stories.tsx
@@ -4,11 +4,28 @@ import Button from './';
 type Story = StoryObj<typeof Button>;
 type Meta = MetaObj<typeof Button>;
 
-export const Default: Story = {
+export const Primary: Story = {
   args: {
     variant: 'primary',
     children: 'Download Node (LTS)',
     disabled: false,
   },
 };
+
+export const Secondary: Story = {
+  args: {
+    variant: 'secondary',
+    children: 'Download Node (LTS)',
+    disabled: false,
+  },
+};
+
+export const Special: Story = {
+  args: {
+    variant: 'special',
+    children: 'Download Node (LTS)',
+    disabled: false,
+  },
+};
+
 export default { component: Button } as Meta;
