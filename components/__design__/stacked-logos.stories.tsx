9fb23abed6d67f1d8b5ba4da76d285fea219d8e6
Niya Shiyas <98641481+niyashiyas@users.noreply.github.com>
2023-09-29 17:52:53 +0530

    chore: added stacked-black and updated stacked-logos.stories.tsx
    Signed-off-by: Niya Shiyas <98641481+niyashiyas@users.noreply.github.com>

--- components/__design__/stacked-logos.stories.tsx
+++ components/__design__/stacked-logos.stories.tsx
@@ -4,42 +4,37 @@ import type { Meta as MetaObj, StoryObj } from '@storybook/react';
 export const StackedLogos: StoryObj = {};
 
 export default {
-  title: 'Design System/Logos',
+  title: 'Design System/Stacked Logos',
   component: () => {
     return (
-    <div>
       <div>
-        <Image
-            src={`/static/images/logos/stacked-white.svg`}
-            alt="Node.js"
-            width={267}
-            height={80}
-          />
-      </div>
-      <div>
-        <Image
-            src={`/static/images/logos/stacked-light.svg`}
-            alt="Node.js"
-            width={267}
-            height={80}
-          />
-      </div>
-      <div>
-        <Image
-            src={`/static/images/logos/stacked-black.svg`}
-            alt="Node.js"
-            width={267}
-            height={80}
-          />
-      </div>
-      <div>
-        <Image
+        <div>
+          <Image
             src={`/static/images/logos/stacked-dark.svg`}
             alt="Node.js"
             width={267}
             height={80}
           />
+          <Image
+            src={`/static/images/logos/stacked-black.svg`}
+            alt="Node.js"
+            width={267}
+            height={80}
+          />
+          <Image
+            src={`/static/images/logos/stacked-white.svg`}
+            alt="Node.js"
+            width={267}
+            height={80}
+          />
+          <Image
+            src={`/static/images/logos/stacked-light.svg`}
+            alt="Node.js"
+            width={267}
+            height={80}
+          />
+        </div>
       </div>
-    </div>
-  )},
+    );
+  },
 } as MetaObj;
