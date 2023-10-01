bbf5913deaa01aa3a9b5a08c97485378d6309789
Caner Akdas <canerakdas@gmail.com>
2023-10-01 03:16:56 +0300

    feat: Select component

---
+++ components/Common/Select/index.stories.tsx
@@ -0,0 +1,40 @@
+import Select from '.';
+import type { Meta as MetaObj, StoryObj } from '@storybook/react';
+
+type Story = StoryObj<typeof Select>;
+type Meta = MetaObj<typeof Select>;
+
+export const Default: Story = {
+  args: {
+    values: ['v20.8.0', 'v19.9.0', 'v18.18.0', 'v17.9.1', 'v16.20.2'],
+    defaultValue: 'v16.20.2',
+    label: 'Node.js version',
+  },
+};
+
+export const Withouth_Label: Story = {
+  args: {
+    values: ['v20.8.0', 'v19.9.0', 'v18.18.0', 'v17.9.1', 'v16.20.2'],
+    defaultValue: 'v16.20.2',
+  },
+};
+
+export const Dropdown_Label: Story = {
+  args: {
+    values: [
+      'Introduction to Node.js',
+      'How to install Node.js',
+      'How much JavaScript do you need to know to use Node.js?',
+      'Differences between Node.js and the Browser',
+      'The V8 JavaScript Engine',
+      'An introduction to the NPM package manager',
+      'ECMAScript 2015 (ES6) and beyond',
+      'Node.js, the difference between development and production',
+    ],
+    label: 'Getting Started',
+    dropdownLabel: 'Getting Started',
+    placeholder: 'Select a guide',
+  },
+};
+
+export default { component: Select } as Meta;
---
+++ components/Common/Select/index.tsx
@@ -0,0 +1,77 @@
+import * as Primitive from '@radix-ui/react-select';
+import { useId } from 'react';
+import { randomBytes } from 'crypto';
+import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/24/outline';
+import type { FC } from 'react';
+
+import styles from './index.module.css';
+
+type SelectProps = {
+  values: string[];
+  defaultValue?: string;
+  placeholder?: string;
+  dropdownLabel?: string;
+  label?: string;
+  onChange?: (value: string) => void;
+};
+
+const Select: FC<SelectProps> = ({
+  values = [],
+  defaultValue,
+  placeholder,
+  label,
+  dropdownLabel,
+  onChange,
+}) => {
+  const id = useId();
+
+  return (
+    <div className={styles.select}>
+      {label && (
+        <label className={styles.label} htmlFor={id}>
+          {label}
+        </label>
+      )}
+      <Primitive.Root defaultValue={defaultValue} onValueChange={onChange}>
+        <Primitive.Trigger
+          className={styles.trigger}
+          aria-label={label}
+          id={id}
+        >
+          <Primitive.Value placeholder={placeholder} />
+          <ChevronDownIcon className={styles.icon} />
+        </Primitive.Trigger>
+        <Primitive.Portal>
+          <Primitive.Content className={styles.dropdown}>
+            <Primitive.ScrollUpButton className={styles.button}>
+              <ChevronUpIcon className={styles.icon} />
+            </Primitive.ScrollUpButton>
+            <Primitive.Viewport>
+              <Primitive.Group>
+                {dropdownLabel && (
+                  <Primitive.Label className={`${styles.item} ${styles.label}`}>
+                    {dropdownLabel}
+                  </Primitive.Label>
+                )}
+                {values.map(value => (
+                  <Primitive.Item
+                    key={randomBytes(16).toString('hex')}
+                    value={value}
+                    className={`${styles.item} ${styles.text}`}
+                  >
+                    <Primitive.ItemText>{value}</Primitive.ItemText>
+                  </Primitive.Item>
+                ))}
+              </Primitive.Group>
+            </Primitive.Viewport>
+            <Primitive.ScrollDownButton className={styles.button}>
+              <ChevronUpIcon className={styles.icon} />
+            </Primitive.ScrollDownButton>
+          </Primitive.Content>
+        </Primitive.Portal>
+      </Primitive.Root>
+    </div>
+  );
+};
+
+export default Select;
