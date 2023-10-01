081cf42ae8a917d7b0f4a28133d707036a2af9f7
Augustin Mauroy <augustin.mauroy@outlook.fr>
2023-10-01 12:13:12 +0200

    update

--- components/Common/AvatarGroup/index.tsx
+++ components/Common/AvatarGroup/index.tsx
@@ -1,25 +1,15 @@
 'use client';
-import * as Avatar from '@radix-ui/react-avatar';
-import { useState } from 'react';
 import type { FC } from 'react';
+import { useState, useMemo } from 'react';
+import Avatar from './Avatar';
 import styles from './index.module.css';
 
 type AvatarGroupProps = {
   avatars: {
     src: string;
-    name: string;
+    alt?: string;
   }[];
   limit?: number;
-};
-
-const userNameToFallBackText = (name: string) => {
-  // sanitize name before using it
-  name = name.toLowerCase().replace(' ', '-');
-  const words = name.split('-');
-  if (words.length > 1) {
-    return words[0][0] + words[1][0];
-  }
-  return name.slice(0, 2);
 };
 
 const AvatarGroup: FC<AvatarGroupProps> = ({ avatars, limit = 10 }) => {
@@ -29,31 +19,27 @@ const AvatarGroup: FC<AvatarGroupProps> = ({ avatars, limit = 10 }) => {
     setShowMore(!showMore);
   };
 
-  console.log('avatars', avatars);
-
-  const renderAvatars = showMore ? avatars : avatars.slice(0, limit);
+  const renderAvatars = useMemo(
+    () => (showMore ? avatars : avatars.slice(0, limit)),
+    [showMore, avatars, limit]
+  );
 
   return (
     <div className={styles.avatarGroup}>
       {renderAvatars.map((avatar, index) => (
-        <Avatar.Root key={index} className={styles.avatarRoot}>
-          <Avatar.Image
-            src={avatar.src}
-            alt={avatar.name}
-            className={styles.avatar}
-          />
-          <Avatar.Fallback className={styles.avatar}>
-            {userNameToFallBackText(avatar.name)}
-          </Avatar.Fallback>
-        </Avatar.Root>
+        <Avatar
+          src={avatar.src}
+          alt={avatar.alt}
+          extraClass={styles.avatarRoot}
+          key={index}
+        />
       ))}
       {avatars.length > limit && (
-        // added span to "emulate" Avatar.Root
-        <span className={styles.avatarRoot}>
-          <span className={styles.avatar} onClick={toggleShowMore}>
-            {showMore ? '-' : `+${avatars.length - limit}`}
-          </span>
-        </span>
+        <Avatar
+          extraClass={styles.avatarRoot}
+          alt={showMore ? '-' : `+${avatars.length - limit}`}
+          onClick={toggleShowMore}
+        />
       )}
     </div>
   );
