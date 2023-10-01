fb6e8d6d760a17cb1e985e9c11e618beca60a8da
junwen-k <40173716+junwen-k@users.noreply.github.com>
2023-10-01 23:16:41 +0800

    refactor(Breadcrumbs): improve code readability

--- components/Common/Breadcrumbs/index.tsx
+++ components/Common/Breadcrumbs/index.tsx
@@ -24,29 +24,30 @@ const Breadcrumbs: FC<BreadcrumbsProps> = ({
   maxLength = 3,
   hideHome = false,
 }) => {
-  const items = useMemo(
-    () =>
-      (links.length > maxLength
-        ? links.slice(links.length - maxLength)
-        : links
-      ).map((link, index, items) => {
-        const isLastItem = index === items.length - 1;
-        return (
-          <BreadcrumbItem key={link.href.toString()} hideSeparator={isLastItem}>
-            <LocalizedLink
-              href={link.href}
-              className={classNames({
-                [styles.active]: isLastItem,
-              })}
-              aria-current={isLastItem ? 'page' : undefined}
-            >
-              {link.label}
-            </LocalizedLink>
-          </BreadcrumbItem>
-        );
-      }),
-    [links, maxLength]
-  );
+  const items = useMemo(() => {
+    const itemsToRender = links.slice(
+      links.length > maxLength ? -maxLength : 0
+    );
+
+    return itemsToRender.map((link, index, items) => {
+      const isLastItem = index === items.length - 1;
+
+      return (
+        <BreadcrumbItem key={link.href.toString()} hideSeparator={isLastItem}>
+          <LocalizedLink
+            href={link.href}
+            className={classNames({
+              [styles.active]: isLastItem,
+            })}
+            aria-current={isLastItem ? 'page' : undefined}
+          >
+            {link.label}
+          </LocalizedLink>
+        </BreadcrumbItem>
+      );
+    });
+  }, [links, maxLength]);
+
   return (
     <BreadcrumbRoot hideHome={hideHome}>
       {links.length > maxLength && (
