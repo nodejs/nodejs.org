b3180683b4d49429d4f3363781615d1eaab05893
junwen-k <40173716+junwen-k@users.noreply.github.com>
2023-10-02 17:29:56 +0800

    feat(Breadcrumbs): use intl for home aria label

--- components/Common/Breadcrumbs/BreadcrumbRoot/index.tsx
+++ components/Common/Breadcrumbs/BreadcrumbRoot/index.tsx
@@ -1,5 +1,6 @@
 import HomeIcon from '@heroicons/react/24/outline/HomeIcon';
 import type { FC, PropsWithChildren } from 'react';
+import { useIntl } from 'react-intl';
 
 import BreadcrumbItem from '@/components/Common/Breadcrumbs/BreadcrumbItem';
 import LocalizedLink from '@/components/LocalizedLink';
@@ -13,32 +14,44 @@ type BreadcrumbRootProps = {
 const BreadcrumbRoot: FC<PropsWithChildren<BreadcrumbRootProps>> = ({
   hideHome = false,
   children,
-}) => (
-  <nav aria-label="breadcrumb">
-    <ol
-      itemScope
-      itemType="https://schema.org/BreadcrumbList"
-      className={styles.list}
-    >
-      {!hideHome && (
-        <BreadcrumbItem>
-          <LocalizedLink
-            itemScope
-            itemType="http://schema.org/Thing"
-            itemProp="item"
-            itemID="/"
-            href="/"
-          >
-            <span itemProp="name">
-              <HomeIcon aria-label="Home" className={styles.icon} />
-            </span>
-            <meta itemProp="position" content="1" />
-          </LocalizedLink>
-        </BreadcrumbItem>
-      )}
-      {children}
-    </ol>
-  </nav>
-);
+}) => {
+  const { formatMessage } = useIntl();
+
+  const navigateToHome = formatMessage({
+    id: 'components.common.breadcrumbs.navigateToHome',
+  });
+
+  return (
+    <nav aria-label="breadcrumb">
+      <ol
+        itemScope
+        itemType="https://schema.org/BreadcrumbList"
+        className={styles.list}
+      >
+        {!hideHome && (
+          <BreadcrumbItem>
+            <LocalizedLink
+              itemScope
+              itemType="http://schema.org/Thing"
+              itemProp="item"
+              itemID="/"
+              href="/"
+            >
+              <span itemProp="name">
+                <HomeIcon
+                  title={navigateToHome}
+                  aria-label={navigateToHome}
+                  className={styles.icon}
+                />
+              </span>
+              <meta itemProp="position" content="1" />
+            </LocalizedLink>
+          </BreadcrumbItem>
+        )}
+        {children}
+      </ol>
+    </nav>
+  );
+};
 
 export default BreadcrumbRoot;
