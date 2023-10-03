26c09769492e42b4f7930fbb240baeb28a9fe219
Adhiraj <adhirajd007@gmail.com>
2023-10-03 13:31:57 +0530

    style: Added `Anchor` styling (#5954)
    * style: Created Anchor styling (#5933)
    
    * fix: Changed css import
    
    * refactor: Rename export to Anchor
    
    * fix(base.css): Added more anchor specifiers

---
+++ components/__design__/text.stories.tsx
@@ -0,0 +1,24 @@
+import type { Meta as MetaObj, StoryObj } from '@storybook/react';
+
+export const Anchor: StoryObj = {
+  render: () => (
+    <div>
+      <p>
+        The current Node.js security policy can be found at{' '}
+        <a href="https://github.com/nodejs/node/blob/HEAD/SECURITY.md#security">
+          https://github.com/nodejs/node/blob/HEAD/SECURITY.md#security
+        </a>
+        , including information on how to report a vulnerability in Node.js.
+        Subscribe to the low-volume announcement-only nodejs-sec mailing list at{' '}
+        <a href="https://groups.google.com/forum/#!forum/nodejs-sec">
+          https://groups.google.com/forum/#!forum/nodejs-sec
+        </a>{' '}
+        to stay up to date on security vulnerabilities and security-related
+        releases of Node.js and the projects maintained in the nodejs GitHub
+        organization.
+      </p>
+    </div>
+  ),
+};
+
+export default { title: 'Design System' } as MetaObj;
