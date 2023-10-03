a4ec472913a38db7c00d3e4972f5aa96f77b64d6
Daniel Selga <danielselga@gmail.com>
2023-10-03 16:38:37 -0400

    fix: remove return statement

--- components/__design__/list.stories.tsx
+++ components/__design__/list.stories.tsx
@@ -1,31 +1,27 @@
 import type { Meta as MetaObj, StoryObj } from '@storybook/react';
 
 export const OrderedLists: StoryObj = {
-  render: () => {
-    return (
-      <div>
-        <ol>
-          <li>Ordered lists</li>
-          <li>Like this</li>
-          <li>with numbers</li>
-        </ol>
-      </div>
-    );
-  },
+  render: () => (
+    <div>
+      <ol>
+        <li>Ordered lists</li>
+        <li>Like this</li>
+        <li>with numbers</li>
+      </ol>
+    </div>
+  ),
 };
 
 export const UnorderedLists: StoryObj = {
-  render: () => {
-    return (
-      <div>
-        <ul>
-          <li>Unordered lists</li>
-          <li>those without</li>
-          <li>numbers</li>
-        </ul>
-      </div>
-    );
-  },
+  render: () => (
+    <div>
+      <ul>
+        <li>Unordered lists</li>
+        <li>those without</li>
+        <li>numbers</li>
+      </ul>
+    </div>
+  ),
 };
 
 export default { title: 'Design System/Lists' } as MetaObj;
