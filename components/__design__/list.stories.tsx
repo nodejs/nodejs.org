3045c8e956ac0bed0f498f8734526b598d3bd6ba
Daniel Selga <danielselga@gmail.com>
2023-10-03 11:20:49 -0400

    fix: storybook disposition

--- components/__design__/list.stories.tsx
+++ components/__design__/list.stories.tsx
@@ -1,33 +1,36 @@
 import type { Meta as MetaObj, StoryObj } from '@storybook/react';
 import type { CSSProperties } from 'react';
 
-export const Lists: StoryObj = {};
-
 const listContainer = {
   padding: '1.5rem',
 } as CSSProperties;
 
-export default {
-  title: 'Design System/Lists',
-  component: () => {
+export const OrderedLists: StoryObj = {
+  render: () => {
     return (
-      <>
-        <div style={listContainer}>
-          <ul>
-            <li>Unordered lists</li>
-            <li>those without</li>
-            <li>numbers</li>
-          </ul>
-        </div>
-
-        <div style={listContainer}>
-          <ol>
-            <li>Ordered lists</li>
-            <li>Like this</li>
-            <li>with numbers</li>
-          </ol>
-        </div>
-      </>
+      <div style={listContainer}>
+        <ol>
+          <li>Ordered lists</li>
+          <li>Like this</li>
+          <li>with numbers</li>
+        </ol>
+      </div>
     );
   },
-} as MetaObj;
+};
+
+export const UnorderedLists: StoryObj = {
+  render: () => {
+    return (
+      <div style={listContainer}>
+        <ul>
+          <li>Unordered lists</li>
+          <li>those without</li>
+          <li>numbers</li>
+        </ul>
+      </div>
+    );
+  },
+};
+
+export default { title: 'Design System/Lists' } as MetaObj;
