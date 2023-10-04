ddb0f46b7d3b3636365208ef288f8c8fdb3caefe
Guilherme Araújo <arauujogui@gmail.com>
2023-10-04 16:13:40 -0300

    refactor: remove decorated component

--- components/Common/Tabs/index.stories.tsx
+++ components/Common/Tabs/index.stories.tsx
@@ -1,24 +1,27 @@
 import type { Meta as MetaObj, StoryObj } from '@storybook/react';
-import type { FC } from 'react';
 
 import { Tabs, TabsList, TabsTrigger, TabsContent } from './index';
 
-const DecoratedTabs: FC = () => (
-  <Tabs defaultValue="package">
-    <TabsList>
-      <TabsTrigger value="package">Package Manager</TabsTrigger>
-      <TabsTrigger value="prebuilt">Prebuilt Installer</TabsTrigger>
-      <TabsTrigger value="source">Source Code</TabsTrigger>
-    </TabsList>
-    <TabsContent value="package">Package Manager</TabsContent>
-    <TabsContent value="prebuilt">Prebuilt Installer</TabsContent>
-    <TabsContent value="source">Source Code</TabsContent>
-  </Tabs>
-);
-
-type Story = StoryObj<typeof DecoratedTabs>;
-type Meta = MetaObj<typeof DecoratedTabs>;
+type Story = StoryObj<typeof Tabs>;
+type Meta = MetaObj<typeof Tabs>;
 
 export const Default: Story = {};
 
-export default { component: DecoratedTabs } as Meta;
+export default {
+  component: Tabs,
+  args: {
+    defaultValue: 'package',
+    children: (
+      <>
+        <TabsList>
+          <TabsTrigger value="package">Package Manager</TabsTrigger>
+          <TabsTrigger value="prebuilt">Prebuilt Installer</TabsTrigger>
+          <TabsTrigger value="source">Source Code</TabsTrigger>
+        </TabsList>
+        <TabsContent value="package">Package Manager</TabsContent>
+        <TabsContent value="prebuilt">Prebuilt Installer</TabsContent>
+        <TabsContent value="source">Source Code</TabsContent>
+      </>
+    ),
+  },
+} as Meta;
