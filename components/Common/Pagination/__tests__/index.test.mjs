6db8a7c74002dec74aeb3c1b9f98c2a62ca3217e
Daniel Marcano <danielmarcanodev@gmail.com>
2023-10-07 00:08:49 +0200

    Feat: add currentPage and pages props to Common/Pagination component

--- components/Common/Pagination/__tests__/index.test.mjs
+++ components/Common/Pagination/__tests__/index.test.mjs
@@ -13,5 +13,38 @@ describe('Pagination', () => {
 
       expect(screen.getByRole('button', { name: /next/i })).toBeVisible();
     });
+
+    it('Renders the passed pages and current page', () => {
+      const currentPage = 1;
+
+      const pages = [
+        {
+          url: '1',
+        },
+        {
+          url: '2',
+        },
+        {
+          url: '3',
+        },
+        {
+          url: '4',
+        },
+      ];
+
+      render(<Pagination currentPage={currentPage} pages={pages} />);
+
+      const pageElements = screen.getAllByRole('link');
+
+      expect(pageElements).toHaveLength(pages.length);
+
+      pageElements.forEach((page, index) => {
+        if (index + 1 === currentPage) {
+          expect(page).toHaveAttribute('aria-current', 'page');
+        }
+
+        expect(page).toBeVisible();
+      });
+    });
   });
 });
--- components/Common/Pagination/index.tsx
+++ components/Common/Pagination/index.tsx
@@ -1,11 +1,39 @@
 import type { FC } from 'react';
 
-const Pagination: FC = () => {
+type Page = {
+  url: string;
+};
+
+export type PaginationProps = {
+  /**
+   * One-based number of the current page
+   */
+  currentPage: number;
+  pages: Page[];
+};
+
+const Pagination: FC<PaginationProps> = ({ currentPage, pages }) => {
+  const pageItems = pages.map(({ url }, index) => {
+    const pageNumber = index + 1;
+
+    return (
+      <li key={url}>
+        <a
+          href={url}
+          {...(pageNumber === currentPage && { 'aria-current': 'page' })}
+        >
+          {pageNumber}
+        </a>
+      </li>
+    );
+  });
+
   return (
     <nav>
       <button type="button" aria-label={'Previous page'}>
         Previous
       </button>
+      <ol>{pageItems}</ol>
       <button type="button" aria-label={'Next page'}>
         Next
       </button>
