fdbbcc854ad29ec33bc8680519baca4e90e61d2d
Kwan Jun Wen <40173716+junwen-k@users.noreply.github.com>
2023-10-10 03:05:34 +0800

    feat(components): add `Breadcrumbs` component (#5928)
    * feat(components): add `Breadcrumbs` component
    
    * feat(Storybook): add Breadcrumbs Hidden Home story
    
    * fix(Breadcrumbs): fix CSS indentation
    
    * fix(Breadcrumbs): fix typo
    
    * fix(Breadcrumbs): fix eslint import order
    
    * refactor(Breadcrumbs): split Breadcrumb into sub components
    
    * style(Breadcrumbs): remove comment and update component
    
    * fix(Breadcrumbs): use LocalizedLink
    
    * style(Breadcrumbs): remove abbreviation
    
    * refactor(Breadcrumbs): improve code readability
    
    * style(Breadcrumbs): format Breadcrumbs story
    
    * fix(Breadcrumbs): add default value for links prop
    
    * test(Breadcrumbs): add unit test
    
    * test(Breadcrumbs): remove unit test
    
    * feat(Breadcrumbs): add microdata
    
    * feat(Breadcrumbs): use intl for home aria label
    
    * fix(Breadcrumbs): itemProp without wrapping with span
    
    * refactor(Breadcrumbs): modularize Breadcrumb and refine logic
    
    * style(Breadcrumbs): rename other to props
    
    * fix(Breadcrumbs): move name to link
    
    * fix(Breadcrumbs): disable microdata for truncated item

---
+++ components/Common/Breadcrumbs/BreadcrumbLink/index.tsx
@@ -0,0 +1,31 @@
+import classNames from 'classnames';
+import type { ComponentProps, FC } from 'react';
+
+import LocalizedLink from '@/components/LocalizedLink';
+
+import styles from './index.module.css';
+
+type BreadcrumbLinkProps = {
+  active?: boolean;
+} & ComponentProps<typeof LocalizedLink>;
+
+const BreadcrumbLink: FC<BreadcrumbLinkProps> = ({
+  href,
+  active,
+  ...props
+}) => (
+  <LocalizedLink
+    itemScope
+    itemType="http://schema.org/Thing"
+    itemProp="item"
+    itemID={href.toString()}
+    href={href}
+    className={classNames({ [styles.active]: active }, props.className)}
+    aria-current={active ? 'page' : undefined}
+    {...props}
+  >
+    <span itemProp="name">{props.children}</span>
+  </LocalizedLink>
+);
+
+export default BreadcrumbLink;
