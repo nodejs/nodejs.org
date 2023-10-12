fcc551dbc09c750584da04c3f91bd84c68593e82
Claudio Wunder <cwunder@hubspot.com>
2023-10-12 23:21:27 +0200

    chore: ensure that light mode has light color scheme

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -14,7 +14,7 @@ const config: StorybookConfig = {
     // This `<style>` is necessary to simulate what `next-themes` (ThemeProvider) does on real applications
     // `next-theme` automatically injects the color-scheme based on the system preference or the current applied theme
     // on Storybook we don't use `next-theme` as we want to simulate themes
-    '<style>html[data-theme="dark"] { color-scheme: dark; }</style>' +
+    '<style>:root { color-scheme: light; } html[data-theme="dark"] { color-scheme: dark; }</style>' +
     // This adds the base styling for dark/light themes within Storybook. This is a Storybook-only style
     '<body class="bg-white text-neutral-950 dark:bg-neutral-950 dark:text-white"></body>',
   core: { disableTelemetry: true, disableWhatsNewNotifications: true },
