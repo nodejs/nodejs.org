6c76cd577c7d98a1ed0fab950c55b8dd52b606be
Claudio Wunder <cwunder@hubspot.com>
2023-10-12 23:11:39 +0200

    chore: simplify storybook config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,3 @@
-import { resolve } from 'node:path';
 import type { StorybookConfig } from '@storybook/nextjs';
 
 const config: StorybookConfig = {
@@ -11,33 +10,14 @@ const config: StorybookConfig = {
   ],
   logLevel: 'error',
   staticDirs: ['../public'],
-  core: {
-    disableTelemetry: true,
-    builder: {
-      name: '@storybook/builder-webpack5',
-      options: { lazyCompilation: true },
-    },
+  previewBody:
+    '<body class="bg-white text-neutral-950 dark:bg-neutral-950 dark:text-white"></body>',
+  core: { disableTelemetry: true, disableWhatsNewNotifications: true },
+  framework: {
+    name: '@storybook/nextjs',
+    options: { builder: { lazyCompilation: true } },
   },
-  framework: { name: '@storybook/nextjs', options: {} },
-  webpackFinal: async config => {
-    // This allows us to resolve node_modules and everything from the Application source
-    config.resolve!.modules = [resolve(__dirname, '..'), 'node_modules'];
-
-    config.resolve!.alias = {
-      ...config.resolve!.alias,
-      // Allows us to use `@` imports with TypeScript
-      '@': resolve(__dirname, '../'),
-    };
-
-    // We want to disable the annoying performance hints
-    // as we know that Storybook has big bundles
-    config.performance = {
-      ...config.performance,
-      hints: false,
-    };
-
-    return config;
-  },
+  webpack: async config => ({ ...config, performance: { hints: false } }),
 };
 
 export default config;
