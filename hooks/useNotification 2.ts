ddf4778e24fcdc4b68c73b34eeb818b601125cbd
Augustin Mauroy <augustin.mauroy@outlook.fr>
2023-10-15 18:06:07 +0200

    fix

--- hooks/useNotification 2.ts
+++
@@ -1,5 +0,0 @@
-import { useContext } from 'react';
-
-import { NotificationDispatch } from '@/providers/notificationProvider';
-
-export const useNotification = () => useContext(NotificationDispatch);
