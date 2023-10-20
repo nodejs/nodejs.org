cf55257aa22f0a3ebc7a105bc8bc12e8cde0b98f
hariprasath.s <prasathsanmugam@hotmail.com>
2023-10-20 20:49:32 +0530

    feat: Create Table styling

---
+++ components/__design__/table.stories.tsx
@@ -0,0 +1,30 @@
+import type { Meta as MetaObj, StoryObj } from '@storybook/react';
+
+export const Table: StoryObj = {
+  render: () => (
+    <table>
+      <tr>
+        <th>Column 1</th>
+        <th>Column 2</th>
+        <th>Column 3</th>
+      </tr>
+      <tr>
+        <td>Data 1</td>
+        <td>Data 2</td>
+        <td>Data 3</td>
+      </tr>
+      <tr>
+        <td>Data 1</td>
+        <td>Data 2</td>
+        <td>Data 3</td>
+      </tr>
+      <tr>
+        <td>Data 1</td>
+        <td>Data 2</td>
+        <td>Data 3</td>
+      </tr>
+    </table>
+  ),
+};
+
+export default { title: 'Design System' } as MetaObj;
