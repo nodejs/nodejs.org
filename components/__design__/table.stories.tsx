0674c1c973697805079c6933028a93ea058768aa
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-10-20 17:38:04 +0530

    Added table styling based on Figma design for both light and dark modes

---
+++ components/__design__/table.stories.tsx
@@ -0,0 +1,36 @@
+import type { Meta as MetaObj, StoryObj } from '@storybook/react';
+
+export const Table: StoryObj = {
+  render: () => (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Column 1</th>
+            <th>Column 2</th>
+            <th>Column 3</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Data 1</td>
+            <td>Data 2</td>
+            <td>Data 3</td>
+          </tr>
+          <tr>
+            <td>Data 1</td>
+            <td>Data 2</td>
+            <td>Data 3</td>
+          </tr>
+          <tr>
+            <td>Data 1</td>
+            <td>Data 2</td>
+            <td>Data 3</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  ),
+};
+
+export default { title: 'Design System' } as MetaObj;
