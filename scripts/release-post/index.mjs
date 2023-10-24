02ffc9f584d2bdc04753cab8863bc3a3298c464e
Levi (Nguyễn Lương Huy) <huy.nguyen.luong96@gmail.com>
2023-10-24 22:40:13 +0700

    chore: formatting release post

--- scripts/release-post/index.mjs
+++ scripts/release-post/index.mjs
@@ -25,6 +25,7 @@ import { writeFile } from 'node:fs/promises';
 import { resolve } from 'node:path';
 
 import handlebars from 'handlebars';
+import { format } from 'prettier';
 
 import { downloadsTable } from './downloadsTable.mjs';
 import { getRelativePath } from '../../next.helpers.mjs';
@@ -50,6 +51,8 @@ const ERRORS = {
     new Error(`Couldn't find matching changelog for ${version}`),
   INVALID_STATUS_CODE: (url, status) =>
     new Error(`Invalid status (!= 200) while retrieving ${url}: ${status}`),
+  FAILED_FILE_FORMATTING: reason =>
+    new Error(`Failed to format Release post: Reason: ${reason}`),
   FAILED_FILE_CREATION: reason =>
     new Error(`Failed to write Release post: Reason: ${reason}`),
 };
@@ -208,6 +211,16 @@ const renderPost = results => {
   return { content: template(templateParameters), ...results };
 };
 
+const formatPost = results => {
+  return new Promise((resolve, reject) => {
+    format(results.content, { parser: 'markdown' })
+      .then(formattedContent =>
+        resolve({ ...results, content: formattedContent })
+      )
+      .catch(error => reject(ERRORS.FAILED_FILE_FORMATTING(error.message)));
+  });
+};
+
 const writeToFile = results => {
   const blogPostPath = resolve(
     __dirname,
@@ -248,6 +261,7 @@ if (import.meta.url.startsWith('file:')) {
       .then(null, findLatestVersion)
       .then(fetchDocs)
       .then(renderPost)
+      .then(formatPost)
       .then(writeToFile)
       .then(
         filepath => console.log('Release post created:', filepath),
