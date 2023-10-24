ee3c0da2e6bf70bb18ef02076d5ad3787f29182c
Brian Muenzenmeyer <brian.muenzenmeyer@gmail.com>
2023-10-24 07:51:04 -0500

    make valid cjs module

--- scripts/lighthouse/index.js
+++ scripts/lighthouse/index.js
@@ -1,21 +1,21 @@
-/* global core */
-
 const formatScore = res => Math.round(res * 100);
 
-// this will be the shape of https://github.com/treosh/lighthouse-ci-action#manifest
-const results = JSON.parse(process.env.LIGHTHOUSE_RESULT);
+module.exports = ({ core }) => {
+  // this will be the shape of https://github.com/treosh/lighthouse-ci-action#manifest
+  const results = JSON.parse(process.env.LIGHTHOUSE_RESULT);
 
-const formattedResults = results
-  .map(({ url, summary }) => {
-    return `Lighthouse results for ${url}
-  Category | Score
-  --- | ---
-  Performance | ${formatScore(summary.performance)}
-  Accessibility | ${formatScore(summary.accessibility)}
-  Best practices | ${formatScore(summary['best-practices'])}
-  SEO | ${formatScore(summary.seo)}
-  `;
-  })
-  .join('\n\n');
+  const formattedResults = results
+    .map(({ url, summary }) => {
+      return `Lighthouse results for ${url}
+    Category | Score
+    --- | ---
+    Performance | ${formatScore(summary.performance)}
+    Accessibility | ${formatScore(summary.accessibility)}
+    Best practices | ${formatScore(summary['best-practices'])}
+    SEO | ${formatScore(summary.seo)}
+    `;
+    })
+    .join('\n\n');
 
-core.setOutput('comment', formattedResults);
+  core.setOutput('comment', formattedResults);
+};
