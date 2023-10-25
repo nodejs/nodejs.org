25e75336d324d6508354aa38ff808e6cbdcbb0f8
Brian Muenzenmeyer <brian.muenzenmeyer@gmail.com>
2023-10-24 19:24:53 -0500

    condensed output

--- scripts/lighthouse/index.js
+++ scripts/lighthouse/index.js
@@ -5,22 +5,36 @@ const formatScore = res => {
   return `${stoplight(normalizedScore)} ${normalizedScore}`;
 };
 
+/**
+ * core is in scope from https://github.com/actions/github-script
+ * This is a CJS formatted file because using ESM here requires setting `type: module` in the package.json and that broke out site capabilities
+ */
 module.exports = ({ core }) => {
   // this will be the shape of https://github.com/treosh/lighthouse-ci-action#manifest
   const results = JSON.parse(process.env.LIGHTHOUSE_RESULT);
 
-  const formattedResults = results
-    .map(({ url, summary }) => {
-      return `Lighthouse results for ${url}
-Category | Score
---- | ---
-Performance | ${formatScore(summary.performance)}
-Accessibility | ${formatScore(summary.accessibility)}
-Best practices | ${formatScore(summary['best-practices'])}
-SEO | ${formatScore(summary.seo)}
-`;
-    })
-    .join('\n\n');
+  // this will be the shape of https://github.com/treosh/lighthouse-ci-action#links
+  const links = JSON.parse(process.env.LIGHTHOUSE_LINKS);
 
-  core.setOutput('comment', formattedResults);
+  // start creating our markdown table
+  const header = [
+    'Lighthouse Results',
+    'URL | Performance | Accessibility | Best Practices | SEO | Report',
+    '| - | - | - | - | - | - |',
+  ];
+
+  // map over each url result, formatting and linking to the output
+  const urlResults = results.map(({ url, summary }) => {
+    return `[${url.replace(
+      process.env.VERCEL_PREVIEW_URL,
+      ''
+    )}](${url}) | ${formatScore(summary.performance)} | ${formatScore(
+      summary.accessibility
+    )} | ${formatScore(summary['best-practices'])} | ${formatScore(
+      summary.seo
+    )} | [🔗](${links[url]})`;
+  });
+
+  const finalResults = [...header, ...urlResults].join('\n');
+  core.setOutput('comment', finalResults);
 };
