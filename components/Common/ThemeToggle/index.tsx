917be3b6dff098fe1947e0d142b4977e09c1f8d2
Claudio Wunder <cwunder@hubspot.com>
2023-10-28 11:18:40 +0200

    meta: corrected designs of themetoggle and languagedropdown

--- components/Common/ThemeToggle/index.tsx
+++ components/Common/ThemeToggle/index.tsx
@@ -1,33 +1,29 @@
 import { MoonIcon, SunIcon } from '@heroicons/react/24/outline';
-import { AccessibleIcon } from '@radix-ui/react-accessible-icon';
 import type { FC, MouseEvent } from 'react';
 import { useIntl } from 'react-intl';
 
 import styles from './index.module.css';
 
 type ThemeToggleProps = {
-  onClick: (event: MouseEvent<HTMLButtonElement>) => void;
+  onClick?: (event: MouseEvent<HTMLButtonElement>) => void;
 };
 
-const ThemeToggle: FC<ThemeToggleProps> = ({ onClick }) => {
+const ThemeToggle: FC<ThemeToggleProps> = ({ onClick = () => {} }) => {
   const { formatMessage } = useIntl();
 
+  const ariaLabel = formatMessage({
+    id: 'components.header.buttons.toggleDarkMode',
+  });
+
   return (
-    <button type="button" onClick={onClick} className={styles.themeToggle}>
-      <AccessibleIcon
-        label={formatMessage({
-          id: 'components.header.buttons.toggleDarkMode',
-        })}
-      >
-        <MoonIcon className="hidden dark:block" />
-      </AccessibleIcon>
-      <AccessibleIcon
-        label={formatMessage({
-          id: 'components.header.buttons.toggleLightMode',
-        })}
-      >
-        <SunIcon className="block dark:hidden" />
-      </AccessibleIcon>
+    <button
+      type="button"
+      onClick={onClick}
+      className={styles.themeToggle}
+      aria-label={ariaLabel}
+    >
+      <MoonIcon className="block dark:hidden" height="20" />
+      <SunIcon className="hidden dark:block" height="20" />
     </button>
   );
 };
