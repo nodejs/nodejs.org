19f72c3971f6fce451b39ef68fc4b9808734395f
manikanta mavireddy <56877683+manikantamavireddy@users.noreply.github.com>
2023-10-30 06:21:10 +0530

    Update components/Common/Sidebar/index.tsx
    Co-authored-by: canerakdas <canerakdas@gmail.com>
    Signed-off-by: manikanta mavireddy <56877683+manikantamavireddy@users.noreply.github.com>

--- components/Common/Sidebar/index.tsx
+++ components/Common/Sidebar/index.tsx
@@ -6,7 +6,7 @@ import SideBarGroup from '@/components/Common/SidebarGroup';
 
 import styles from './index.module.css';
 
-type SideBarGroupProps = { groups: Array<SidebarGroupType> };
+type SideBarGroupProps = { groups: SidebarGroupType[] };
 
 const SideBar: FC<SideBarGroupProps> = ({ groups }) => {
   const [activeItem, setActiveItem] = useState<ActiveItem>();
