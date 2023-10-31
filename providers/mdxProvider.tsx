daae9ea6a29ca0d2c8fc5b9f08066fed84261cb7
Claudio Wunder <cwunder@hubspot.com>
2023-10-31 02:24:52 +0100

    chore: just some text

--- providers/mdxProvider.tsx
+++ providers/mdxProvider.tsx
@@ -15,6 +15,8 @@ export const MDXProvider: FC<{ content: string }> = ({ content }) => {
   useEffect(() => window.startLegacyApp(), []);
 
   // Parses the MDX Function and eval's it into a React Component
+  // We don't need asynchronous runtime here as we want to render the MDX
+  // as soon as it is available and be able to make initial renders
   const MDXContent = useMemo(() => runMDX(content), [content]);
 
   return <MDXContent components={combinedComponents} />;
