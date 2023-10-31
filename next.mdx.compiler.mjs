3006a943a57e7829d1feb2b9a4942c2e5734b371
Claudio Wunder <cwunder@hubspot.com>
2023-10-31 02:35:29 +0100

    chore: some comments

--- next.mdx.compiler.mjs
+++ next.mdx.compiler.mjs
@@ -15,7 +15,7 @@ import { nextRehypePlugins, nextRemarkPlugins } from './next.mdx.mjs';
  * @returns {Promise<{ content: VFile; headings: import('@vcarl/remark-headings').Heading[]; frontmatter: Record<string, any>}>}
  */
 export async function compileMDX(source, fileExtension) {
-  // Parses the Frontmatter to the VFile and removes from the origina source
+  // Parses the Frontmatter to the VFile and removes from the original source
   // cleaning the frontmatter to the source that is going to be parsed by the MDX Compiler
   matter(source, { strip: true });
 
@@ -24,10 +24,13 @@ export async function compileMDX(source, fileExtension) {
     rehypePlugins: nextRehypePlugins(fileExtension),
     remarkPlugins: nextRemarkPlugins(fileExtension),
     format: fileExtension,
+    // This results on the minimal possible MDX parsed, and delegates
+    // another parser to actualy evaluate the MDX into JSX
     outputFormat: 'function-body',
   });
 
   // Retrieve some parsed data from the VFile metadata
+  // such as frontmatter and Markdown headings
   const { headings, matter: frontmatter } = source.data;
 
   return { content: compiledSource, headings, frontmatter };
@@ -42,6 +45,8 @@ export async function compileMDX(source, fileExtension) {
  */
 export function runMDX(source) {
   const { default: content } = runSync(source, {
+    // We need to pass the current JSX runtime down the road to the MDX Runtime
+    // so that it can properly evaluate the MDX into JSX
     ...runtime,
     baseUrl: import.meta.url,
   });
