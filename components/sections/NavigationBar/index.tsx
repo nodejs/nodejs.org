f77ae6951de6601de91e0d90f065384451a119c7
Rouak-stha <rounakshrestha1153@gmail.com>
2023-11-13 22:16:15 +0545

    chore: Convert SVG to Components

--- components/sections/NavigationBar/index.tsx
+++ components/sections/NavigationBar/index.tsx
@@ -2,16 +2,17 @@ import Hamburger from '@heroicons/react/24/solid/Bars3Icon';
 import XMark from '@heroicons/react/24/solid/XMarkIcon';
 import * as Label from '@radix-ui/react-label';
 import classNames from 'classnames';
-import Image from 'next/image';
 import { type FC, type ComponentProps, useState } from 'react';
 
 import LanguageDropdown from '@/components/Common/LanguageDropDown';
 import ThemeToggle from '@/components/Common/ThemeToggle';
-import Github from '@/components/Icons/Github';
+import GithubLogo from '@/components/Icons/GitHubLogo';
+import NodejsLogoDark from '@/components/Icons/NodejsLogoDark';
+import NodejsLogoLight from '@/components/Icons/NodejsLogoLight';
 import NavItem from '@/components/sections/NavItem';
-import { BASE_PATH } from '@/next.constants.mjs';
 
 import style from './index.module.css';
+
 type NavItem = { text: string; href: string };
 
 type NavbarProps = {
@@ -37,20 +38,8 @@ const NavigationBar: FC<NavbarProps> = ({
     <nav className={style.container}>
       <div className={style.leftItems}>
         <div className={style.nodeIconWrapper}>
-          <Image
-            className="dark:hidden"
-            height={24}
-            width={80}
-            src={`${BASE_PATH}/static/images/logos/horizontal-dark.svg`}
-            alt="Node.js"
-          />
-          <Image
-            className="hidden dark:block"
-            height={24}
-            width={80}
-            src={`${BASE_PATH}/static/images/logos/horizontal-light.svg`}
-            alt="Node.js"
-          />
+          <NodejsLogoDark className="h-6 w-20 dark:hidden" />
+          <NodejsLogoLight className="hidden h-6 w-20 dark:block" />
         </div>
         <Label.Root
           onClick={() => setIsMenuOpen(prev => !prev)}
@@ -83,7 +72,7 @@ const NavigationBar: FC<NavbarProps> = ({
           />
           <a href="https://github.com/nodejs/node">
             <button className={style.ghIconWrapper} aria-label="Node.js Github">
-              <Github />
+              <GithubLogo />
             </button>
           </a>
         </div>
