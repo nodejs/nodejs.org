1a195e432871a4990dc453bd17329bc03e03aa48
manikanta mavireddy <56877683+manikantamavireddy@users.noreply.github.com>
2023-11-15 06:57:49 +0530

    feat: Add SidebarItem, sideBarGroup and sideBar components (#6065)
    * feat: Add SidebarItem, sideBarGroup and sideBar components
    
    * feat: Add styling to sideBarGroup
    
    * Update components/Common/Sidebar/index.tsx
    
    Co-authored-by: canerakdas <canerakdas@gmail.com>
    Signed-off-by: manikanta mavireddy <56877683+manikantamavireddy@users.noreply.github.com>
    
    * Update types/sidebar.ts
    
    Co-authored-by: Augustin Mauroy <augustin.mauroy@outlook.fr>
    Signed-off-by: manikanta mavireddy <56877683+manikantamavireddy@users.noreply.github.com>
    
    * Update components/Common/Sidebar/index.tsx
    
    Co-authored-by: canerakdas <canerakdas@gmail.com>
    Signed-off-by: manikanta mavireddy <56877683+manikantamavireddy@users.noreply.github.com>
    
    * Update components/Common/Sidebar/index.tsx
    
    Co-authored-by: canerakdas <canerakdas@gmail.com>
    Signed-off-by: manikanta mavireddy <56877683+manikantamavireddy@users.noreply.github.com>
    
    * Update components/Common/SidebarItem/index.module.css
    
    Co-authored-by: canerakdas <canerakdas@gmail.com>
    Signed-off-by: manikanta mavireddy <56877683+manikantamavireddy@users.noreply.github.com>
    
    * Update components/Common/SidebarItem/index.tsx
    
    Co-authored-by: canerakdas <canerakdas@gmail.com>
    Signed-off-by: manikanta mavireddy <56877683+manikantamavireddy@users.noreply.github.com>
    
    * feat: refactored component structure for sidebar
    
    * removed console log
    
    * Update components/Common/Sidebar/SidebarGroup/index.module.css
    
    Co-authored-by: canerakdas <canerakdas@gmail.com>
    Signed-off-by: manikanta mavireddy <56877683+manikantamavireddy@users.noreply.github.com>
    
    * Update components/Common/Sidebar/SidebarGroup/index.module.css
    
    Co-authored-by: canerakdas <canerakdas@gmail.com>
    Signed-off-by: manikanta mavireddy <56877683+manikantamavireddy@users.noreply.github.com>
    
    * Update components/Common/Sidebar/SidebarGroup/index.module.css
    
    Co-authored-by: canerakdas <canerakdas@gmail.com>
    Signed-off-by: manikanta mavireddy <56877683+manikantamavireddy@users.noreply.github.com>
    
    * Update components/Common/Sidebar/SidebarGroup/index.tsx
    
    Co-authored-by: canerakdas <canerakdas@gmail.com>
    Signed-off-by: manikanta mavireddy <56877683+manikantamavireddy@users.noreply.github.com>
    
    * Update components/Common/Sidebar/SidebarGroup/index.tsx
    
    Co-authored-by: canerakdas <canerakdas@gmail.com>
    Signed-off-by: manikanta mavireddy <56877683+manikantamavireddy@users.noreply.github.com>
    
    * removed unused import
    
    * feat:refactored sidebar component removed internal state
    
    * updated styling to match ui with figma design
    
    * updated styling
    
    * minor code refactoring and updated storybook to make it look like real page
    
    * fixed lint issues
    
    * updated imports
    
    * chore: address review feedback, align with figma
    
    ---------
    
    Signed-off-by: manikanta mavireddy <56877683+manikantamavireddy@users.noreply.github.com>
    Co-authored-by: Manikanta Mavireddy <manikantamavireddy@Manikantas-MacBook-Air.local>
    Co-authored-by: canerakdas <canerakdas@gmail.com>
    Co-authored-by: Augustin Mauroy <augustin.mauroy@outlook.fr>
    Co-authored-by: Brian Muenzenmeyer <brian.muenzenmeyer@gmail.com>

---
+++ components/Common/Sidebar/SidebarGroup/index.tsx
@@ -0,0 +1,35 @@
+import type { ComponentProps, FC } from 'react';
+
+import SidebarItem from '@/components/Common/Sidebar/SidebarItem';
+
+import styles from './index.module.css';
+
+type SidebarGroupProps = {
+  groupName: string;
+  items: ComponentProps<typeof SidebarItem>[];
+  activeItem?: ComponentProps<typeof SidebarItem>;
+};
+
+const SidebarGroup: FC<SidebarGroupProps> = ({
+  groupName,
+  items,
+  activeItem,
+}) => (
+  <section className={styles.group}>
+    <label className={styles.groupName}>{groupName}</label>
+    <ul className={styles.itemList}>
+      {items.map(({ title, url }) => (
+        <SidebarItem
+          key={title}
+          title={title}
+          url={url}
+          isActive={
+            activeItem?.title === title && activeItem.url === activeItem.url
+          }
+        />
+      ))}
+    </ul>
+  </section>
+);
+
+export default SidebarGroup;
---
+++ components/Common/Sidebar/index.stories.tsx
@@ -0,0 +1,160 @@
+import type { Meta as MetaObj, StoryObj } from '@storybook/react';
+
+import Sidebar from '@/components/Common/Sidebar';
+
+type Story = StoryObj<typeof Sidebar>;
+type Meta = MetaObj<typeof Sidebar>;
+
+export const Default: Story = {
+  args: {
+    groups: [
+      {
+        groupName: 'About Node.js',
+        items: [
+          {
+            url: '/item1',
+            title: 'About Node.js',
+          },
+          {
+            url: '/item2',
+            title: 'Project Governance',
+          },
+          {
+            url: '/item3',
+            title: 'Releases',
+          },
+          {
+            url: '/item4',
+            title: 'Branding',
+          },
+          {
+            url: '/item5',
+            title: 'Privacy Policy',
+          },
+          {
+            url: '/item6',
+            title: 'Security Reporting',
+          },
+        ],
+      },
+      {
+        groupName: 'Get Involved',
+        items: [
+          {
+            url: '/item7',
+            title: 'Get Involved',
+          },
+          {
+            url: '/item8',
+            title: 'Collab Summit',
+          },
+          {
+            url: '/item9',
+            title: 'Contribute',
+          },
+          {
+            url: '/item10',
+            title: 'Code of Conduct',
+          },
+        ],
+      },
+      {
+        groupName: 'Download',
+        items: [
+          {
+            url: '/item11',
+            title: 'Download',
+          },
+          {
+            url: '/item12',
+            title: 'Package Manager',
+          },
+          {
+            url: '/item13',
+            title: 'Previous Releases',
+          },
+        ],
+      },
+    ],
+  },
+};
+
+export const ActiveItem: Story = {
+  args: {
+    groups: [
+      {
+        groupName: 'About Node.js',
+        items: [
+          {
+            url: '/item1',
+            title: 'About Node.js',
+          },
+          {
+            url: '/item2',
+            title: 'Project Governance',
+          },
+          {
+            url: '/item3',
+            title: 'Releases',
+          },
+          {
+            url: '/item4',
+            title: 'Branding',
+          },
+          {
+            url: '/item5',
+            title: 'Privacy Policy',
+          },
+          {
+            url: '/item6',
+            title: 'Security Reporting',
+          },
+        ],
+      },
+      {
+        groupName: 'Get Involved',
+        items: [
+          {
+            url: '/item7',
+            title: 'Get Involved',
+          },
+          {
+            url: '/item8',
+            title: 'Collab Summit',
+          },
+          {
+            url: '/item9',
+            title: 'Contribute',
+          },
+          {
+            url: '/item10',
+            title: 'Code of Conduct',
+          },
+        ],
+      },
+      {
+        groupName: 'Download',
+        items: [
+          {
+            url: '/item11',
+            title: 'Download',
+          },
+          {
+            url: '/item12',
+            title: 'Package Manager',
+          },
+          {
+            url: '/item13',
+            title: 'Previous Releases',
+          },
+        ],
+      },
+    ],
+    activeItem: {
+      url: '/item1',
+      title: 'About Node.js',
+    },
+  },
+};
+
+export default { component: Sidebar } as Meta;
