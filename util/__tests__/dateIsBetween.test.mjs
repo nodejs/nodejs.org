9909eaf75e09f3e635911608b50164a350b0c204
Augustin Mauroy <97875033+AugustinMauroy@users.noreply.github.com>
2023-11-16 09:39:41 +0100

    test: dateIsBetween

---
+++ util/__tests__/dateIsBetween.test.mjs
@@ -0,0 +1,30 @@
+import { dateIsBetween } from '../dateIsBetween';
+
+describe('dateIsBetween', () => {
+  it('returns true when the current date is between start and end dates', () => {
+    const startDate = '2022-01-01T00:00:00.000Z';
+    const endDate = '2069-01-01T00:00:00.000Z';
+
+    const result = dateIsBetween(startDate, endDate);
+
+    expect(result).toBe(true);
+  });
+
+  it('returns false when the current date is not between start and end dates', () => {
+    const startDate = '2010-01-01T00:00:00.000Z';
+    const endDate = '2020-01-01T00:00:00.000Z';
+
+    const result = dateIsBetween(startDate, endDate);
+
+    expect(result).toBe(false);
+  });
+
+  it('returns false when either start or end date is invalid', () => {
+    const invalidStartDate = 'Invalid Start Date';
+    const validEndDate = '2024-01-01T00:00:00.000Z';
+
+    const result = dateIsBetween(invalidStartDate, validEndDate);
+
+    expect(result).toBe(false);
+  });
+});
