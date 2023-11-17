306f62bd803d9e2f58a249171133a38963c9d246
Claudio Wunder <cwunder@hubspot.com>
2023-11-18 00:41:19 +0100

    meta: updated next-intl and dependent packages

--- i18n.tsx
+++ i18n.tsx
@@ -1,11 +1,36 @@
 import type { RichTranslationValues } from 'next-intl';
 import { getRequestConfig } from 'next-intl/server';
 
-export const defaultRichTextValues: RichTranslationValues = {
+import { availableLocaleCodes } from './next.locales.mjs';
+
+// Loads the Application Locales/Translations Dynamically
+const loadLocaleDictionary = async (locale: string) => {
+  if (locale === 'en') {
+    // This enables HMR on the English Locale, so that instante refresh
+    // happens while we add/change texts on the source locale
+    return import('./i18n/locales/en.json').then(f => f.default);
+  }
+
+  if (locale in availableLocaleCodes) {
+    // Other languages don't really require HMR as they will never be development languages
+    // so we can load them dynamically
+    return import(`./i18n/locales/${locale}.json`).then(f => f.default);
+  }
+
+  throw new Error(`Unsupported locale: ${locale}`);
+};
+
+// Defines default Rich Text Components
+const defaultRichTextValues: RichTranslationValues = {
   graySpan: c => <span className="small color-lightgray">{c}</span>,
 };
 
+// Provides `next-intl` configuration for RSC/SSR
 export default getRequestConfig(async ({ locale }) => ({
-  messages: (await import(`./i18n/locales/${locale}.json`)).default,
+  // This is the dictionary of messages to be loaded
+  messages: await loadLocaleDictionary(locale),
+  // Default Rich Text Translations
+  defaultTranslationValues: defaultRichTextValues,
+  // We always define the App timezone as UTC
   timeZone: 'Etc/UTC',
 }));
