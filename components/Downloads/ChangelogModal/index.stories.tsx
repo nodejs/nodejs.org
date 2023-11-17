ae306d0405ee0710d9c39ca02a080de3002c8045
Cole <colewestbrookphoto@gmail.com>
2023-11-17 11:24:53 -0500

    feat: changelogModal styles and story

--- components/Downloads/ChangelogModal/index.stories.tsx
+++ components/Downloads/ChangelogModal/index.stories.tsx
@@ -1,31 +1,40 @@
 import type { Meta as MetaObj, StoryObj } from '@storybook/react';
 
 import Button from '@/components/Common/Button';
+import { githubProfileAvatarUrl } from '@/util/gitHubUtils';
 
 import ChangelogModal from '.';
 
 type Story = StoryObj<typeof ChangelogModal>;
 type Meta = MetaObj<typeof ChangelogModal>;
 
-// If the component has any props that are interactable, they should be passed here
-// We recommend reading Storybook docs for args: https://storybook.js.org/docs/react/writing-stories/args
-export const DefaultLightStory: Story = {
+const names = [
+  'ovflowd',
+  'bmuenzenmeyer',
+  'AugustinMauroy',
+  'HinataKah0',
+  'Harkunwar',
+  'rodion-arr',
+  'mikeesto',
+  'bnb',
+  'benhalverson',
+  'aymen94',
+  'shanpriyan',
+  'Wai-Dung',
+  'manishprivet',
+  'araujogui',
+  'thefossildev',
+];
+
+export const Default: Story = {
   args: {
     trigger: <Button>Trigger</Button>,
-    heading: 'Big patch',
-    subheading: 'Version 1.0.0',
+    heading: 'Node v18.17.0',
+    subheading: "2023-07-18, Version 18.17.0 'Hydrogen' (LTS), @danielleadams",
     avatars: [
-      {
-        alt: 'Cole Westbrook',
-        src: 'https:avatars.githubusercontent.com/u/',
-      },
+      ...names.map(name => ({ src: githubProfileAvatarUrl(name), alt: name })),
     ],
   },
-};
-
-// If the Component has more than one State/Layout/Variant, there should be one Story for each variant
-export const DarkStory: Story = {
-  args: {},
 };
 
 export default { component: ChangelogModal } as Meta;
--- components/Downloads/ChangelogModal/index.tsx
+++ components/Downloads/ChangelogModal/index.tsx
@@ -2,9 +2,9 @@ import * as Dialog from '@radix-ui/react-dialog';
 import type { FC, PropsWithChildren, ReactNode, ComponentProps } from 'react';
 
 import AvatarGroup from '@/components/Common/AvatarGroup';
-// TODO: Add styling
-// import styles from './index.module.css';
 import Link from '@/components/Link';
+
+import styles from './index.module.css';
 
 type ChangelogModalProps = {
   heading: string;
@@ -20,24 +20,28 @@ const ChangelogModal: FC<PropsWithChildren<ChangelogModalProps>> = ({
   avatars,
   trigger,
   children,
-}) => {
-  return (
-    <Dialog.Root>
-      <Dialog.Trigger asChild>{trigger}</Dialog.Trigger>
-      <Dialog.Portal>
-        <Dialog.Content>
-          <Dialog.Title>{heading}</Dialog.Title>
-          <Dialog.Description>{subheading}</Dialog.Description>
-          <div>
+}) => (
+  <Dialog.Root>
+    <Dialog.Trigger asChild>{trigger}</Dialog.Trigger>
+    <Dialog.Portal>
+      <Dialog.Overlay className={styles.dialogOverlay}>
+        <Dialog.Content className={styles.dialogContent}>
+          <Dialog.Title className={styles.dialogTitle}>{heading}</Dialog.Title>
+          <Dialog.Description className={styles.dialogDescription}>
+            {subheading}
+          </Dialog.Description>
+          <div className={styles.authorsContainer}>
             <AvatarGroup avatars={avatars} />
-            <Link href="/get-involved">Start Contributing</Link>
+            <Link href="/get-involved" className={styles.contributorLink}>
+              Start Contributing
+            </Link>
           </div>
           {children}
           <Dialog.Close />
         </Dialog.Content>
-      </Dialog.Portal>
-    </Dialog.Root>
-  );
-};
+      </Dialog.Overlay>
+    </Dialog.Portal>
+  </Dialog.Root>
+);
 
 export default ChangelogModal;
