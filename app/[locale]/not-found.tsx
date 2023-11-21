eac9cac28beae1127f4de08708ee3a18d709f0ee
Claudio Wunder <cwunder@hubspot.com>
2023-11-21 23:40:18 +0100

    fix: locale and layouts errors

--- app/[locale]/not-found.tsx
+++ app/[locale]/not-found.tsx
@@ -1,3 +1,15 @@
-import NotFound from '@/app/not-found';
+import { useTranslations } from 'next-intl';
+import type { FC } from 'react';
 
-export default NotFound;
+const LocalizedNotFound: FC = () => {
+  const t = useTranslations();
+
+  return (
+    <div className="container">
+      <h2>{t('pages.404.title')}</h2>
+      <h3>{t('pages.404.description')}</h3>
+    </div>
+  );
+};
+
+export default LocalizedNotFound;
