50882d4d924c43673e1703909d271c74ae55e041
Claudio Wunder <cwunder@hubspot.com>
2023-11-23 19:25:33 +0100

    chore: do not provide error details to users

--- app/[locale]/error.tsx
+++ app/[locale]/error.tsx
@@ -2,16 +2,10 @@
 
 import type { FC } from 'react';
 
-const ErrorPage: FC<{ error: Error }> = ({ error }) => (
+const ErrorPage: FC<{ error: Error }> = () => (
   <div className="container">
     <h2>500: Internal Server Error</h2>
     <h3>This Page has thrown a non-recoverable Error</h3>
-    <small>
-      Details: <br />
-      <pre>
-        <code>${error.message}</code>
-      </pre>
-    </small>
   </div>
 );
 
