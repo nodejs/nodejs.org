93887e8556e59b78aa97b2c0c92ecc26cbbfa8c0
Augustin Mauroy <augustin.mauroy@outlook.fr>
2023-11-25 23:20:00 +0100

    update

--- components/Common/ThemeToggle/__tests__/index.jest.mjs
+++ components/Common/ThemeToggle/__tests__/index.jest.mjs
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
-import ThemeToggle from '../';
+import ThemeToggle from '..';
 
 let mockCurrentTheme = 'light';
 
