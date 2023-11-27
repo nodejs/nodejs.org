bff3ba27723c749e0ac50b464d150b128adb3485
Claudio Wunder <cwunder@hubspot.com>
2023-11-27 12:22:57 +0100

    fix: fixed sentry errors on 404 pages

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,5 +1,4 @@
 import { init } from '@sentry/nextjs';
-import { ProfilingIntegration } from '@sentry/profiling-node';
 
 import {
   SENTRY_DSN,
@@ -14,9 +13,4 @@ init({
   dsn: SENTRY_DSN,
   // Percentage of events to send to Sentry (1% of them) (for performance metrics)
   tracesSampleRate: SENTRY_CAPTURE_RATE,
-  // Percentage of events to send to Sentry (all of them) (for profiling metrics)
-  // This number is relative to tracesSampleRate - so all traces get profiled
-  profilesSampleRate: 1.0,
-  // Add profiling integration to capture performance metrics
-  integrations: [new ProfilingIntegration()],
 });
