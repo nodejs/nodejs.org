e914814d61c962b5595cabd51db6baa63a0ad1c6
Claudio Wunder <cwunder@hubspot.com>
2023-11-29 14:39:54 +0100

    chore: design fixes

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,12 +5,7 @@ const rootClasses = classNames(
   // note: this is hard-coded sadly as next/font can only be loaded within next.js context
   '__variable_open-sans-normal',
   // note: this is hard-coded sadly as next/font can only be loaded within next.js context
-  '__variable_ibm-plex-mono-normal-600',
-  'font-open-sans',
-  'bg-white',
-  'text-neutral-950',
-  'dark:bg-neutral-950',
-  'dark:text-white'
+  '__variable_ibm-plex-mono-normal-600'
 );
 
 const config: StorybookConfig = {
@@ -27,7 +22,7 @@ const config: StorybookConfig = {
     // This `<style>` is necessary to simulate what `next-themes` (ThemeProvider) does on real applications
     // `next-theme` automatically injects the color-scheme based on the system preference or the current applied theme
     // on Storybook we don't use `next-theme` as we want to simulate themes
-    '<style>:root { color-scheme: light; } html[data-theme="dark"] { color-scheme: dark; } #storybook-root { width: 100%; height: 100%; }</style>' +
+    '<style>:root { color-scheme: light; } html[data-theme="dark"] { color-scheme: dark; }</style>' +
     // This adds the base styling for dark/light themes within Storybook. This is a Storybook-only style
     `<body class="${rootClasses}"></body>`,
   addons: [
