8afc37215d5b823f25100b43166121d82de9991d
Claudio Wunder <cwunder@hubspot.com>
2023-12-03 03:03:07 +0100

    chore: added file back

---
+++ components/withSidebar.tsx
@@ -0,0 +1,26 @@
+import type { RichTranslationValues } from 'next-intl';
+import type { FC } from 'react';
+
+import Sidebar from '@/components/Common/Sidebar';
+import { useSiteNavigation } from '@/hooks/server';
+import type { NavigationKeys } from '@/types';
+
+type WithSidebarProps = {
+  navKeys: NavigationKeys[];
+  context?: Record<string, RichTranslationValues>;
+};
+
+const WithSidebar: FC<WithSidebarProps> = ({ navKeys, context }) => {
+  const { getSideNavigation } = useSiteNavigation();
+
+  const mappedSidebarItems = getSideNavigation(navKeys, context).map(
+    ([, { label, items }]) => ({
+      groupName: label,
+      items: items.map(([, item]) => item),
+    })
+  );
+
+  return <Sidebar groups={mappedSidebarItems} />;
+};
+
+export default WithSidebar;
