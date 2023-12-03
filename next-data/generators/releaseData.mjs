858229ae8fe9f5e4e3086c71cb826c8dcdd6ce96
Claudio Wunder <cwunder@hubspot.com>
2023-12-03 02:49:07 +0100

    chore: normalise promises

--- next-data/generators/releaseData.mjs
+++ next-data/generators/releaseData.mjs
@@ -1,5 +1,7 @@
 'use strict';
 
+import nodevu from '@nodevu/core';
+
 // Gets the appropriate release status for each major release
 const getNodeReleaseStatus = (now, support) => {
   const { endOfLife, maintenanceStart, ltsStart, currentStart } = support;
@@ -29,10 +31,8 @@ const getNodeReleaseStatus = (now, support) => {
  *
  * @returns {Promise<import('../../types').NodeRelease[]>}
  */
-const generateReleaseData = async () => {
-  return import('@nodevu/core').then(async ({ default: nodevu }) => {
-    const nodevuOutput = await nodevu({ fetch: fetch });
-
+const generateReleaseData = () => {
+  return nodevu({ fetch: fetch }).then(nodevuOutput => {
     // Filter out those without documented support
     // Basically those not in schedule.json
     const majors = Object.values(nodevuOutput).filter(major => !!major.support);
@@ -64,17 +64,13 @@ const generateReleaseData = async () => {
       };
     });
 
-    return Promise.resolve(
-      // nodevu returns duplicated v0.x versions (v0.12, v0.10, ...).
-      // This behavior seems intentional as the case is hardcoded in nodevu,
-      // see https://github.com/cutenode/nodevu/blob/0c8538c70195fb7181e0a4d1eeb6a28e8ed95698/core/index.js#L24.
-      // This line ignores those duplicated versions and takes the latest
-      // v0.x version (v0.12.18). It is also consistent with the legacy
-      // nodejs.org implementation.
-      nodeReleases.filter(
-        release => release.major !== 0 || release.version === '0.12.18'
-      )
-    );
+    // nodevu returns duplicated v0.x versions (v0.12, v0.10, ...).
+    // This behavior seems intentional as the case is hardcoded in nodevu,
+    // see https://github.com/cutenode/nodevu/blob/0c8538c70195fb7181e0a4d1eeb6a28e8ed95698/core/index.js#L24.
+    // This line ignores those duplicated versions and takes the latest
+    // v0.x version (v0.12.18). It is also consistent with the legacy
+    // nodejs.org implementation.
+    return nodeReleases.filter(r => r.major !== 0 || r.version === '0.12.18');
   });
 };
 
