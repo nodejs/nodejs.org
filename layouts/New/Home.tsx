f488aadb4a15519bc13439443463326b77e972b2
Claudio Wunder <cwunder@hubspot.com>
2024-01-05 19:26:19 +0000

    feat: home page layout

---
+++ layouts/New/Home.tsx
@@ -0,0 +1,21 @@
+import classNames from 'classnames';
+import type { FC, PropsWithChildren } from 'react';
+
+import WithFooter from '@/components/withFooter';
+import WithNavBar from '@/components/withNavBar';
+
+import styles from './layouts.module.css';
+
+const layoutClassNames = classNames(styles.homeLayout, 'mdxContent');
+
+const HomeLayout: FC<PropsWithChildren> = ({ children }) => (
+  <>
+    <WithNavBar />
+
+    <div className={layoutClassNames}>{children}</div>
+
+    <WithFooter />
+  </>
+);
+
+export default HomeLayout;
