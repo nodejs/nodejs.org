64c9a8f769a926ff4cd144224d6afc581bfb6b3c
Claudio Wunder <cwunder@hubspot.com>
2024-01-09 21:36:41 +0000

    chore: minor changes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -129,7 +129,7 @@ export default {
         'gradient-subtle-white':
           'linear-gradient(180deg, theme(colors.white) 0%, theme(colors.white / 80%) 100%)',
         'gradient-glow-backdrop':
-          'radial-gradient(8em circle at calc(100% - 40px) 10px, theme(colors.green.500), transparent 30%)',
+          'radial-gradient(8em circle at calc(50%) 10px, theme(colors.green.500), transparent 30%)',
       },
       boxShadow: {
         xs: '0px 1px 2px 0px theme(colors.shadow / 5%)',
