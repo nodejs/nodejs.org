340eacfbcf2429e61080a6509c6fcc4a2632b5ce
Caner Akdas <canerakdas@gmail.com>
2024-01-17 01:13:30 +0300

    refactor: mdx to i18n

--- app/[locale]/not-found.tsx
+++ app/[locale]/not-found.tsx
@@ -1,35 +1,39 @@
-import { getLocale } from 'next-intl/server';
+import { useTranslations } from 'next-intl';
 import type { FC } from 'react';
 
-import { MDXRenderer } from '@/components/mdxRenderer';
-import WithLayout from '@/components/withLayout';
+import Button from '@/components/Common/Button';
+import CenteredLayout from '@/layouts/New/Centered';
 import { ENABLE_WEBSITE_REDESIGN } from '@/next.constants.mjs';
-import { dynamicRouter } from '@/next.dynamic.mjs';
 
-/** @deprecated replace the legacy page when website redesign is done */
-const notFoundSource = ENABLE_WEBSITE_REDESIGN ? 'new-design/404' : '404';
-
-const LocalizedNotFound: FC = async () => {
-  const locale = await getLocale();
-
-  // Retrieves the markdown source created for the not-found page
-  const { source, filename } = await dynamicRouter.getMarkdownFile(
-    locale,
-    notFoundSource
-  );
-
-  // Parses the source Markdown content and returns a React Component
-  const { MDXContent } = await dynamicRouter.getMDXContent(source, filename);
+/** @deprecated remove legacy component when website redesign is done */
+const LegacyNotFound: FC = () => {
+  const t = useTranslations();
 
   return (
-    <WithLayout layout="centered.hbs">
-      <div className="glowingBackdrop" />
-
-      <main>
-        <MDXRenderer Component={MDXContent} />
-      </main>
-    </WithLayout>
+    <div className="container">
+      <h2>{t('pages.404.title')}</h2>
+      <h3>{t('pages.404.description')}</h3>
+    </div>
   );
 };
 
-export default LocalizedNotFound;
+const NotFound: FC = async () => {
+  const t = useTranslations();
+
+  return (
+    <CenteredLayout>
+      <div className="glowingBackdrop" />
+
+      <main>
+        404
+        <h1 className="special -mt-4">{t('layouts.notFound.title')}</h1>
+        <p className="-mt-4 max-w-sm text-center text-lg">
+          {t('layouts.notFound.description')}
+        </p>
+        <Button href="/">{t('layouts.notFound.backToHome')}</Button>
+      </main>
+    </CenteredLayout>
+  );
+};
+
+export default ENABLE_WEBSITE_REDESIGN ? NotFound : LegacyNotFound;
