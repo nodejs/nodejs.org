553a5910e4e2df05b2a34481a091ae3d15dd8efb
Caner Akdas <canerakdas@gmail.com>
2024-01-16 17:10:48 +0300

    refactor: source moved outside of the component

--- app/[locale]/not-found.tsx
+++ app/[locale]/not-found.tsx
@@ -6,11 +6,11 @@ import WithLayout from '@/components/withLayout';
 import { ENABLE_WEBSITE_REDESIGN } from '@/next.constants.mjs';
 import { dynamicRouter } from '@/next.dynamic.mjs';
 
+/** @deprecated replace the legacy page when website redesign is done */
+const notFoundSource = ENABLE_WEBSITE_REDESIGN ? 'new-design/404' : '404';
+
 const LocalizedNotFound: FC = async () => {
   const locale = await getLocale();
-
-  /** @deprecated replace the legacy page when website redesign is done */
-  const notFoundSource = ENABLE_WEBSITE_REDESIGN ? 'new-design/404' : '404';
 
   // Retrieves the markdown source created for the not-found page
   const { source, filename } = await dynamicRouter.getMarkdownFile(
