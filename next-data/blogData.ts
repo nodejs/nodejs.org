bc54e253505758d4667c3fd6de70161b13928d62
Claudio Wunder <cwunder@gnome.org>
2024-01-25 22:11:39 +0100

    chore: add failsafe for blog data parsing

--- next-data/blogData.ts
+++ next-data/blogData.ts
@@ -6,6 +6,11 @@ import {
 } from '@/next.constants.mjs';
 import type { BlogPostsRSC } from '@/types';
 
+// Prevents React from throwing an Error when not able to fulfil a request
+// due to missing category or internal processing errors
+const parseBlogDataResponse = (data: string): BlogPostsRSC =>
+  data.startsWith('{') ? JSON.parse(data) : { posts: [], pagination: {} };
+
 const getBlogData = (cat: string, page?: number): Promise<BlogPostsRSC> => {
   // When we're using Static Exports the Next.js Server is not running (during build-time)
   // hence the self-ingestion APIs will not be available. In this case we want to load
@@ -26,7 +31,7 @@ const getBlogData = (cat: string, page?: number): Promise<BlogPostsRSC> => {
   // When we're on RSC with Server capabilities we prefer using Next.js Data Fetching
   // as this will load cached data from the server instead of generating data on the fly
   // this is extremely useful for ISR and SSG as it will not generate this data on every request
-  return fetch(fetchURL).then(r => r.json());
+  return fetch(fetchURL).then(r => r.text().then(parseBlogDataResponse));
 };
 
 export default getBlogData;
