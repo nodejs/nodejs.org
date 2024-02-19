88559fafab519d436b831082a9a9efadef0cea6e
Augustin Mauroy <augustin.mauroy@outlook.fr>
2024-02-19 09:22:19 +0100

    Fix: typo "Github" -> "GitHub"
    Co-authored-by: Claudio W <cwunder@gnome.org>
    Signed-off-by: Augustin Mauroy <augustin.mauroy@outlook.fr>

--- util/__tests__/gitHubUtils.test.mjs
+++ util/__tests__/gitHubUtils.test.mjs
@@ -5,7 +5,7 @@ import {
   getGitHubApiDocsUrl,
 } from '@/util/gitHubUtils';
 
-describe('Github utils', () => {
+describe('GitHub utils', () => {
   it('getGitHubAvatarUrl returns the correct URL', () => {
     expect(getGitHubAvatarUrl('octocat')).toBe(
       'https://avatars.githubusercontent.com/octocat'
