641a35740c16388b2c2ed3f6dca857485fae5bc8
Augustin Mauroy <augustin.mauroy@outlook.fr>
2024-02-19 17:48:11 +0100

    clean: story of `Event`

--- components/MDX/Calendar/Event/index.stories.tsx
+++ components/MDX/Calendar/Event/index.stories.tsx
@@ -1,25 +1,18 @@
 import type { Meta as MetaObj, StoryObj } from '@storybook/react';
-import type { ComponentProps } from 'react';
 
 import Event from '@/components/MDX/Calendar/Event';
 
 type Story = StoryObj<typeof Event>;
 type Meta = MetaObj<typeof Event>;
 
-const defaultArgs: ComponentProps<typeof Event> = {
-  start: {
-    date: new Date().toISOString(),
-  },
-  end: {
-    date: new Date().toISOString(),
-  },
-  summary: 'Example Event',
-  location: 'Event Location',
-  description: 'This is an example event description.',
-};
-
 export const Default: Story = {
-  args: defaultArgs,
+  args: {
+    start: { date: '2024-02-19T12:30:00.000Z' },
+    end: { date: '2024-02-19T16:00:00.000Z' },
+    summary: 'Example Event',
+    location: 'Event Location',
+    description: 'This is an example event description.',
+  },
 };
 
 export default { component: Event } as Meta;
