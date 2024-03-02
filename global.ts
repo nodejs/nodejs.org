8254ab90611a6f02248b70669ab0ab02829f1de6
Claudio Wunder <cwunder@gnome.org>
2024-03-02 15:52:21 +0100

    chore: add client components to mdx intrinsic types

--- global.ts
+++ global.ts
@@ -1,5 +1,7 @@
+import type { clientMdxComponents } from '@/next.mdx.use.client.mjs';
 import type { mdxComponents } from '@/next.mdx.use.mjs';
 
 declare global {
-  type MDXProvidedComponents = typeof mdxComponents;
+  type MDXProvidedComponents = typeof mdxComponents &
+    typeof clientMdxComponents;
 }
