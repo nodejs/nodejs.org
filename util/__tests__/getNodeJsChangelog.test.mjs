c1961855033c61102aac08a11072aefe94f210f3
Claudio Wunder <cwunder@gnome.org>
2024-03-02 16:10:36 +0100

    chore: fix unit tests bad import

--- util/__tests__/getNodeJsChangelog.test.mjs
+++ util/__tests__/getNodeJsChangelog.test.mjs
@@ -1,12 +1,12 @@
-import { getNodejsChangelog } from '../getNodeJsChangelog';
+import { getNodeJsChangelog } from '../getNodeJsChangelog';
 
-describe('getNodejsChangelog', () => {
+describe('getNodeJsChangelog', () => {
   it('returns the correct changelog URL for major version >= 4', () => {
     const version = '14.2.0';
     const expectedUrl =
       'https://github.com/nodejs/node/blob/main/doc/changelogs/CHANGELOG_V14.md#14.2.0';
 
-    const result = getNodejsChangelog(version);
+    const result = getNodeJsChangelog(version);
 
     expect(result).toBe(expectedUrl);
   });
@@ -16,7 +16,7 @@ describe('getNodejsChangelog', () => {
     const expectedUrl =
       'https://github.com/nodejs/node/blob/main/doc/changelogs/CHANGELOG_IOJS.md#1.8.3';
 
-    const result = getNodejsChangelog(version);
+    const result = getNodeJsChangelog(version);
 
     expect(result).toBe(expectedUrl);
   });
@@ -26,7 +26,7 @@ describe('getNodejsChangelog', () => {
     const expectedUrl1 =
       'https://github.com/nodejs/node/blob/main/doc/changelogs/CHANGELOG_V6.md#6.12.0';
 
-    const result1 = getNodejsChangelog(version1);
+    const result1 = getNodeJsChangelog(version1);
 
     expect(result1).toBe(expectedUrl1);
 
@@ -34,7 +34,7 @@ describe('getNodejsChangelog', () => {
     const expectedUrl2 =
       'https://github.com/nodejs/node/blob/main/doc/changelogs/CHANGELOG_V8.md#8.10.0';
 
-    const result2 = getNodejsChangelog(version2);
+    const result2 = getNodeJsChangelog(version2);
 
     expect(result2).toBe(expectedUrl2);
   });
@@ -44,7 +44,7 @@ describe('getNodejsChangelog', () => {
     const expectedUrl =
       'https://github.com/nodejs/node/blob/main/doc/changelogs/CHANGELOG_V012.md#0.12.7';
 
-    const result = getNodejsChangelog(version);
+    const result = getNodeJsChangelog(version);
 
     expect(result).toBe(expectedUrl);
   });
