b088c7306236d4b16448078f75472518ab23a1c3
Nirnay Behera <nirnay.1behera@gmail.com>
2024-03-03 03:28:05 +0530

    hid the events tab

--- layouts/Blog.tsx
+++ layouts/Blog.tsx
@@ -51,7 +51,7 @@ const BlogLayout: FC = async () => {
               'announcements',
               'release',
               'vulnerability',
-              'events',
+              // 'events',
             ])}
           />
         </main>
