5b79ba11200a3068c75e374962c3ec5a11f4a1f1
Augustin Mauroy <augustin.mauroy@outlook.fr>
2024-03-20 17:09:28 +0100

    fix import

--- hooks/react-client/__tests__/useMediaQuery.test.mjs
+++ hooks/react-client/__tests__/useMediaQuery.test.mjs
@@ -1,6 +1,6 @@
 import { renderHook } from '@testing-library/react';
 
-import { useMediaQuery } from '@/hooks/react-client/useMediaQuery';
+import useMediaQuery from '@/hooks/react-client/useMediaQuery';
 
 describe('useMediaQuery', () => {
   it('should check for matchMedia support', () => {
