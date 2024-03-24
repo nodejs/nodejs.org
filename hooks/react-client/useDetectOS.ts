f7c66135c28394431095a07fa1d56baca12f7229
Caner Akdas <canerakdas@gmail.com>
2024-03-24 15:13:27 +0300

    fix: Wrong bitness information (#6485)
    * fix: x86 option in Linux
    
    * fix: set default bitness as 64
    
    * fix: more user agent checks for x64
    
    * refactor: long condition group moved into the regex
    
    * refactor: default bitness values and docs

--- hooks/react-client/useDetectOS.ts
+++ hooks/react-client/useDetectOS.ts
@@ -25,13 +25,15 @@ const useDetectOS = () => {
       ([bitness, architecture]) => {
         const userAgent: string | undefined =
           (typeof navigator === 'object' && navigator.userAgent) || '';
-        const defaultBitness: number = 86; // Default bitness if unable to determine
+        // Default bitness if unable to determine
+        const defaultBitness: number = 86;
+        // Regex to detect 64-bit architecture in user agent
+        const bitnessRegex = /WOW64|Win64|x86_64|x86-64|x64_64|x64;|AMD64/;
+
         setUserOSState({
           os: detectOS(),
           bitness:
-            bitness === '64' ||
-            userAgent?.includes('WOW64') ||
-            userAgent?.includes('Win64')
+            bitness === '64' || bitnessRegex.test(userAgent)
               ? 64
               : defaultBitness,
           architecture: architecture ? architecture : '',
