977f81e89a2709ac399474b37e40af67ea5977d8
João Gabriel Quaresma de Almeida <joaoGabriel55>
2024-03-29 15:47:02 -0300

    test: update util/__tests__/blogUtils.test.mjs

--- util/__tests__/blogUtils.test.mjs
+++ util/__tests__/blogUtils.test.mjs
@@ -1,21 +1,33 @@
 import { mapAuthorToCardAuthors } from '../blogUtils';
 
 describe('mapAuthorToCardAuthors', () => {
-  it('returns the correct card authors', () => {
-    const result = mapAuthorToCardAuthors(
-      'John Doe, Jane Doe',
-      'johndoe, janedoe'
-    );
+  describe('when the author name does not have GitHub username', () => {
+    it('returns the correct card authors', () => {
+      const result = mapAuthorToCardAuthors('John Doe, Jane Doe');
 
-    expect(result).toStrictEqual([
-      {
-        fullName: 'John Doe',
-        src: 'https://github.com/johndoe.png?size=40',
-      },
-      {
-        fullName: 'Jane Doe',
-        src: 'https://github.com/janedoe.png?size=40',
-      },
-    ]);
+      expect(result).toStrictEqual([
+        {
+          fullName: 'John Doe',
+          src: 'https://ui-avatars.com/api/?name=John Doe',
+        },
+        {
+          fullName: 'Jane Doe',
+          src: 'https://ui-avatars.com/api/?name=Jane Doe',
+        },
+      ]);
+    });
+  });
+
+  describe('when the author name has GitHub username', () => {
+    it('returns the correct card authors', () => {
+      const result = mapAuthorToCardAuthors('Timothy J Fontaine');
+
+      expect(result).toStrictEqual([
+        {
+          fullName: 'Timothy J Fontaine',
+          src: 'https://avatars.githubusercontent.com/tjfontaine',
+        },
+      ]);
+    });
   });
 });
