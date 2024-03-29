20ab714cd88a5476fe3d08a73504e66c520c0027
João Gabriel Quaresma de Almeida <joaoGabriel55>
2024-03-29 15:53:46 -0300

    chore: update mapAuthorToCardAuthors regex + tests

--- util/__tests__/blogUtils.test.mjs
+++ util/__tests__/blogUtils.test.mjs
@@ -1,6 +1,29 @@
 import { mapAuthorToCardAuthors } from '../blogUtils';
 
 describe('mapAuthorToCardAuthors', () => {
+  describe('when has more than one author', () => {
+    it.each([
+      ['Timothy J Fontaine, John Doe'],
+      ['Timothy J Fontaine and John Doe'],
+      ['Timothy J Fontaine;John Doe'],
+      ['Timothy J Fontaine & John Doe'],
+      ['Timothy J Fontaine by John Doe'],
+    ])('returns the correct card authors', author => {
+      const result = mapAuthorToCardAuthors(author);
+
+      expect(result).toStrictEqual([
+        {
+          fullName: 'Timothy J Fontaine',
+          src: 'https://avatars.githubusercontent.com/tjfontaine',
+        },
+        {
+          fullName: 'John Doe',
+          src: 'https://ui-avatars.com/api/?name=John Doe',
+        },
+      ]);
+    });
+  });
+
   describe('when the author name does not have GitHub username', () => {
     it('returns the correct card authors', () => {
       const result = mapAuthorToCardAuthors('John Doe, Jane Doe');
