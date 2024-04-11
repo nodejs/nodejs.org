dae82a8a2266a37b6abe669c674e73d4b29f7597
mAmineChniti <emin.chniti@esprit.tn>
2024-04-11 11:18:30 +0100

    making sure that links in WithSidebarCrossLinks are valid

--- components/withSidebarCrossLinks.tsx
+++ components/withSidebarCrossLinks.tsx
@@ -12,28 +12,39 @@ const WithSidebarCrossLinks: FC<WithCrossLinksProps> = ({ navKey }) => {
 
   const [[, sidebarNavigation]] = getSideNavigation([navKey]);
 
+  if (!sidebarNavigation || !sidebarNavigation.items) {
+    return null; // Return null if sidebar navigation or its items are not available
+  }
+
   const crossLinkItems = sidebarNavigation.items
     .map(([, { items }]) => items.map(([, item]) => item))
     .flat();
 
-  const currentItem = crossLinkItems.findIndex(({ link }) => link === pathname);
+  const validCrossLinkItems = crossLinkItems.filter(({ link }) => link);
 
-  const [previousCrossLink, nextCrossLink] = [
-    crossLinkItems[currentItem - 1],
-    crossLinkItems[currentItem + 1],
-  ];
+  const currentItemIndex = validCrossLinkItems.findIndex(
+    ({ link }) => link === pathname
+  );
+
+  const previousCrossLink =
+    currentItemIndex > 0 ? validCrossLinkItems[currentItemIndex - 1] : null;
+
+  const nextCrossLink =
+    currentItemIndex < validCrossLinkItems.length - 1
+      ? validCrossLinkItems[currentItemIndex + 1]
+      : null;
 
   return (
     <div className="mt-4 grid w-full grid-cols-2 gap-4 xs:grid-cols-1">
-      {(previousCrossLink && (
+      {previousCrossLink && previousCrossLink.link && (
         <CrossLink
           type="previous"
           text={previousCrossLink.label}
           link={previousCrossLink.link}
         />
-      )) || <div />}
+      )}
 
-      {nextCrossLink && (
+      {nextCrossLink && nextCrossLink.link && (
         <CrossLink
           type="next"
           text={nextCrossLink.label}
