b48a8779a250cef34724d5095aee1944bfcdfa20
MattIPv4 <me@mattcowley.co.uk>
2024-04-21 23:32:23 +0100

    Fix bad switch cascade in download snippets

--- util/getNodeDownloadSnippet.ts
+++ util/getNodeDownloadSnippet.ts
@@ -13,9 +13,8 @@ export const getNodeDownloadSnippet = (release: NodeRelease, os: UserOS) => {
     CHOCO: '',
   };
 
-  switch (true) {
-    case os === 'WIN' || os === 'MAC' || os === 'LINUX':
-      snippets.DOCKER = dedent`
+  if (os === 'WIN' || os === 'MAC' || os === 'LINUX') {
+    snippets.DOCKER = dedent`
       # pulls the Node.js Docker image
       docker pull node:${release.major}-${release.major >= 4 ? 'alpine' : 'slim'}
 
@@ -24,59 +23,61 @@ export const getNodeDownloadSnippet = (release: NodeRelease, os: UserOS) => {
 
       # verifies the right NPM version is in the environment
       docker run node:${release.major}-${release.major >= 4 ? 'alpine' : 'slim'} npm -v # should print \`${release.npm}\``;
-    // eslint-disable-next-line no-fallthrough
-    case os === 'MAC' || os === 'LINUX':
-      snippets.NVM = dedent`
-        # installs NVM (Node Version Manager)
-        curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+  }
 
-        # download and install Node.js
-        nvm install ${release.major}
+  if (os === 'MAC' || os === 'LINUX') {
+    snippets.NVM = dedent`
+      # installs NVM (Node Version Manager)
+      curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 
-        # verifies the right Node.js version is in the environment
-        node -v # should print \`${release.versionWithPrefix}\`
+      # download and install Node.js
+      nvm install ${release.major}
 
-        # verifies the right NPM version is in the environment
-        npm -v # should print \`${release.npm}\``;
+      # verifies the right Node.js version is in the environment
+      node -v # should print \`${release.versionWithPrefix}\`
 
-      snippets.FNM = dedent`
-        # installs fnm (Fast Node Manager)
-        curl -fsSL https://fnm.vercel.app/install | bash
+      # verifies the right NPM version is in the environment
+      npm -v # should print \`${release.npm}\``;
 
-        # download and install Node.js
-        fnm use --install-if-missing ${release.major}
+    snippets.FNM = dedent`
+      # installs fnm (Fast Node Manager)
+      curl -fsSL https://fnm.vercel.app/install | bash
 
-        # verifies the right Node.js version is in the environment
-        node -v # should print \`${release.versionWithPrefix}\`
+      # download and install Node.js
+      fnm use --install-if-missing ${release.major}
 
-        # verifies the right NPM version is in the environment
-        npm -v # should print \`${release.npm}\``;
+      # verifies the right Node.js version is in the environment
+      node -v # should print \`${release.versionWithPrefix}\`
 
-      snippets.BREW = dedent`
-        # download and install Node.js
-        brew install node@${release.major}
+      # verifies the right NPM version is in the environment
+      npm -v # should print \`${release.npm}\``;
 
-        # verifies the right Node.js version is in the environment
-        node -v # should print \`${release.versionWithPrefix}\`
+    snippets.BREW = dedent`
+      # download and install Node.js
+      brew install node@${release.major}
 
-        # verifies the right NPM version is in the environment
-        npm -v # should print \`${release.npm}\``;
-    // eslint-disable-next-line no-fallthrough
-    case os === 'WIN':
-      snippets.FNM = dedent`
-        # installs fnm (Fast Node Manager)
-        winget install Schniz.fnm
+      # verifies the right Node.js version is in the environment
+      node -v # should print \`${release.versionWithPrefix}\`
 
-        # download and install Node.js
-        fnm use --install-if-missing ${release.major}
+      # verifies the right NPM version is in the environment
+      npm -v # should print \`${release.npm}\``;
+  }
 
-        # verifies the right Node.js version is in the environment
-        node -v # should print \`${release.versionWithPrefix}\`
+  if (os === 'WIN') {
+    snippets.FNM = dedent`
+      # installs fnm (Fast Node Manager)
+      winget install Schniz.fnm
 
-        # verifies the right NPM version is in the environment
-        npm -v # should print \`${release.npm}\``;
+      # download and install Node.js
+      fnm use --install-if-missing ${release.major}
 
-      snippets.CHOCO = dedent`
+      # verifies the right Node.js version is in the environment
+      node -v # should print \`${release.versionWithPrefix}\`
+
+      # verifies the right NPM version is in the environment
+      npm -v # should print \`${release.npm}\``;
+
+    snippets.CHOCO = dedent`
       # download and install Node.js
       choco install nodejs${release.isLts ? '-lts' : ''} --version="${release.version}"
 
@@ -85,9 +86,6 @@ export const getNodeDownloadSnippet = (release: NodeRelease, os: UserOS) => {
 
       # verifies the right NPM version is in the environment
       npm -v # should print \`${release.npm}\``;
-    // eslint-disable-next-line no-fallthrough
-    default:
-      break;
   }
 
   return snippets;
