cef3b976bfb029e7aa177a57592222d49094c0c4
An Tran <tquocan.vn@gmail.com>
2024-05-04 23:30:57 +0700

    fix: correct same level side navigation on breadcrumbs

--- components/withBreadcrumbs.tsx
+++ components/withBreadcrumbs.tsx
@@ -8,22 +8,19 @@ import { useClientContext, useMediaQuery, useSiteNavigation } from '@/hooks';
 import type { NavigationKeys } from '@/types';
 import { dashToCamelCase } from '@/util/stringUtils';
 
-const WithBreadcrumbs: FC = () => {
-  const { navigationItems, getSideNavigation } = useSiteNavigation();
+type WithBreadcrumbsProps = {
+  navKeys?: Array<NavigationKeys>;
+};
+
+const WithBreadcrumbs: FC<WithBreadcrumbsProps> = ({ navKeys = [] }) => {
+  const { getSideNavigation } = useSiteNavigation();
   const { pathname } = useClientContext();
   const isMobileScreen = useMediaQuery('(max-width: 639px)');
 
   const maxLength = isMobileScreen ? 2 : 4;
 
   const getBreadrumbs = () => {
-    const [navigationKey] =
-      navigationItems.find(([, item]) => pathname.includes(item.link)) || [];
-
-    if (navigationKey === undefined) {
-      return [];
-    }
-
-    const navigationTree = getSideNavigation([navigationKey as NavigationKeys]);
+    const navigationTree = getSideNavigation(navKeys);
 
     const pathList = pathname
       .split('/')
@@ -34,9 +31,14 @@ const WithBreadcrumbs: FC = () => {
 
     // Reduce the pathList to a breadcrumbs array by finding each path in the current navigation layer,
     // updating the currentNode to the found node's items(next layer) for the next iteration.
-    return pathList.reduce((breadcrumbs, path) => {
+    return pathList.reduce((breadcrumbs, path, index) => {
       const nodeWithCurrentPath = currentNode.find(
-        ([nodePath]) => nodePath === path
+        ([nodePath, entry]) =>
+          nodePath === path &&
+          (index === pathList.length - 1 ||
+            entry.items.some(
+              ([childPath]) => childPath === pathList[index + 1]
+            ))
       );
 
       if (nodeWithCurrentPath) {
