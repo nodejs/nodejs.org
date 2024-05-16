ca0f314cfe361da528ea85261a1122bac9c8b495
abizek <abishekilango@protonmail.com>
2024-05-16 12:43:59 +0530

    docs: Add comment explaining eslint-disable-next-line
    Signed-off-by: abizek <abishekilango@protonmail.com>

--- hooks/react-client/useNavigationState.ts
+++ hooks/react-client/useNavigationState.ts
@@ -33,6 +33,7 @@ const useNavigationState = <T extends HTMLElement>(
 
       return () => element.removeEventListener('scroll', handleScroll);
     }
+    // We need this effect to run only on mount
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 };
