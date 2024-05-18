97b851f5328e7f0c5bf76938e67b7c3d6e8440f2
Abishek Ilango <abishekilango@protonmail.com>
2024-05-18 23:27:36 +0530

    feat: Navigation state provider and hook (#6709)
    * feat: Navigation state provider and hook
    
    Temporary workaround for #6409
    Get more info here - #6675
    
    Signed-off-by: abizek <abishekilango@protonmail.com>
    
    * refactor: Navigation state hook effect
    
    Signed-off-by: abizek <abishekilango@protonmail.com>
    
    * docs: Add comment explaining eslint-disable-next-line
    
    Signed-off-by: abizek <abishekilango@protonmail.com>
    
    ---------
    
    Signed-off-by: abizek <abishekilango@protonmail.com>

--- layouts/Base.tsx
+++ layouts/Base.tsx
@@ -2,13 +2,16 @@
 
 import type { FC, PropsWithChildren } from 'react';
 
+import { NavigationStateProvider } from '@/providers/navigationStateProvider';
 import { NotificationProvider } from '@/providers/notificationProvider';
 
 import styles from './layouts.module.css';
 
 const BaseLayout: FC<PropsWithChildren> = ({ children }) => (
   <NotificationProvider viewportClassName="absolute bottom-0 right-0 list-none">
-    <div className={styles.baseLayout}>{children}</div>
+    <NavigationStateProvider>
+      <div className={styles.baseLayout}>{children}</div>
+    </NavigationStateProvider>
   </NotificationProvider>
 );
 
