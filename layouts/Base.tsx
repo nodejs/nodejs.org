cc2318e46ce1e59f71c6a095aaf0da279df31f2b
TenzDelek <122612557+TenzDelek@users.noreply.github.com>
2024-05-27 20:50:38 +0530

    Fix: positioning of the toast notification (copy to clipboard) (#6737)
    fix:positioning of the toast notification

--- layouts/Base.tsx
+++ layouts/Base.tsx
@@ -8,7 +8,7 @@ import { NotificationProvider } from '@/providers/notificationProvider';
 import styles from './layouts.module.css';
 
 const BaseLayout: FC<PropsWithChildren> = ({ children }) => (
-  <NotificationProvider viewportClassName="absolute bottom-0 right-0 list-none">
+  <NotificationProvider viewportClassName="fixed bottom-0 right-0 list-none">
     <NavigationStateProvider>
       <div className={styles.baseLayout}>{children}</div>
     </NavigationStateProvider>
