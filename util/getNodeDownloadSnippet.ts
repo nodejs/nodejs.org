ed9625b421742c0f55d47f20b8f3540c6e0a0ef5
NotYourAverageGamer <104248676+NotYourAverageGamer@users.noreply.github.com>
2024-06-06 22:23:55 +1000

    Revise Download instructions for Unix & Fix Grammar in CONTRIBUTING.md (#6769)
    * Update CONTRIBUTING.md
    
    Signed-off-by: NotYourAverageGamer <104248676+NotYourAverageGamer@users.noreply.github.com>
    
    * Update getNodeDownloadSnippet.ts
    
    updated download instructions to notify users on unix systems that restarting the terminal may be necessary to continue to step 2 after first installing nvm
    
    Signed-off-by: NotYourAverageGamer <104248676+NotYourAverageGamer@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: NotYourAverageGamer <104248676+NotYourAverageGamer@users.noreply.github.com>
    Co-authored-by: Michael Esteban <mickel13@gmail.com>

--- util/getNodeDownloadSnippet.ts
+++ util/getNodeDownloadSnippet.ts
@@ -36,7 +36,7 @@ export const getNodeDownloadSnippet = (release: NodeRelease, os: UserOS) => {
       # installs nvm (Node Version Manager)
       curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 
-      # download and install Node.js
+      # download and install Node.js (you may need to restart the terminal)
       nvm install ${release.major}
 
       # verifies the right Node.js version is in the environment
