ae5e1e3f5ac7ca10c6d9a5c2f8d25095580814b6
Michele Riva <ciao@micheleriva.it>
2024-07-04 15:55:56 +0200

    feat(orama): updates Orama and simplifies Orama Cloud sync (#6899)

--- apps/site/scripts/orama-search/sync-orama-cloud.mjs
+++ apps/site/scripts/orama-search/sync-orama-cloud.mjs
@@ -1,16 +1,18 @@
+import { CloudManager } from '@oramacloud/client';
+
 import { siteContent } from './get-documents.mjs';
 import { ORAMA_SYNC_BATCH_SIZE } from '../../next.constants.mjs';
 
-// The following follows the instructions at https://docs.oramasearch.com/cloud/data-sources/custom-integrations/webhooks
+// The following follows the instructions at https://docs.orama.com/cloud/data-sources/custom-integrations/webhooks
 
 const INDEX_ID = process.env.ORAMA_INDEX_ID;
 const API_KEY = process.env.ORAMA_SECRET_KEY;
-const ORAMA_API_BASE_URL = `https://api.oramasearch.com/api/v1/webhooks/${INDEX_ID}`;
 
-const oramaHeaders = {
-  'Content-Type': 'application/json',
-  Authorization: `Bearer ${API_KEY}`,
-};
+const oramaCloudManager = new CloudManager({
+  api_key: API_KEY,
+});
+
+const oramaIndex = oramaCloudManager.index(INDEX_ID);
 
 console.log(`Syncing ${siteContent.length} documents to Orama Cloud index`);
 
@@ -26,50 +28,9 @@ const runUpdate = async () => {
 
   console.log(`Sending ${batches.length} batches of ${batchSize} documents`);
 
-  await Promise.all(batches.map(insertBatch));
-};
-
-// We call the "notify" API to upsert the documents in the index.
-// Orama will keep a queue of all the documents we send, and will process them once we call the "deploy" API.
-// Full docs on the "notify" API: https://docs.oramasearch.com/cloud/data-sources/custom-integrations/webhooks#updating-removing-inserting-elements-in-a-live-index
-const insertBatch = async batch => {
-  const { ok, statusText } = await fetch(`${ORAMA_API_BASE_URL}/notify`, {
-    method: 'POST',
-    headers: oramaHeaders,
-    body: JSON.stringify({ upsert: batch }),
-  });
-
-  if (!ok) {
-    throw new Error(`Request to "/notify" failed with status: ${statusText}`);
-  }
-};
-
-// We call the "deploy" API to trigger a deployment of the index, which will process all the documents in the queue.
-// Full docs on the "deploy" API: https://docs.oramasearch.com/cloud/data-sources/custom-integrations/webhooks#deploying-the-index
-const triggerDeployment = async () => {
-  const { ok, statusText } = await fetch(`${ORAMA_API_BASE_URL}/deploy`, {
-    method: 'POST',
-    headers: oramaHeaders,
-  });
-
-  if (!ok) {
-    throw new Error(`Request to "/deploy" failed with status: ${statusText}`);
-  }
-};
-
-// We call the "snapshot" API to empty the index before inserting the new documents.
-// The "snapshot" API is typically used to replace the entire index with a fresh set of documents, but we use it here to empty the index.
-// This operation gets queued, so the live index will still be available until we call the "deploy" API and redeploy the index.
-// Full docs on the "snapshot" API: https://docs.oramasearch.com/cloud/data-sources/custom-integrations/webhooks#inserting-a-snapshot
-const emptyOramaIndex = async () => {
-  const { ok, statusText } = await fetch(`${ORAMA_API_BASE_URL}/snapshot`, {
-    method: 'POST',
-    headers: oramaHeaders,
-    body: JSON.stringify([]),
-  });
-
-  if (!ok) {
-    throw new Error(`Request to "/snapshot" failed with status: ${statusText}`);
+  for (const batch of batches) {
+    // In Orama, "update" is an upsert operation.
+    await oramaIndex.update(batch);
   }
 };
 
@@ -79,8 +40,8 @@ const emptyOramaIndex = async () => {
 // 3. Trigger a deployment
 // Once all these steps are done, the new documents will be available in the live index.
 // Allow Orama up to 1 minute to distribute the documents to all the 300+ nodes worldwide.
-await emptyOramaIndex();
+await oramaIndex.empty();
 await runUpdate();
-await triggerDeployment();
+await oramaIndex.deploy();
 
 console.log('Orama Cloud sync completed successfully!');
