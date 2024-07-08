186e0ad26776108390b94fb2df3c9bc39ee56153
Michele Riva <ciao@micheleriva.it>
2024-07-08 10:03:18 +0200

    feat(search): implements Orama searchbox

--- apps/site/components/Common/Search/index.tsx
+++ apps/site/components/Common/Search/index.tsx
@@ -1,61 +1,43 @@
-'use client';
+"use client";
 
-import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import classNames from 'classnames';
-import { useTranslations } from 'next-intl';
-import { useState, type FC } from 'react';
+import type { FC } from "react";
+import type {
+	RegisterSearchBoxProps,
+	RegisterSearchButtonProps,
+} from "@orama/searchbox";
+import { SearchBox, SearchButton as OramaSearchButton } from "@orama/searchbox";
+import { OramaClient } from "@oramacloud/client";
+import "@orama/searchbox/dist/index.css";
 
-import { WithSearchBox } from '@/components/Common/Search/States/WithSearchBox';
-import { useDetectOS } from '@/hooks';
-import { useKeyboardCommands } from '@/hooks/react-client';
+import {
+	ORAMA_CLOUD_ENDPOINT,
+	ORAMA_CLOUD_API_KEY,
+} from "@/next.constants.mjs";
 
-import styles from './index.module.css';
+const oramaClient = new OramaClient({
+	endpoint: ORAMA_CLOUD_ENDPOINT,
+	api_key: ORAMA_CLOUD_API_KEY,
+});
+
+const searchBoxProps: RegisterSearchBoxProps = {
+	oramaInstance: oramaClient,
+	colorScheme: "dark",
+	backdrop: true,
+	resultsMap: {
+		title: "pageSectionTitle",
+		description: "pageSectionContent",
+	},
+};
+
+const searchButtonProps: RegisterSearchButtonProps = {
+	colorScheme: "dark",
+};
 
 export const SearchButton: FC = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const t = useTranslations();
-  const openSearchBox = () => setIsOpen(true);
-  const closeSearchBox = () => setIsOpen(false);
-
-  useKeyboardCommands(cmd => {
-    switch (cmd) {
-      case 'cmd-k':
-        openSearchBox();
-        break;
-      case 'escape':
-        closeSearchBox();
-        break;
-      default:
-    }
-  });
-
-  const { os } = useDetectOS();
-
-  const osCommandKey = os === 'MAC' ? '⌘' : 'Ctrl';
-  const isOSLoading = os === 'LOADING';
-
-  return (
-    <>
-      <button
-        type="button"
-        onClick={openSearchBox}
-        className={styles.searchButton}
-        aria-label={t('components.search.searchBox.placeholder')}
-      >
-        <MagnifyingGlassIcon className={styles.magnifyingGlassIcon} />
-
-        {t('components.search.searchBox.placeholder')}
-        <kbd
-          title={`${osCommandKey} K`}
-          className={classNames(styles.shortcutIndicator, {
-            'opacity-0': isOSLoading,
-          })}
-        >
-          <abbr>{osCommandKey} K</abbr>
-        </kbd>
-      </button>
-
-      {isOpen ? <WithSearchBox onClose={closeSearchBox} /> : null}
-    </>
-  );
+	return (
+		<>
+			<OramaSearchButton {...searchButtonProps} />
+			<SearchBox {...searchBoxProps} />
+		</>
+	);
 };
