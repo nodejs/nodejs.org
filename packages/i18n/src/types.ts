43fe575a0189ca522e3dbad127ea3ea6e4c60876
Guilherme Araújo <arauujogui@gmail.com>
2024-08-18 15:58:41 -0300

    feat: create initial i18n package

---
+++ packages/i18n/src/types.ts
@@ -0,0 +1,10 @@
+export interface LocaleConfig {
+  code: string;
+  localName: string;
+  name: string;
+  langDir: string;
+  dateFormat: string;
+  hrefLang: string;
+  enabled: boolean;
+  default: boolean;
+}
