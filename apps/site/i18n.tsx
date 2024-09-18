dac4cb2d0427a5758a3874d2b15bc64f92ada95a
Jan Amann <jan@amann.work>
2024-09-18 15:16:47 +0200

    fix: Use default messages from `en` as fallback if messages are missing in another locale

--- apps/site/i18n.tsx
+++ apps/site/i18n.tsx
@@ -1,22 +1,28 @@
 import { importLocale } from '@node-core/website-i18n';
+import deepmerge from 'deepmerge';
 import { getRequestConfig } from 'next-intl/server';
 
 import { availableLocaleCodes } from '@/next.locales.mjs';
 
 // Loads the Application Locales/Translations Dynamically
 const loadLocaleDictionary = async (locale: string) => {
+  // This enables HMR on the English Locale, so that instant refresh
+  // happens while we add/change texts on the source locale
+  const defaultMessages = await import(
+    '@node-core/website-i18n/locales/en.json'
+  ).then(f => f.default);
+
   if (locale === 'en') {
-    // This enables HMR on the English Locale, so that instant refresh
-    // happens while we add/change texts on the source locale
-    return import('@node-core/website-i18n/locales/en.json').then(
-      f => f.default
-    );
+    return defaultMessages;
   }
 
   if (availableLocaleCodes.includes(locale)) {
     // Other languages don't really require HMR as they will never be development languages
     // so we can load them dynamically
-    return importLocale(locale);
+    const messages = await importLocale(locale);
+
+    // Use default messages as fallback
+    return deepmerge(defaultMessages, messages);
   }
 
   throw new Error(`Unsupported locale: ${locale}`);
