43e4e082b8fd7465288fdf8d3bb609003e12c166
Joe <joeeames@gmail.com>
2024-10-14 18:44:33 -0600

    fixed issues identified by maintainers

--- apps/site/components/Downloads/Release/ReleaseCodeBox.tsx
+++ apps/site/components/Downloads/Release/ReleaseCodeBox.tsx
@@ -1,9 +1,11 @@
 'use client';
 
-import { useTranslations, useLocale } from 'next-intl';
+import { useTranslations } from 'next-intl';
 import { useContext, useEffect, useState } from 'react';
 import type { FC } from 'react';
+import semVer from 'semver';
 
+import Banner from '@/components/Common/Banner';
 import CodeBox from '@/components/Common/CodeBox';
 import { ReleaseContext } from '@/providers/releaseProvider';
 import { shikiPromise, highlightToHtml } from '@/util/getHighlighter';
@@ -13,7 +15,7 @@ const memoizedShiki = shikiPromise.then(highlightToHtml);
 
 const ReleaseCodeBox: FC = () => {
   const { platform, os, release } = useContext(ReleaseContext);
-  const locale = useLocale();
+  // const locale = useLocale();
 
   const [code, setCode] = useState('');
   const t = useTranslations();
@@ -29,28 +31,14 @@ const ReleaseCodeBox: FC = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [release.versionWithPrefix, os, platform]);
 
-  const parseVersion = () => {
-    const version = release.versionWithPrefix.replace('v', '');
-    const versionParts = version.split('.');
-    return {
-      major: parseInt(versionParts[0]),
-      minor: parseInt(versionParts[1]),
-      patch: parseInt(versionParts[2]),
-    };
-  };
-
   const codeLanguage = os === 'WIN' ? 'PowerShell' : 'Bash';
-
+  //{parseVersion().major < 18 && (
   return (
     <div className="mb-2 mt-6 flex flex-col gap-2">
-      {parseVersion().major < 18 && (
-        <div
-          className="border-danger-400 bg-danger-100 relative rounded border px-4 py-3 text-red-700"
-          role="alert"
-        >
+      {semVer.lt(release.versionWithPrefix, '18.0.0') && (
+        <Banner type="error" link="/about/previous-releases/">
           {t('layouts.download.codeBox.unsupportedVersionWarning')}&nbsp;
-          <a href={`/${locale}/about/previous-releases/`}>Previous Releases</a>
-        </div>
+        </Banner>
       )}
       <CodeBox language={codeLanguage} className="min-h-[15.5rem]">
         <code dangerouslySetInnerHTML={{ __html: code }} />
