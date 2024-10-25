46a7836067cbd2f350a643e26e16827bc77fb220
Brian Muenzenmeyer <brian.muenzenmeyer@gmail.com>
2024-10-25 15:24:01 -0500

    adds link to github profile

--- apps/site/components/Common/AvatarGroup/Avatar/index.tsx
+++ apps/site/components/Common/AvatarGroup/Avatar/index.tsx
@@ -1,6 +1,8 @@
 import * as RadixAvatar from '@radix-ui/react-avatar';
 import type { FC } from 'react';
 
+import Link from '@/components/Link';
+
 import styles from './index.module.css';
 
 export type AvatarProps = {
@@ -11,16 +13,18 @@ export type AvatarProps = {
 
 const Avatar: FC<AvatarProps> = ({ src, alt, fallback }) => (
   <RadixAvatar.Root className={styles.avatarRoot}>
-    <RadixAvatar.Image
-      loading="lazy"
-      src={src}
-      alt={alt}
-      title={alt}
-      className={styles.avatar}
-    />
-    <RadixAvatar.Fallback delayMs={500} className={styles.avatar}>
-      {fallback}
-    </RadixAvatar.Fallback>
+    <Link href={`https://github.com/${alt}`}>
+      <RadixAvatar.Image
+        loading="lazy"
+        src={src}
+        alt={alt}
+        title={alt}
+        className={styles.avatar}
+      />
+      <RadixAvatar.Fallback delayMs={500} className={styles.avatar}>
+        {fallback}
+      </RadixAvatar.Fallback>
+    </Link>
   </RadixAvatar.Root>
 );
 
