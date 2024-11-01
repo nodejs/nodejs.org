fcff96f66764abf374739ef725141ace45058be3
Caner Akdas <canerakdas@gmail.com>
2024-11-02 01:45:32 +0300

    refactor: Moving the version support button to the footer (#7156)
    refactor: moving the version support button to the footer

--- apps/site/components/Common/ActiveLink/index.tsx
+++ apps/site/components/Common/ActiveLink/index.tsx
@@ -5,7 +5,6 @@ import type { ComponentProps, FC } from 'react';
 
 import Link from '@/components/Link';
 import { usePathname } from '@/navigation.mjs';
-import { VERSION_SUPPORT_SHORTCUT } from '@/next.constants.mjs';
 
 type ActiveLocalizedLinkProps = ComponentProps<typeof Link> & {
   activeClassName?: string;
@@ -27,9 +26,7 @@ const ActiveLink: FC<ActiveLocalizedLinkProps> = ({
       ? // When using allowSubPath we want only to check if
         // the current pathname starts with the utmost upper level
         // of an href (e.g. /docs/...)
-        pathname.startsWith(`/${href.toString().split('/')[1]}`) &&
-        // but not when this link is for the deep link shortcut to previous releases
-        href.toString() !== VERSION_SUPPORT_SHORTCUT
+        pathname.startsWith(`/${href.toString().split('/')[1]}`)
       : href.toString() === pathname,
   });
 
