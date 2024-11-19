93eaf71e220f5f8fa6be7f6d725ccfda1cd32bad
CodingRule <ioumih32@gmail.com>
2024-11-19 04:30:20 -0800

    Update detectOS.ts
    Signed-off-by: CodingRule <ioumih32@gmail.com>

--- apps/site/util/detectOS.ts
+++ apps/site/util/detectOS.ts
@@ -1,21 +1,16 @@
 import type { UserOS } from '@/types/userOS';
 
 export const detectOsInUserAgent = (userAgent: string | undefined): UserOS => {
-  const osMatch = userAgent?.match(/(Win|Mac|Linux)/);
-  switch (osMatch && osMatch[1]) {
-    case 'Win':
-      return 'WIN';
-    case 'Mac':
-      return 'MAC';
-    case 'Linux':
-      return 'LINUX';
-    case 'AIX':
-      return 'AIX';
-    default:
-      return 'OTHER';
-  }
+  const osMatch = userAgent?.match(/(Win|Mac|Linux|AIX)/);
+  const osMap: Record<string, UserOS> = {
+    Win: 'WIN',
+    Mac: 'MAC',
+    Linux: 'LINUX',
+    AIX: 'AIX',
+  };
+  return osMap[osMatch?.[1] ?? ''] || 'OTHER';
 };
 
-// Since `navigator.appVersion` is deprecated, we use the `userAgent``
+// Since navigator.appVersion is deprecated, we use the userAgent
 // https://developer.mozilla.org/en-US/docs/Web/API/Navigator/appVersion
 export const detectOS = (): UserOS => detectOsInUserAgent(navigator?.userAgent);
