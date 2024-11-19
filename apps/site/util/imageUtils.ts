4d16aa856bcc89690faf4e3ac1acbc2bf7300084
Caner Akdas <canerakdas@gmail.com>
2024-11-19 23:18:09 +0300

    chore: review updates

--- apps/site/util/imageUtils.ts
+++ apps/site/util/imageUtils.ts
@@ -1,13 +1,20 @@
-export const isSvgImage = (src: string) => {
-  let isSvg = false;
+/**
+ * Checks if the given source string points to an SVG image.
+ *
+ * This function examines the base part of the provided string (ignoring query parameters)
+ * to determine if it ends with the `.svg` extension.
+ *
+ * @param src - The URL or string representing the source of the image.
+ * @returns `true` if the source points to an SVG image, otherwise `false`.
+ */
+export const isSvgImage = (src: string): boolean => {
+  // Split the source string at the '?' character to separate the main path from query parameters
+  const [image] = src.split('?');
 
-  if (src.includes('.svg')) {
-    const image = new URL(src);
-
-    if (image.pathname.endsWith('.svg')) {
-      isSvg = true;
-    }
+  // Check if the base path (before any query parameters) ends with '.svg'
+  if (image.endsWith('.svg')) {
+    return true;
   }
 
-  return isSvg;
+  return false;
 };
