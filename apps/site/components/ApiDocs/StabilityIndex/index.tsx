61fd52e3c6ad2c113cb18e9a0ddf165f386d412d
Augustin Mauroy <augustin.mauroy@outlook.fr>
2024-12-22 22:17:01 +0100

    feat(StabilityIndex): introduce

---
+++ apps/site/components/ApiDocs/StabilityIndex/index.tsx
@@ -0,0 +1,19 @@
+import classNames from 'classnames';
+import type { FC, PropsWithChildren } from 'react';
+
+import styles from './index.module.css';
+
+type StabilityIndexProps = PropsWithChildren<{
+  level: number;
+}>;
+
+const StabilityIndex: FC<StabilityIndexProps> = ({ level, children }) => (
+    <div className={classNames(styles.stabilityIndex, styles[`stabilityLevel${level}`])}>
+      <span
+        className={styles.indexLelvel}
+      >{level}</span>{children}
+    </div>
+  );
+
+
+export default StabilityIndex;
