8172f30fac9ad61d7157386e161a2f14a1a65107
@mootoday <154029656+mootoday@users.noreply.github.com>
2024-12-28 22:54:54 -0800

    Update apps/site/components/Icons/Platform/Devbox.tsx
    Co-authored-by: Michael Esteban <mickel13@gmail.com>
    Signed-off-by: @mootoday <154029656+mootoday@users.noreply.github.com>

--- apps/site/components/Icons/Platform/Devbox.tsx
+++ apps/site/components/Icons/Platform/Devbox.tsx
@@ -1,6 +1,6 @@
 import type { FC, SVGProps } from 'react';
 
-const Choco: FC<SVGProps<SVGSVGElement>> = props => (
+const Devbox: FC<SVGProps<SVGSVGElement>> = props => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width="32"
