9eac78afab025bb4e6dfb133eb9268526148794e
Claudio W <cwunder@gnome.org>
2025-01-26 22:07:40 +0000

    chore: better links for source

--- apps/site/components/Downloads/DownloadLink.tsx
+++ apps/site/components/Downloads/DownloadLink.tsx
@@ -2,23 +2,32 @@
 
 import type { FC, PropsWithChildren } from 'react';
 
+import LinkWithArrow from '@/components/LinkWithArrow';
 import { useClientContext } from '@/hooks';
 import type { NodeRelease } from '@/types';
+import type { DownloadKind } from '@/util/getNodeDownloadUrl';
 import { getNodeDownloadUrl } from '@/util/getNodeDownloadUrl';
 import { getUserPlatform } from '@/util/getUserPlatform';
 
-type DownloadLinkProps = { release: NodeRelease };
+type DownloadLinkProps = { release: NodeRelease; kind?: DownloadKind };
 
 const DownloadLink: FC<PropsWithChildren<DownloadLinkProps>> = ({
   release: { versionWithPrefix },
+  kind = 'installer',
   children,
 }) => {
   const { os, bitness, architecture } = useClientContext();
 
   const platform = getUserPlatform(architecture, bitness);
-  const downloadLink = getNodeDownloadUrl(versionWithPrefix, os, platform);
 
-  return <a href={downloadLink}>{children}</a>;
+  const downloadLink = getNodeDownloadUrl(
+    versionWithPrefix,
+    os,
+    platform,
+    kind
+  );
+
+  return <LinkWithArrow href={downloadLink}>{children}</LinkWithArrow>;
 };
 
 export default DownloadLink;
