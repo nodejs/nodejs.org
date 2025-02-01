bcdfdfd7a5d3c8188ef753fdc5701eeb529da4dc
Michael Esteban <mickel13@gmail.com>
2025-02-01 15:56:43 +1000

    remove spaces

--- apps/site/app/[locale]/next-data/og/[category]/[title]/route.tsx
+++ apps/site/app/[locale]/next-data/og/[category]/[title]/route.tsx
@@ -55,7 +55,7 @@ export const generateStaticParams = async () => [
   {
     locale: defaultLocale.code,
     category: 'announcement',
-    title: encodeURIComponent('Run JavaScript Everywhere'),
+    title: 'RunJavaScriptEverywhere',
   },
 ];
 
