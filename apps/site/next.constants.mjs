31f7b039bc421f042f483faaf5ac768b6e6b8744
Jordan Harband <ljharb@gmail.com>
2025-02-02 13:49:25 -0800

    fix: don’t hardcode 3000; use the actual port

--- apps/site/next.constants.mjs
+++ apps/site/next.constants.mjs
@@ -98,7 +98,7 @@ export const NEXT_DATA_URL = process.env.NEXT_PUBLIC_DATA_URL
   ? process.env.NEXT_PUBLIC_DATA_URL
   : VERCEL_ENV
     ? `${BASE_URL}${BASE_PATH}/en/next-data/`
-    : `http://localhost:3000/en/next-data/`;
+    : `http://localhost:${process.env.PORT ?? 3000}/en/next-data/`;
 
 /**
  * This ReGeX is used to remove the `index.md(x)` suffix of a name and to remove
