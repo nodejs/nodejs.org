b1ccc8ea2ceb039b123635ece77a81c95fcadaf1
John Gee <john@ruru.gen.nz>
2025-02-17 22:08:08 +1300

    Tweak wording

--- apps/site/snippets/en/download/n.bash
+++ apps/site/snippets/en/download/n.bash
@@ -1,5 +1,5 @@
 # For more about n-install: https://github.com/mklement0/n-install
-# For more about n, including other ways to install n: https://github.com/tj/n
+# For more about n and other ways to install: https://github.com/tj/n
 
 # Download and install n and Node.js with a single command:
 curl -L https://bit.ly/n-install | bash -s ${props.release.major}
