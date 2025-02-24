53914ec2f633b5477eabc063cfa15f08b1030feb
John Gee <john@ruru.gen.nz>
2025-02-24 19:43:39 +1300

    Remove extra links to n and n-install home pages

--- apps/site/snippets/en/download/n.bash
+++ apps/site/snippets/en/download/n.bash
@@ -1,7 +1,4 @@
-# For more about n-install: https://github.com/mklement0/n-install
-# For more about n and other ways to install: https://github.com/tj/n
-
-# Download and install n and Node.js with a single command:
+# Download and install n and Node.js:
 curl -fsSL https://raw.githubusercontent.com/mklement0/n-install/stable/bin/n-install | bash -s ${props.release.major}
 
 # Download and install Node.js, done for you by n-install:
