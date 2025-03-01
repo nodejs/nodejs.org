9b42280e4c71909feb0ba39061d6023040e08fd3
Claudio Wunder <cwunder@hubspot.com>
2025-03-01 15:24:55 -0300

    chore: simplify link props

--- apps/site/components/Link.tsx
+++ apps/site/components/Link.tsx
@@ -2,11 +2,11 @@ import type { FC, HTMLProps } from 'react';
 
 import { Link as LocalizedLink } from '@/navigation.mjs';
 
-type LinkProps = Omit<HTMLProps<HTMLAnchorElement>, 'href'> & {
-  href?: string;
-};
-
-const Link: FC<LinkProps> = ({ children, href, ...props }) => {
+const Link: FC<HTMLProps<HTMLAnchorElement>> = ({
+  children,
+  href,
+  ...props
+}) => {
   if (!href || href.toString().startsWith('http')) {
     return (
       <a href={href} {...props}>
