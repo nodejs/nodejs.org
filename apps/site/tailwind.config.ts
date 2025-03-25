74fba34c8c35fc1b902bc32365d9b4ee66e8449e
avivkeller <me@aviv.sh>
2025-03-25 07:58:51 -0400

    fix: apply tailwind to `tsx` files in `ui-components`

--- apps/site/tailwind.config.ts
+++ apps/site/tailwind.config.ts
@@ -1,1 +1,5 @@
-export { default } from '@node-core/ui-components/tailwind.config';
+import twConfig from '@node-core/ui-components/tailwind.config';
+
+twConfig.content.push('../../packages/ui-components/**/*.tsx');
+
+export default twConfig;
