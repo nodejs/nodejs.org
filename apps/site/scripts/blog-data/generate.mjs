dfb486c079b6638fefba9804d6738bd2944e7481
Dario Piotrowicz <dario@cloudflare.com>
2025-04-02 22:16:01 +0100

    fixup! remove blog-data endpoint and use public json file instead
    remove unnecessary chokidar

--- apps/site/scripts/blog-data/generate.mjs
+++ apps/site/scripts/blog-data/generate.mjs
@@ -2,26 +2,13 @@ import { writeFileSync } from 'node:fs';
 import { dirname } from 'node:path';
 import { fileURLToPath } from 'node:url';
 
-import { watch } from 'chokidar';
-
 import generateBlogData from '../../next-data/generators/blogData.mjs';
 
-await generateAndSaveBlogData();
+const blogData = await generateBlogData();
 
-const watchMode =
-  process.argv.includes('--watch') || process.argv.includes('-w');
-
-if (watchMode) {
-  watch('pages/en', { ignoreInitial: true }).on('all', generateAndSaveBlogData);
-}
-
-async function generateAndSaveBlogData() {
-  const blogData = await generateBlogData();
-
-  const __dirname = dirname(fileURLToPath(import.meta.url));
-  writeFileSync(
-    `${__dirname}/../../public/blog-data.json`,
-    JSON.stringify(blogData),
-    'utf8'
-  );
-}
+const __dirname = dirname(fileURLToPath(import.meta.url));
+writeFileSync(
+  `${__dirname}/../../public/blog-data.json`,
+  JSON.stringify(blogData),
+  'utf8'
+);
