40550edb729d0a88dd1de9305ea770c9506cc3ad
avivkeller <me@aviv.sh>
2025-04-05 12:24:56 -0400

    use node --run

--- tests/utilities.mjs
+++ tests/utilities.mjs
@@ -1,3 +1,6 @@
+// This helper checks if a given element is visible.
+// Asserting the output of this is the equal of running
+// expect(element).toBeVisible()
 export function isVisible(element) {
   while (element) {
     const style = element.ownerDocument.defaultView.getComputedStyle(element);
