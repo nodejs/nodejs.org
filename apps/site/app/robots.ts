820c768585e2c06133a79fb31924d5d4c447eae5
Claudio Wunder <cwunder@gnome.org>
2025-04-10 15:23:33 +0200

    chore: turbopack fixes

--- apps/site/app/robots.ts
+++ apps/site/app/robots.ts
@@ -16,4 +16,9 @@ export default robots;
 
 // Enforces that this route is used as static rendering
 // @see https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config#dynamic
-export const dynamic = 'error';
+export const dynamic = 'force-static';
+
+// Ensures that this endpoint is invalidated and re-executed every X minutes
+// so that when new deployments happen, the data is refreshed
+// @see https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config#revalidate
+export const revalidate = false;
