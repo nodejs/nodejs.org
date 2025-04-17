dd3f334e0a43ad73e4bf87315f20f1324ac069a7
Guilherme Araújo <arauujogui@gmail.com>
2025-04-17 16:02:04 -0300

    refactor: small nit

--- apps/site/util/getReleaseAnnounceLink.ts
+++ apps/site/util/getReleaseAnnounceLink.ts
@@ -3,12 +3,11 @@ import semVer from 'semver';
 import type { NodeRelease } from '@/types';
 
 export const getReleaseAnnounceLink = (release: NodeRelease) => {
-  // No release announcement for versions <= 18
-  if (semVer.satisfies(release.version, '<=18')) {
-    return null;
-  }
-
-  if (release.status === 'Current') {
+  // No release announcements for the current version or for versions ≤ 18
+  if (
+    semVer.satisfies(release.version, '<=18') ||
+    release.status === 'Current'
+  ) {
     return null;
   }
 
