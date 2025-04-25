e19099e2562f6f6e2e908acb1b5eda6e1a6fed1e
Jonathan Netley <jpbnetley@gmail.com>
2025-04-25 15:58:40 +0200

    docs: nvm references latest version (#7684)

--- apps/site/snippets/en/download/nvm.bash
+++ apps/site/snippets/en/download/nvm.bash
@@ -1,5 +1,5 @@
 # Download and install nvm:
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 
 # in lieu of restarting the shell
 \. "$HOME/.nvm/nvm.sh"
