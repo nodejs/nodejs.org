442a25a857a7f65916622797e3e11c7d9bf2e6e3
Claudio Wunder <cwunder@gnome.org>
2025-05-03 01:47:51 +0200

    chore: local fonts and removed some weird files

--- apps/site/next.fonts.ts
+++ apps/site/next.fonts.ts
@@ -1,21 +1,19 @@
-import { Open_Sans, IBM_Plex_Mono } from 'next/font/google';
+import localFont from 'next/font/local';
 
 // This configures the Next.js Font for Open Sans
 // We then export a variable and class name to be used
 // within Tailwind (tailwind.config.ts) and Storybook (preview.js)
-export const OPEN_SANS = Open_Sans({
-  weight: ['300', '400', '600', '700'],
+export const OPEN_SANS = localFont({
+  src: './public/static/fonts/open-sans.woff2',
   display: 'fallback',
-  subsets: ['latin'],
   variable: '--font-open-sans',
 });
 
 // This configures the Next.js Font for IBM Plex Mono
 // We then export a variable and class name to be used
 // within Tailwind (tailwind.config.ts) and Storybook (preview.js)
-export const IBM_PLEX_MONO = IBM_Plex_Mono({
-  weight: ['400', '600'],
-  subsets: ['latin'],
-  fallback: ['ui-monospace'],
+export const IBM_PLEX_MONO = localFont({
+  src: './public/static/fonts/ibm-plex-mono.woff2',
+  display: 'fallback',
   variable: '--font-ibm-plex-mono',
 });
