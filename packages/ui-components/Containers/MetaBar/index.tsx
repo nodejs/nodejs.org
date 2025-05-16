e752090853464ce7a9493b02eff9361ae61feae4
Utkarsh Pandey <utk2665@gmail.com>
2025-05-17 03:38:40 +0530

    Restored packages/ui-components/Containers/MetaBar/index.tsx

--- packages/ui-components/Containers/MetaBar/index.tsx
+++ packages/ui-components/Containers/MetaBar/index.tsx
@@ -49,12 +49,12 @@ const MetaBar: FC<MetaBarProps> = ({
               <ol>
                 {filteredHeadings.map(head => (
                   <li
-                    key={head?.data?.id}
+                    key={head.value}
                     className={
                       head.depth === 3 ? 'pl-2' : head.depth === 4 ? 'pl-4' : ''
                     }
                   >
-                    <Component href={`#${head.data.id}`}>
+                    <Component href={`#${head?.data?.id}`}>
                       {' '}
                       {head.value}
                     </Component>
