c3c09654635824bbcbc1d794f23ee8fd652ef0af
avivkeller <me@aviv.sh>
2025-05-20 10:17:54 -0400

    chore(ui): move `ArticleLayout` to `ui-components`

--- packages/ui-components/Containers/Article/index.tsx
+++ packages/ui-components/Containers/Article/index.tsx
@@ -1,6 +1,6 @@
 import type { FC, PropsWithChildren } from 'react';
 
-import styles from './layouts.module.css';
+import styles from './index.module.css';
 
 const ArticleLayout: FC<PropsWithChildren> = ({ children }) => (
   <div className={styles.articleLayout}>{children}</div>
