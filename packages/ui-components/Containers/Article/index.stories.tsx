8161d6df5c8fb10ad0e2be56026a77ce14758ee4
Aviv Keller <me@aviv.sh>
2025-05-22 04:34:20 -0400

    chore(ui): move `ArticleLayout` to `ui-components` (#7762)
    * chore(ui): move `ArticleLayout` to `ui-components`
    
    * fixup! chore(ui): move `ArticleLayout` to `ui-components`
    
    * add story

---
+++ packages/ui-components/Containers/Article/index.stories.tsx
@@ -0,0 +1,39 @@
+import type { Meta as MetaObj, StoryObj } from '@storybook/react';
+
+import Article from '.';
+
+type Story = StoryObj<typeof Article>;
+type Meta = MetaObj<typeof Article>;
+
+const Sidebar = () => (
+  <div className="text-center">
+    <h3>Sidebar</h3>
+    <ul>
+      <li>Navigation Item 1</li>
+      <li>Navigation Item 2</li>
+      <li>Navigation Item 3</li>
+      <li>Navigation Item 4</li>
+    </ul>
+  </div>
+);
+
+export const Default: Story = {
+  args: {
+    children: (
+      <>
+        <Sidebar />
+        <div>
+          <main>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+          </main>
+          <Sidebar />
+        </div>
+      </>
+    ),
+  },
+};
+
+export default { component: Article } as Meta;
