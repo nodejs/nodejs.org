e3f500c0b7fa6c0f73f4d33e4046ffb6e59823c7
Harkunwar Kochar <10580591+Harkunwar@users.noreply.github.com>
2025-06-15 14:26:34 -0700

    feat(ui-components): add change history component

---
+++ packages/ui-components/Common/ChangeHistory/index.tsx
@@ -0,0 +1,80 @@
+import { ChevronDownIcon, ClockIcon } from '@heroicons/react/24/outline';
+import type { FC } from 'react';
+
+import styles from './index.module.css';
+
+type ChangeHistoryProps = React.JSX.IntrinsicElements['div'] & {
+  label: string;
+  changes: Array<{
+    versions: Array<string>;
+    label: string;
+    url?: string;
+  }>;
+  align?: 'left' | 'right';
+};
+
+const ChangeHistory: FC<ChangeHistoryProps> = ({
+  label = 'History',
+  changes = [],
+  align = 'right',
+  className,
+  'aria-label': ariaLabel = label,
+  ...props
+}) => (
+  <div className={`${styles.changeHistory} ${className}`} {...props}>
+    {changes.length > 0 ? (
+      <details className="group">
+        <summary className={styles.summary} role="button" aria-haspopup="true">
+          <ClockIcon className={styles.icon} aria-hidden="true" />
+          <span>{label}</span>
+          <ChevronDownIcon className={styles.chevron} aria-hidden="true" />
+        </summary>
+        <div
+          className={`${styles.dropdownContent} ${
+            align === 'left' ? styles.alignLeft : styles.alignRight
+          }`}
+          role="menu"
+          aria-label={ariaLabel}
+        >
+          <div className={styles.dropdownContentInner}>
+            {changes.map((change, index) => {
+              const content = (
+                <>
+                  <div className={styles.dropdownLabel}>{change.label}</div>
+                  <div className={styles.dropdownVersions}>
+                    {change.versions.join(', ')}
+                  </div>
+                </>
+              );
+
+              return (
+                <a
+                  key={index}
+                  href={change.url}
+                  className={styles.dropdownItem}
+                  role="menuitem"
+                  tabIndex={0}
+                  aria-label={`${change.label}: ${change.versions.join(', ')}`}
+                >
+                  {content}
+                </a>
+              );
+            })}
+          </div>
+        </div>
+      </details>
+    ) : (
+      <div
+        className={`${styles.summary} ${styles.disabled}`}
+        role="button"
+        aria-disabled="true"
+      >
+        <ClockIcon className={styles.icon} aria-hidden="true" />
+        <span>History</span>
+        <ChevronDownIcon className={styles.chevron} aria-hidden="true" />
+      </div>
+    )}
+  </div>
+);
+
+export default ChangeHistory;
