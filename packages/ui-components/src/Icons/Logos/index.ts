3d2fe0520a05458cf1394ba1c0f625aae4c9faaf
avivkeller <me@aviv.sh>
2025-07-08 11:04:07 -0400

    remove unused logos

--- packages/ui-components/src/Icons/Logos/index.ts
+++ packages/ui-components/src/Icons/Logos/index.ts
@@ -1,17 +1,4 @@
-import JsGreen from '#ui/Icons/Logos/JsGreen';
 import JsWhite from '#ui/Icons/Logos/JsWhite';
 import Nodejs from '#ui/Icons/Logos/Nodejs';
-import NodejsStackedBlack from '#ui/Icons/Logos/NodejsStackedBlack';
-import NodejsStackedDark from '#ui/Icons/Logos/NodejsStackedDark';
-import NodejsStackedLight from '#ui/Icons/Logos/NodejsStackedLight';
-import NodejsStackedWhite from '#ui/Icons/Logos/NodejsStackedWhite';
 
-export {
-  JsGreen,
-  JsWhite,
-  Nodejs,
-  NodejsStackedBlack,
-  NodejsStackedDark,
-  NodejsStackedLight,
-  NodejsStackedWhite,
-};
+export { JsWhite, Nodejs };
