77ae5e9c4c519e847dc5569ee4c91a30c622092d
avivkeller <me@aviv.sh>
2025-07-08 16:20:53 -0400

    update ui-components

--- packages/ui-components/__design__/node-logos.stories.tsx
+++ packages/ui-components/__design__/node-logos.stories.tsx
@@ -2,20 +2,18 @@ import type { Meta as MetaObj, StoryObj } from '@storybook/react';
 
 import { JsWhite, Nodejs } from '#ui/Icons/Logos';
 
-export const HorizontalLogo: StoryObj = {
-  render: () => <Nodejs width={267} height={80} />,
-};
-
-export const PrideLogo: StoryObj = {
-  render: () => <Nodejs variant="pride" width={267} height={80} />,
-};
-
-export const JSSymbols: StoryObj = {
+export const NodejsLogos: StoryObj = {
   render: () => (
-    <div className="flex flex-row gap-4">
-      <JsWhite width={30} height={30} />
+    <div className="flex flex-col gap-6">
+      {(['default', 'pride'] as const).map(variant => (
+        <Nodejs key={variant} width={267} height={80} variant={variant} />
+      ))}
+      <div className="flex flex-row gap-4">
+        <JsWhite width={30} height={30} />
+      </div>
     </div>
   ),
+  name: 'Node.js Logos',
 };
 
 export default { title: 'Design System' } as MetaObj;
