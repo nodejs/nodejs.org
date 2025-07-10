4d3182b1699ee0824d1632746808071522f65023
Aviv Keller <me@aviv.sh>
2025-07-10 07:20:14 -0400

    feat(ui): add NotificationProvider (#7967)

--- apps/site/hooks/react-client/useNotification.ts
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import { useContext } from 'react';
-
-import { NotificationDispatch } from '#site/providers/notificationProvider';
-
-const useNotification = () => useContext(NotificationDispatch);
-
-export default useNotification;
