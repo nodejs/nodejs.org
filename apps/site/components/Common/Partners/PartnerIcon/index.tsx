6a5568ccc45649e8f7570ca864adf2ba00569183
Sebastian Beltran <bjohansebas@gmail.com>
2025-07-21 23:13:28 +0000

    ui: add name tooltip for partnerIcon

--- apps/site/components/Common/Partners/PartnerIcon/index.tsx
+++ apps/site/components/Common/Partners/PartnerIcon/index.tsx
@@ -1,4 +1,5 @@
 import Skeleton from '@node-core/ui-components/Common/Skeleton';
+import Tooltip from '@node-core/ui-components/Common/Tooltip';
 import type { ComponentProps, FC } from 'react';
 import { cloneElement } from 'react';
 
@@ -9,16 +10,18 @@ import Button from '../../Button';
 
 type ParnetsIconProps = Partners & ComponentProps<typeof Skeleton>;
 
-const PartnersIcon: FC<ParnetsIconProps> = ({ href, logo, loading }) => {
+const PartnersIcon: FC<ParnetsIconProps> = ({ name, href, logo, loading }) => {
   return (
     <Skeleton loading={loading} className="h-9 w-9 p-2">
-      <Button kind="secondary" href={href} className={style.partnerIcon}>
-        {cloneElement(logo, {
-          className: 'h-4 w-auto',
-          width: 'auto',
-          height: '16px',
-        })}
-      </Button>
+      <Tooltip content={name}>
+        <Button kind="secondary" href={href} className={style.partnerIcon}>
+          {cloneElement(logo, {
+            className: 'h-4 w-auto',
+            width: 'auto',
+            height: '16px',
+          })}
+        </Button>
+      </Tooltip>
     </Skeleton>
   );
 };
