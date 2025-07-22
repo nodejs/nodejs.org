8302e8a46508dfca5d024e7dc542af2b58913b6e
Sebastian Beltran <bjohansebas@gmail.com>
2025-07-22 15:36:47 +0000

    chore: add partner list on security blog post

--- apps/site/layouts/Post.tsx
+++ apps/site/layouts/Post.tsx
@@ -1,6 +1,7 @@
 import Preview from '@node-core/ui-components/Common/Preview';
 import type { FC, PropsWithChildren } from 'react';
 
+import Link from '#site/components/Link';
 import WithAvatarGroup from '#site/components/withAvatarGroup';
 import WithBlogCrossLinks from '#site/components/withBlogCrossLinks';
 import WithFooter from '#site/components/withFooter';
@@ -11,6 +12,7 @@ import { mapAuthorToCardAuthors } from '#site/util/author';
 import { mapBlogCategoryToPreviewType } from '#site/util/blog';
 
 import styles from './layouts.module.css';
+import PartnersLogoList from '../components/Common/Partners/PartnersLogoList';
 
 const PostLayout: FC<PropsWithChildren> = ({ children }) => {
   const { frontmatter } = useClientContext();
@@ -37,7 +39,17 @@ const PostLayout: FC<PropsWithChildren> = ({ children }) => {
             <Preview title={frontmatter.title!} type={type} />
 
             {children}
-
+            {type === 'vulnerability' && (
+              <section className="flex-col! flex w-full gap-4">
+                <h2 className="text-center">Thanks by Our Partners</h2>
+                <p>
+                  We are able to offer security releases proudly due to the
+                  support of these partners
+                  <Link href="/about/partners/"> and more</Link>.
+                </p>
+                <PartnersLogoList categories="security" />
+              </section>
+            )}
             <WithBlogCrossLinks />
           </main>
         </div>
