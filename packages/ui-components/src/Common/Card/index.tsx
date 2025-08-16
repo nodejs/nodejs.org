4da53c76b6c379606bda8ce40b512cee3a547c69
Guilherme Araújo <arauujogui@gmail.com>
2025-08-16 10:28:13 -0300

    refactor: remove classname concatenation

--- packages/ui-components/src/Common/Card/index.tsx
+++ packages/ui-components/src/Common/Card/index.tsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames';
 import type { ComponentPropsWithRef, FC } from 'react';
 
 import styles from './index.module.css';
@@ -5,13 +6,13 @@ import styles from './index.module.css';
 type CardProps = ComponentPropsWithRef<'div'>;
 
 export const Card: FC<CardProps> = ({ className, ...props }) => {
-  return <div className={`${styles.card} ${className || ''}`} {...props} />;
+  return <div className={classNames(styles.card, className)} {...props} />;
 };
 
 export const CardHeader: FC<CardProps> = ({ className, ...props }) => {
-  return <div className={`${styles.header} ${className || ''}`} {...props} />;
+  return <div className={classNames(styles.header, className)} {...props} />;
 };
 
 export const CardBody: FC<CardProps> = ({ className, ...props }) => {
-  return <div className={`${styles.body} ${className || ''}`} {...props} />;
+  return <div className={classNames(styles.body, className)} {...props} />;
 };
