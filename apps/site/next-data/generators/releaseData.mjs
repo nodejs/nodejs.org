6c023cbef9430ddc6ba920afdded23dbb8c7d57c
Caner Akdas <canerakdas@gmail.com>
2025-08-22 21:16:13 +0300

    chore: cached nodevu data

--- apps/site/next-data/generators/releaseData.mjs
+++ apps/site/next-data/generators/releaseData.mjs
@@ -1,6 +1,6 @@
 'use strict';
 
-import nodevuPromise from './nodevuPromise.mjs';
+import getMajorNodeReleases from './majorNodeReleases.mjs';
 
 // Gets the appropriate release status for each major release
 const getNodeReleaseStatus = (now, support) => {
@@ -32,27 +32,7 @@ const getNodeReleaseStatus = (now, support) => {
  * @returns {Promise<Array<import('../../types').NodeRelease>>}
  */
 const generateReleaseData = async () => {
-  const nodevuData = await nodevuPromise;
-
-  const majors = Object.entries(nodevuData).filter(([version, { support }]) => {
-    // Filter out those without documented support
-    // Basically those not in schedule.json
-    if (!support) {
-      return false;
-    }
-
-    // nodevu returns duplicated v0.x versions (v0.12, v0.10, ...).
-    // This behavior seems intentional as the case is hardcoded in nodevu,
-    // see https://github.com/cutenode/nodevu/blob/0c8538c70195fb7181e0a4d1eeb6a28e8ed95698/core/index.js#L24.
-    // This line ignores those duplicated versions and takes the latest
-    // v0.x version (v0.12.18). It is also consistent with the legacy
-    // nodejs.org implementation.
-    if (version.startsWith('v0.') && version !== 'v0.12') {
-      return false;
-    }
-
-    return true;
-  });
+  const majors = await getMajorNodeReleases();
 
   return majors.map(([, major]) => {
     const [latestVersion] = Object.values(major.releases);
