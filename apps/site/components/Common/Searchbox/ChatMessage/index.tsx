1fdda0ead52a6e5ef10031abab63ce03dbdf19d9
Francesca Giannino <francesca.giannino86@gmail.com>
2025-09-16 17:19:50 +0200

    fix: markdown style

--- apps/site/components/Common/Searchbox/ChatMessage/index.tsx
+++ apps/site/components/Common/Searchbox/ChatMessage/index.tsx
@@ -44,7 +44,7 @@ export const ChatMessage: FC<ChatMessageProps> = ({ interaction }) => {
           <ChatInteractions.AssistantMessage
             className={styles.chatAssistantMessage}
             markdownClassnames={{
-              p: 'mb-4 leading-relaxed',
+              p: 'my-2 leading-relaxed',
               pre: `my-6 text-md overflow-x-auto hljs [&_pre]:bg-neutral-900! [&_pre]:text-sm [&_pre]:rounded-md [&_pre]:p-4 [&_pre]:whitespace-break-spaces wrap-break-word`,
               code: 'px-2 py-1 rounded text-sm whitespace-pre-wrap hljs bg-neutral-300 dark:bg-neutral-900',
               table: 'w-full border-collapse my-6',
@@ -54,10 +54,10 @@ export const ChatMessage: FC<ChatMessageProps> = ({ interaction }) => {
               tr: 'border-b border-white/10',
               h1: 'text-2xl font-bold mb-4 mt-6',
               h2: 'text-xl font-bold mb-3 mt-5',
-              h3: 'text-lg font-extrabold mb-6 mt-3',
+              h3: 'text-lg font-extrabold mb-3 mt-3',
               ul: 'list-disc pl-10 my-4',
               ol: 'list-decimal pl-4 my-4',
-              li: 'mb-1',
+              li: 'mb-0 [&_p]:mb-0',
               hr: 'border-t border-neutral-300 dark:border-neutral-900 my-6',
               blockquote: 'border-l-4 pl-4 my-4 italic',
               a: 'underline',
