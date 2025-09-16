962df65582da55e2d2fae10e8298e5bbbb15cb7c
Francesca Giannino <francesca.giannino86@gmail.com>
2025-09-16 18:39:51 +0200

    chore: update env var name

--- apps/site/next.constants.mjs
+++ apps/site/next.constants.mjs
@@ -159,20 +159,20 @@ export const ORAMA_CLOUD_ENDPOINT =
  * This is a public API key and can be shared publicly on the frontend.
  */
 export const ORAMA_CLOUD_READ_API_KEY =
-  process.env.NEXT_PUBLIC_ORAMA_PUBLIC_API_KEY ||
+  process.env.NEXT_PUBLIC_NEW_ORAMA_API_KEY ||
   'qopIuAERiWP2EZOpDjvczjws7WV40yrj';
 
 /**
  * The default Orama Cloud Datasource ID to use when searching with Orama Cloud.
  */
 export const ORAMA_CLOUD_DATASOURCE_ID =
-  process.env.NEXT_PUBLIC_ORAMA_DATASOURCE_ID || '';
+  process.env.NEXT_PUBLIC_NEW_ORAMA_DATASOURCE_ID || '';
 
 /**
  * The default Orama Cloud Project ID to use when initializing Orama Cloud.
  */
 export const ORAMA_CLOUD_PROJECT_ID =
-  process.env.NEXT_PUBLIC_ORAMA_PUBLIC_PROJECT_ID || '';
+  process.env.NEXT_PUBLIC_NEW_ORAMA_PROJECT_ID || '';
 
 /**
  * A GitHub Access Token for accessing the GitHub API and not being rate-limited
