72faa73d046ff32e2bfeb9de69c46e6646aa5085
Francesca Giannino <francesca.giannino86@gmail.com>
2025-09-16 17:14:00 +0200

    style: mobile fixes for chat panel

--- apps/site/components/Common/Searchbox/ChatInput/index.tsx
+++ apps/site/components/Common/Searchbox/ChatInput/index.tsx
@@ -2,37 +2,63 @@
 
 import { PaperAirplaneIcon } from '@heroicons/react/20/solid';
 import { PauseCircleIcon } from '@heroicons/react/24/solid';
-import { PromptTextArea } from '@orama/ui/components';
+import { PromptTextArea, Suggestions } from '@orama/ui/components';
+import { useChat } from '@orama/ui/hooks';
 import { useTranslations } from 'next-intl';
 import type { FC } from 'react';
 
 import styles from './index.module.css';
 
+const suggestions = [
+  'How to install Node.js?',
+  'How to create an HTTP server?',
+  'Upgrading Node.js versions',
+];
+
 export const ChatInput: FC = () => {
   const t = useTranslations();
+  const {
+    context: { interactions },
+  } = useChat();
+
+  const hasInteractions = interactions && interactions.length > 0;
 
   return (
-    <div className={styles.textareaContainer}>
-      <PromptTextArea.Wrapper className={styles.textareaWrapper}>
-        <PromptTextArea.Field
-          id="chat-input"
-          name="chat-input"
-          placeholder={t('components.search.chatPlaceholder')}
-          rows={1}
-          maxLength={500}
-          autoFocus
-          className={styles.textareaField}
-        />
-        <PromptTextArea.Button
-          abortContent={<PauseCircleIcon />}
-          className={styles.textareaButton}
-        >
-          <PaperAirplaneIcon />
-        </PromptTextArea.Button>
-      </PromptTextArea.Wrapper>
-      <div className={styles.textareaFooter}>
-        <small>{t('components.search.disclaimer')}</small>
+    <>
+      {!hasInteractions && (
+        <Suggestions.Wrapper className={styles.suggestionsWrapper}>
+          {suggestions.map(suggestion => (
+            <Suggestions.Item
+              className={styles.suggestionsItem}
+              key={suggestion}
+            >
+              {suggestion}
+            </Suggestions.Item>
+          ))}
+        </Suggestions.Wrapper>
+      )}
+      <div className={styles.textareaContainer}>
+        <PromptTextArea.Wrapper className={styles.textareaWrapper}>
+          <PromptTextArea.Field
+            id="chat-input"
+            name="chat-input"
+            placeholder={t('components.search.chatPlaceholder')}
+            rows={1}
+            maxLength={500}
+            autoFocus
+            className={styles.textareaField}
+          />
+          <PromptTextArea.Button
+            abortContent={<PauseCircleIcon />}
+            className={styles.textareaButton}
+          >
+            <PaperAirplaneIcon />
+          </PromptTextArea.Button>
+        </PromptTextArea.Wrapper>
+        <div className={styles.textareaFooter}>
+          <small>{t('components.search.disclaimer')}</small>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
