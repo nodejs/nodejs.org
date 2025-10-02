fca86e709a3042a18d688c7f6f709a4f2553a026
Caner Akdas <canerakdas@gmail.com>
2025-10-02 18:03:11 +0300

    chore: docs comments

--- apps/site/types/partners.ts
+++ apps/site/types/partners.ts
@@ -2,17 +2,29 @@ import type { ReactElement, SVGProps } from 'react';
 
 export type Partners = {
   id: string;
-  // The name of the partner
+  /**
+   * The name of the partner
+   */
   name: string;
-  // A logo to render on the partners page
+  /**
+   * A logo to render on the partners page
+   */
   logo: ReactElement<SVGProps<SVGSVGElement>>;
-  // The promoted link to their website or social media
+  /**
+   * The promoted link to their website or social media
+   */
   href: string;
-  // The categories this partner belongs to
+  /**
+   * The categories this partner belongs to
+   */
   categories: Array<PartnerCategory>;
-  // An optional description of the partner
+  /**
+   * An optional description of the partner
+   */
   description?: string;
-  // The weight of the partner, used for random selection
+  /**
+   * The weight of the partner, used for random selection
+   */
   weight: number;
 };
 
