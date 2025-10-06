4c2d6cba5e229eecd7ef560e55700c98a92a30ce
Caner Akdas <canerakdas@gmail.com>
2025-10-06 19:32:07 +0300

    refactor: move shuffle util into the array utils

--- apps/site/components/Common/Partners/utils.ts
+++ apps/site/components/Common/Partners/utils.ts
@@ -1,31 +1,5 @@
-import type {
-  RandomPartnerListConfig,
-  Partners,
-} from '#site/types/partners.js';
-
-// Fisher-Yates shuffle algorithm with a seed for deterministic results
-async function shuffle(
-  array: Array<Partners>,
-  seed: number
-): Promise<Array<Partners>> {
-  const shuffled = [...array];
-  const encoder = new TextEncoder();
-  const buffer = encoder.encode(String(seed));
-  const hashBuffer = await crypto.subtle.digest('SHA-256', buffer);
-  const hash = new Uint8Array(hashBuffer);
-
-  for (let i = shuffled.length - 1; i > 0; i--) {
-    // Use hash bytes to generate deterministic "random" index
-    const hashIndex = (i + seed) % 32;
-    // Normalize to 0-1
-    const randomValue = hash[hashIndex] / 255;
-
-    const j = Math.floor(randomValue * (i + 1));
-    [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
-  }
-
-  return shuffled;
-}
+import type { RandomPartnerListConfig, Partners } from '#site/types';
+import { shuffle } from '#site/util/array';
 
 async function randomPartnerList(
   partners: Array<Partners>,
