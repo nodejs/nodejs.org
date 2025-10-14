b394459e32cdeb5e44ea5c095613767cad4f9cda
Claudio Wunder <cwunder@gnome.org>
2025-10-15 00:47:37 +0200

    chore: forgot providers

--- apps/site/app/global-error.tsx
+++ apps/site/app/global-error.tsx
@@ -1,27 +1,35 @@
 'use client';
 
+import { NextIntlClientProvider } from 'next-intl';
 import type { FC } from 'react';
 
 import Button from '#site/components/Common/Button';
 import BaseLayout from '#site/layouts/Base';
 import GlowingBackdropLayout from '#site/layouts/GlowingBackdrop';
+import { ThemeProvider } from '#site/providers/themeProvider';
 
 const GlobalErrorPage: FC<{ error: Error }> = () => (
   <html>
     <body>
-      <BaseLayout>
-        <GlowingBackdropLayout>
-          <span>500</span>
+      <NextIntlClientProvider>
+        <ThemeProvider>
+          <BaseLayout>
+            <GlowingBackdropLayout>
+              <span>500</span>
 
-          <h1 className="special -mt-4 text-center">Internal Server Error</h1>
+              <h1 className="special -mt-4 text-center">
+                Internal Server Error
+              </h1>
 
-          <p className="-mt-4 max-w-sm text-center text-lg">
-            This page has thrown a non-recoverable error.
-          </p>
+              <p className="-mt-4 max-w-sm text-center text-lg">
+                This page has thrown a non-recoverable error.
+              </p>
 
-          <Button href="/">Back to Home</Button>
-        </GlowingBackdropLayout>
-      </BaseLayout>
+              <Button href="/">Back to Home</Button>
+            </GlowingBackdropLayout>
+          </BaseLayout>
+        </ThemeProvider>
+      </NextIntlClientProvider>
     </body>
   </html>
 );
