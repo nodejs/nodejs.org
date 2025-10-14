74294f27db45b11e0a30e8c7e545653bb92a24a0
Claudio Wunder <cwunder@gnome.org>
2025-10-15 00:47:37 +0200

    chore: forgot providers

--- apps/site/app/global-not-found.tsx
+++ apps/site/app/global-not-found.tsx
@@ -1,31 +1,41 @@
+'use client';
+
+import { NextIntlClientProvider } from 'next-intl';
 import type { FC } from 'react';
 
 import Button from '#site/components/Common/Button';
 import Turtle from '#site/components/Common/Turtle';
 import BaseLayout from '#site/layouts/Base';
 import GlowingBackdropLayout from '#site/layouts/GlowingBackdrop';
+import { ThemeProvider } from '#site/providers/themeProvider';
 
 const NotFoundPage: FC = () => (
   <html>
     <body>
-      <BaseLayout>
-        <GlowingBackdropLayout kind="default">
-          <span>404</span>
+      <NextIntlClientProvider>
+        <ThemeProvider>
+          <BaseLayout>
+            <GlowingBackdropLayout kind="default">
+              <span>404</span>
 
-          <h1 className="special -mt-4 text-center">Page could not be found</h1>
+              <h1 className="special -mt-4 text-center">
+                Page could not be found
+              </h1>
 
-          <div className="my-4 flex h-[150px] items-center justify-center md:h-[300px]">
-            <Turtle />
-          </div>
+              <div className="my-4 flex h-[150px] items-center justify-center md:h-[300px]">
+                <Turtle />
+              </div>
 
-          <p className="-mt-4 max-w-sm text-center text-lg">
-            Sorry, we couldn't find the page you're after! Try starting again
-            from the homepage.
-          </p>
+              <p className="-mt-4 max-w-sm text-center text-lg">
+                Sorry, we couldn't find the page you're after! Try starting
+                again from the homepage.
+              </p>
 
-          <Button href="/">Back to Home</Button>
-        </GlowingBackdropLayout>
-      </BaseLayout>
+              <Button href="/">Back to Home</Button>
+            </GlowingBackdropLayout>
+          </BaseLayout>
+        </ThemeProvider>
+      </NextIntlClientProvider>
     </body>
   </html>
 );
