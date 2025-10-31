6cb2454915dcb57fad35b91e8540f1166ca68305
Dario Piotrowicz <dario@cloudflare.com>
2025-10-31 18:09:28 +0000

    add open-next custom image loader

--- apps/site/next.config.mjs
+++ apps/site/next.config.mjs
@@ -13,19 +13,36 @@ const nextConfig = {
   // is being built on a subdirectory (e.g. /nodejs-website)
   basePath: BASE_PATH,
   // Vercel/Next.js Image Optimization Settings
-  images: {
-    // We disable image optimisation during static export builds
-    unoptimized: ENABLE_STATIC_EXPORT,
-    // We add it to the remote pattern for the static images we use from multiple sources
-    // to be marked as safe sources (these come from Markdown files)
-    remotePatterns: [
-      new URL('https://avatars.githubusercontent.com/**'),
-      new URL('https://bestpractices.coreinfrastructure.org/**'),
-      new URL('https://raw.githubusercontent.com/nodejs/**'),
-      new URL('https://user-images.githubusercontent.com/**'),
-      new URL('https://website-assets.oramasearch.com/**'),
-    ],
-  },
+  images:
+    // If we're building for the Cloudflare deployment we want to use the custom cloudflare image loader
+    //
+    // Important: The custom loader ignores `remotePatterns` as those are configured as allowed source origins
+    //            (https://developers.cloudflare.com/images/transform-images/sources/)
+    //            in the Cloudflare dashboard itself instead (to the exact same values present in `remotePatterns` below).
+    //
+    // TODO: The `OPEN_NEXT_CLOUDFLARE` environment variable is being
+    //       defined in the worker building script, ideally the open-next
+    //       adapter should set it itself when it invokes the Next.js build
+    //       process, once it does that remove the manual `OPEN_NEXT_CLOUDFLARE`
+    //       definition in the package.json script.
+    process.env.OPEN_NEXT_CLOUDFLARE
+      ? {
+          loader: 'custom',
+          loaderFile: './cloudflare-image-loader.ts',
+        }
+      : {
+          // We disable image optimisation during static export builds
+          unoptimized: ENABLE_STATIC_EXPORT,
+          // We add it to the remote pattern for the static images we use from multiple sources
+          // to be marked as safe sources (these come from Markdown files)
+          remotePatterns: [
+            new URL('https://avatars.githubusercontent.com/**'),
+            new URL('https://bestpractices.coreinfrastructure.org/**'),
+            new URL('https://raw.githubusercontent.com/nodejs/**'),
+            new URL('https://user-images.githubusercontent.com/**'),
+            new URL('https://website-assets.oramasearch.com/**'),
+          ],
+        },
   serverExternalPackages: ['twoslash'],
   outputFileTracingIncludes: {
     // Twoslash needs TypeScript declarations to function, and, by default, Next.js
@@ -86,7 +103,7 @@ const nextConfig = {
   // TODO: The `OPEN_NEXT_CLOUDFLARE` environment variable is being
   //       defined in the worker building script, ideally the open-next
   //       adapter should set it itself when it invokes the Next.js build
-  //       process, onces it does that remove the manual `OPEN_NEXT_CLOUDFLARE`
+  //       process, once it does that remove the manual `OPEN_NEXT_CLOUDFLARE`
   //       definition in the package.json script.
   deploymentId: process.env.OPEN_NEXT_CLOUDFLARE
     ? (await import('@opennextjs/cloudflare')).getDeploymentId()
