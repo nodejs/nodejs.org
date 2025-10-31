670e686a0bb95d33081cdee71e4226d468ac6c19
Dario Piotrowicz <dario@cloudflare.com>
2025-10-31 14:22:39 +0000

    add open-next DO queue

--- apps/site/open-next.config.ts
+++ apps/site/open-next.config.ts
@@ -2,11 +2,13 @@ import { defineCloudflareConfig } from '@opennextjs/cloudflare';
 import type { OpenNextConfig } from '@opennextjs/cloudflare';
 import r2IncrementalCache from '@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache';
 import { withRegionalCache } from '@opennextjs/cloudflare/overrides/incremental-cache/regional-cache';
+import doQueue from '@opennextjs/cloudflare/overrides/queue/do-queue';
 
 const cloudflareConfig = defineCloudflareConfig({
   incrementalCache: withRegionalCache(r2IncrementalCache, {
     mode: 'long-lived',
   }),
+  queue: doQueue,
 });
 
 const openNextConfig: OpenNextConfig = {
