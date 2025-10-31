89b6b2f2ab11d10e638240749c1ed57fe5560d37
Dario Piotrowicz <dario@cloudflare.com>
2025-10-31 14:06:23 +0000

    for the open-next incremental cache use R2 with regional cache instead of kv

--- apps/site/open-next.config.ts
+++ apps/site/open-next.config.ts
@@ -1,9 +1,13 @@
 import { defineCloudflareConfig } from '@opennextjs/cloudflare';
-import incrementalCache from '@opennextjs/cloudflare/overrides/incremental-cache/kv-incremental-cache';
-
 import type { OpenNextConfig } from '@opennextjs/cloudflare';
+import r2IncrementalCache from '@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache';
+import { withRegionalCache } from '@opennextjs/cloudflare/overrides/incremental-cache/regional-cache';
 
-const cloudflareConfig = defineCloudflareConfig({ incrementalCache });
+const cloudflareConfig = defineCloudflareConfig({
+  incrementalCache: withRegionalCache(r2IncrementalCache, {
+    mode: 'long-lived',
+  }),
+});
 
 const openNextConfig: OpenNextConfig = {
   ...cloudflareConfig,
