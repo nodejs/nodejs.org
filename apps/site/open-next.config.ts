dcc93c77f7dfe4a0c7d41a95006fdf797202adad
Dario Piotrowicz <dario@cloudflare.com>
2025-11-04 12:42:45 +0000

    add inline comment for regional cache

--- apps/site/open-next.config.ts
+++ apps/site/open-next.config.ts
@@ -5,6 +5,11 @@ import { withRegionalCache } from '@opennextjs/cloudflare/overrides/incremental-
 import doQueue from '@opennextjs/cloudflare/overrides/queue/do-queue';
 
 const cloudflareConfig = defineCloudflareConfig({
+  /**
+   * The regional cache implementation with R2 (instead of a KV one) is is chosen here
+   * for both R2's strong consistency alongside the regional cache performance gains.
+   * @see https://opennext.js.org/cloudflare/caching
+   */
   incrementalCache: withRegionalCache(r2IncrementalCache, {
     mode: 'long-lived',
   }),
