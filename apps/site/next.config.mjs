12ede14b672a6d2c10ff55b15bec866e0cad573e
Dario Piotrowicz <dario@cloudflare.com>
2025-11-04 12:54:38 +0000

    avoid ternaries in `next.config.mjs`

--- apps/site/next.config.mjs
+++ apps/site/next.config.mjs
@@ -17,31 +17,7 @@ const nextConfig = {
   // is being built on a subdirectory (e.g. /nodejs-website)
   basePath: BASE_PATH,
   // Vercel/Next.js Image Optimization Settings
-  images:
-    // If we're building for the Cloudflare deployment we want to use the custom cloudflare image loader
-    //
-    // Important: The custom loader ignores `remotePatterns` as those are configured as allowed source origins
-    //            (https://developers.cloudflare.com/images/transform-images/sources/)
-    //            in the Cloudflare dashboard itself instead (to the exact same values present in `remotePatterns` below).
-    //
-    OPEN_NEXT_CLOUDFLARE
-      ? {
-          loader: 'custom',
-          loaderFile: './cloudflare-image-loader.ts',
-        }
-      : {
-          // We disable image optimisation during static export builds
-          unoptimized: ENABLE_STATIC_EXPORT,
-          // We add it to the remote pattern for the static images we use from multiple sources
-          // to be marked as safe sources (these come from Markdown files)
-          remotePatterns: [
-            new URL('https://avatars.githubusercontent.com/**'),
-            new URL('https://bestpractices.coreinfrastructure.org/**'),
-            new URL('https://raw.githubusercontent.com/nodejs/**'),
-            new URL('https://user-images.githubusercontent.com/**'),
-            new URL('https://website-assets.oramasearch.com/**'),
-          ],
-        },
+  images: getImagesConfig(),
   serverExternalPackages: ['twoslash'],
   outputFileTracingIncludes: {
     // Twoslash needs TypeScript declarations to function, and, by default, Next.js
@@ -97,12 +73,47 @@ const nextConfig = {
       'shiki',
     ],
   },
-  // If we're building for the Cloudflare deployment we want to set
-  // an appropriate deploymentId (needed for skew protection)
-  deploymentId: OPEN_NEXT_CLOUDFLARE
-    ? (await import('@opennextjs/cloudflare')).getDeploymentId()
-    : undefined,
+  deploymentId: await getDeploymentId(),
 };
+
+function getImagesConfig() {
+  if (OPEN_NEXT_CLOUDFLARE) {
+    // If we're building for the Cloudflare deployment we want to use the custom cloudflare image loader
+    //
+    // Important: The custom loader ignores `remotePatterns` as those are configured as allowed source origins
+    //            (https://developers.cloudflare.com/images/transform-images/sources/)
+    //            in the Cloudflare dashboard itself instead (to the exact same values present in `remotePatterns` below).
+    //
+    return {
+      loader: 'custom',
+      loaderFile: './cloudflare-image-loader.ts',
+    };
+  }
+
+  return {
+    // We disable image optimisation during static export builds
+    unoptimized: ENABLE_STATIC_EXPORT,
+    // We add it to the remote pattern for the static images we use from multiple sources
+    // to be marked as safe sources (these come from Markdown files)
+    remotePatterns: [
+      new URL('https://avatars.githubusercontent.com/**'),
+      new URL('https://bestpractices.coreinfrastructure.org/**'),
+      new URL('https://raw.githubusercontent.com/nodejs/**'),
+      new URL('https://user-images.githubusercontent.com/**'),
+      new URL('https://website-assets.oramasearch.com/**'),
+    ],
+  };
+}
+
+async function getDeploymentId() {
+  if (OPEN_NEXT_CLOUDFLARE) {
+    // If we're building for the Cloudflare deployment we want to set
+    // an appropriate deploymentId (needed for skew protection)
+    return await import('@opennextjs/cloudflare').getDeploymentId();
+  }
+
+  return undefined;
+}
 
 const withNextIntl = createNextIntlPlugin('./i18n.tsx');
 export default withNextIntl(nextConfig);
