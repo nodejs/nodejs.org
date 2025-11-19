0f636913640d29a919868ed15c88e92638b9be98
Efe Karasakal <dogukankrskl@gmail.com>
2025-11-19 20:41:39 +0100

    feat: new stories

--- packages/ui-components/src/Common/Switch/index.stories.tsx
+++ packages/ui-components/src/Common/Switch/index.stories.tsx
@@ -1,25 +1,13 @@
 import type { Meta as MetaObj, StoryObj } from '@storybook/react';
-import { useState } from 'react';
 
 import Switch from '#ui/Common/Switch';
 
 type Story = StoryObj<typeof Switch>;
 type Meta = MetaObj<typeof Switch>;
 
-export const Uncontrolled: Story = {
+export const WithLabel: Story = {
   args: {
     label: 'Enable Feature',
-  },
-};
-
-export const Controlled: Story = {
-  args: {
-    label: 'Enable Feature',
-  },
-  render: args => {
-    const [checked, setChecked] = useState(false);
-
-    return <Switch {...args} checked={checked} onCheckedChange={setChecked} />;
   },
 };
 
