d12f6e4a530f2803d55eb4a28a24a27c0b2159b9
Efe <dogukankrskl@gmail.com>
2025-11-23 15:07:17 +0100

    feat: add toggle to show/hide LTS releases (#8263)
    * feat: add switch in eol table
    
    * feat: translation keys
    
    * refactor: client renaming
    
    * fix: dont create arrow fn onCheckedChange
    
    * chore: use tilde for version
    
    * refactor: rename prop
    
    * fixes test, but does it?
    
    * fix: add use client directive
    
    * fix: apply motion safe
    
    Co-authored-by: Caner Akdas <canerakdas@gmail.com>
    Signed-off-by: Efe <dogukankrskl@gmail.com>
    
    * refactor: use size instead of w&h
    
    Co-authored-by: Caner Akdas <canerakdas@gmail.com>
    Signed-off-by: Efe <dogukankrskl@gmail.com>
    
    * fix: use motion safe
    
    Co-authored-by: Caner Akdas <canerakdas@gmail.com>
    Signed-off-by: Efe <dogukankrskl@gmail.com>
    
    * refactor: use import type for types
    
    Co-authored-by: Caner Akdas <canerakdas@gmail.com>
    Signed-off-by: Efe <dogukankrskl@gmail.com>
    
    * fix: adjust focus visible ring offset
    
    * chore: update pnpm lock
    
    * revert: releaseData.mjs changes
    
    * revert: tablebody
    
    * feat: implement the switch in html/css only
    
    * revert: @radix-ui/react-switch install
    
    * revert: pnpm.lock
    
    * revert: tests
    
    * feat: new stories
    
    * ci: don't mind me triggering the ci
    
    ---------
    
    Signed-off-by: Efe <dogukankrskl@gmail.com>
    Co-authored-by: Brian Muenzenmeyer <brian.muenzenmeyer@gmail.com>
    Co-authored-by: Caner Akdas <canerakdas@gmail.com>

---
+++ packages/ui-components/src/Common/Switch/index.tsx
@@ -0,0 +1,30 @@
+import classNames from 'classnames';
+import type { FC, PropsWithChildren, InputHTMLAttributes } from 'react';
+
+import styles from './index.module.css';
+
+type SwitchProps = InputHTMLAttributes<HTMLInputElement> & {
+  id: string;
+  label?: string;
+  thumbClassName?: string;
+};
+
+const Switch: FC<PropsWithChildren<SwitchProps>> = ({
+  label,
+  className,
+  thumbClassName,
+  id,
+  ...props
+}) => {
+  return (
+    <label className={styles.switch}>
+      {label && <span className={styles.label}>{label}</span>}
+      <input id={id} type="checkbox" className={styles.input} {...props} />
+      <div className={classNames(styles.root, className)}>
+        <div className={classNames(styles.thumb, thumbClassName)} />
+      </div>
+    </label>
+  );
+};
+
+export default Switch;
