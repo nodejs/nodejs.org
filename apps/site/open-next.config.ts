8b265952c0d17a6b66f7b6f47d30c1e3b317d8c4
Dario Piotrowicz <dario@cloudflare.com>
2025-11-25 22:11:45 +0000

    fix linting issues

--- apps/site/open-next.config.ts
+++ apps/site/open-next.config.ts
@@ -1,8 +1,9 @@
 import { defineCloudflareConfig } from '@opennextjs/cloudflare';
-import type { OpenNextConfig } from '@opennextjs/cloudflare';
 import r2IncrementalCache from '@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache';
 import { withRegionalCache } from '@opennextjs/cloudflare/overrides/incremental-cache/regional-cache';
 import doQueue from '@opennextjs/cloudflare/overrides/queue/do-queue';
+
+import type { OpenNextConfig } from '@opennextjs/cloudflare';
 
 const cloudflareConfig = defineCloudflareConfig({
   /**
