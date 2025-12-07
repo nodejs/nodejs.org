23ef0464874e40d53cd222e54b81b920abce8f45
Aviv Keller <me@aviv.sh>
2025-12-07 15:49:27 -0500

    fixup!

--- apps/site/util/fetch.ts
+++ apps/site/util/fetch.ts
@@ -1,13 +1,13 @@
 import { setTimeout } from 'node:timers/promises';
 
 type RetryOptions = RequestInit & {
-  maxRetry: number;
-  delay: number;
+  maxRetry?: number;
+  delay?: number;
 };
 
 export const fetchWithRetry = async (
   url: string,
-  { maxRetry = 3, delay = 100, ...options }: RetryOptions
+  { maxRetry = 3, delay = 100, ...options }: RetryOptions = {}
 ) => {
   for (let i = 1; i <= maxRetry; i++) {
     try {
