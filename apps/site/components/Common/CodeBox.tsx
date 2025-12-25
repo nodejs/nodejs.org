68c44215cc1b98a070322e22411cbd3effd7e4b0
Chase Naples <Cnaples79@gmail.com>
2025-12-25 15:10:11 -0500

    feat: inline copy feedback for code blocks

--- apps/site/components/Common/CodeBox.tsx
+++ apps/site/components/Common/CodeBox.tsx
@@ -1,9 +1,6 @@
 'use client';
 
-import { CodeBracketIcon } from '@heroicons/react/24/outline';
 import BaseCodeBox from '@node-core/ui-components/Common/BaseCodeBox';
-import styles from '@node-core/ui-components/Common/BaseCodeBox/index.module.css';
-import { useNotification } from '@node-core/ui-components/Providers/NotificationProvider';
 import { useTranslations } from 'next-intl';
 
 import Link from '#site/components/Link';
@@ -18,30 +15,24 @@ type CodeBoxProps = {
 };
 
 const CodeBox: FC<PropsWithChildren<CodeBoxProps>> = props => {
-  const [, copyToClipboard] = useCopyToClipboard();
-  const notify = useNotification();
+  const [copied, copyToClipboard] = useCopyToClipboard();
   const t = useTranslations();
 
   const onCopy = (text: string) => {
     copyToClipboard(text);
-
-    notify({
-      duration: 800,
-      message: (
-        <div className="flex items-center gap-3">
-          <CodeBracketIcon className={styles.icon} />
-          {t('components.common.codebox.copied')}
-        </div>
-      ),
-    });
   };
 
   return (
     <BaseCodeBox
       as={Link}
       onCopy={onCopy}
+       copied={copied}
       {...props}
-      buttonText={t('components.common.codebox.copy')}
+       buttonText={
+         copied
+           ? t('components.common.codebox.copied')
+           : t('components.common.codebox.copy')
+       }
     />
   );
 };
