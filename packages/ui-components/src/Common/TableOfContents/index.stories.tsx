17546fcea1997d4dacd02ef93bc7a1b2edb3823a
Guilherme Araújo <arauujogui@gmail.com>
2026-01-23 15:47:53 -0300

    feat: create toc component (#8560)
    * feat: create toc component
    
    * refactor(ui-components): review
    
    * fix: custom anchor component
    
    * refactor: review
    
    * feat: i18n prop
    
    * choire: patch version
    
    * review(ui-components): some nits

---
+++ packages/ui-components/src/Common/TableOfContents/index.stories.tsx
@@ -0,0 +1,65 @@
+import TableOfContents from '#ui/Common/TableOfContents';
+
+import type { Meta as MetaObj, StoryObj } from '@storybook/react-webpack5';
+
+type Story = StoryObj<typeof TableOfContents>;
+type Meta = MetaObj<typeof TableOfContents>;
+
+export const Default: Story = {};
+
+export const CustomDepth: Story = {
+  args: {
+    minDepth: 1,
+    maxDepth: 6,
+  },
+};
+
+export default {
+  component: TableOfContents,
+  args: {
+    ariaLabel: 'Table of Contents',
+    summaryTitle: 'On this page',
+    headings: [
+      {
+        value: 'OpenSSL update assessment, and Node.js project plans',
+        depth: 1,
+        data: { id: 'heading-1' },
+      },
+      {
+        value: 'Summary',
+        depth: 2,
+        data: { id: 'summary' },
+      },
+      {
+        value: 'Analysis',
+        depth: 2,
+        data: { id: 'analysis' },
+      },
+      {
+        value: 'The c_rehash script allows command injection (CVE-2022-2068)',
+        depth: 3,
+        data: { id: 'the_c_rehash' },
+      },
+      {
+        value: 'Contact and future updates',
+        depth: 3,
+        data: { id: 'contact_and_future_updates' },
+      },
+      {
+        value: 'Email',
+        depth: 4,
+        data: { id: 'email' },
+      },
+      {
+        value: 'Slack',
+        depth: 4,
+        data: { id: 'slack' },
+      },
+      {
+        value: '#node-website',
+        depth: 5, // h5s do not get shown
+        data: { id: 'node-website' },
+      },
+    ],
+  },
+} as Meta;
---
+++ packages/ui-components/src/Common/TableOfContents/index.tsx
@@ -0,0 +1,55 @@
+import classNames from 'classnames';
+
+import { LinkLike } from '#ui/types';
+
+import type { Heading } from '@vcarl/remark-headings';
+import type { ComponentProps, FC } from 'react';
+
+import styles from './index.module.css';
+
+const depthClasses: Record<number, string> = {
+  3: styles.depthThree,
+  4: styles.depthFour,
+};
+
+type TableOfContentsProps = ComponentProps<'details'> & {
+  headings: Array<Heading>;
+  summaryTitle: string;
+  minDepth?: number;
+  maxDepth?: number;
+  as?: LinkLike;
+};
+
+const TableOfContents: FC<TableOfContentsProps> = ({
+  headings,
+  summaryTitle,
+  minDepth = 2,
+  className,
+  maxDepth = 4,
+  as: Component = 'a',
+  ...props
+}) => {
+  const filteredHeadings = headings.filter(
+    ({ depth }) => depth >= minDepth && depth <= maxDepth
+  );
+
+  return (
+    <details className={classNames(styles.details, className)} {...props}>
+      <summary className={styles.summary}>{summaryTitle}</summary>
+      <ul className={styles.list}>
+        {filteredHeadings.map((head, index) => (
+          <li key={head.data?.id ?? index}>
+            <Component
+              href={head.data?.id && `#${head.data.id}`}
+              className={classNames(styles.link, depthClasses[head.depth])}
+            >
+              {head.value}
+            </Component>
+          </li>
+        ))}
+      </ul>
+    </details>
+  );
+};
+
+export default TableOfContents;
