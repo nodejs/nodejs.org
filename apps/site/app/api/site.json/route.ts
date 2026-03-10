ffb792d447397a65868781a93649733a6191b04d
Guilherme Araújo <arauujogui@gmail.com>
2026-03-10 11:28:27 -0300

    feat: cache response

--- apps/site/app/api/site.json/route.ts
+++ apps/site/app/api/site.json/route.ts
@@ -2,6 +2,11 @@ import { NextResponse } from 'next/server';
 
 import { siteConfig } from '#site/next.json.mjs';
 
-export const GET = () => NextResponse.json(siteConfig);
+export const GET = () =>
+  NextResponse.json(siteConfig, {
+    headers: {
+      'Cache-Control': 'public, max-age=300, stale-while-revalidate=3600',
+    },
+  });
 
 export const dynamic = 'force-static';
