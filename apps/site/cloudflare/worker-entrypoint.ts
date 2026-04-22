746a7936dcf14409f5fd211d49ea6fb8d98ee38c
Dario Piotrowicz <dario@cloudflare.com>
2026-04-22 14:03:51 +0100

    remove `sendDefaultPii` and use `setTags`

--- apps/site/cloudflare/worker-entrypoint.ts
+++ apps/site/cloudflare/worker-entrypoint.ts
@@ -3,9 +3,13 @@
 //        - the official open-next docs: https://opennext.js.org/cloudflare/howtos/custom-worker
 //        - the official sentry docs: https://docs.sentry.io/platforms/javascript/guides/cloudflare
 
-import { withSentry } from '@sentry/cloudflare';
+import { setTags, withSentry } from '@sentry/cloudflare';
 
-import type { ExecutionContext } from '@cloudflare/workers-types';
+import type {
+  ExecutionContext,
+  Iso3166Alpha2Code,
+  Request,
+} from '@cloudflare/workers-types';
 
 import { default as handler } from '../.open-next/worker.js';
 
@@ -18,9 +22,6 @@ export default withSentry(
     SENTRY_DSN?: string;
   }) => ({
     dsn: env.SENTRY_DSN,
-    // Adds request headers and IP for users, for more info visit:
-    // https://docs.sentry.io/platforms/javascript/guides/cloudflare/configuration/options/#sendDefaultPii
-    sendDefaultPii: true,
     // Enable logs to be sent to Sentry
     enableLogs: true,
     // Set tracesSampleRate to 0.05 to capture 5% of spans for tracing.
@@ -34,6 +35,16 @@ export default withSentry(
       env: Record<string, unknown>,
       ctx: ExecutionContext
     ) {
+      setTags({
+        request_id: crypto.randomUUID(),
+        user_agent: request.headers.get('user-agent'),
+        ray_id: request.headers.get('cf-ray'),
+
+        // Type casts needed to keep lsp happy
+        ip_country: request.cf?.country as Iso3166Alpha2Code | undefined,
+        colo: request.cf?.colo as string | undefined,
+      });
+
       return handler.fetch(request, env, ctx);
     },
   }
