55258855f8e9c56efdb4205eee3abee0aee7e4b0
Dario Piotrowicz <dario@cloudflare.com>
2026-05-16 14:48:02 +0100

    chore(open-next): avoid the use of DurableObjects by using `queueCache` with `memoryQueue` instead (#8878)

--- apps/site/open-next.config.ts
+++ apps/site/open-next.config.ts
@@ -1,7 +1,8 @@
 import { defineCloudflareConfig } from '@opennextjs/cloudflare';
 import r2IncrementalCache from '@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache';
 import { withRegionalCache } from '@opennextjs/cloudflare/overrides/incremental-cache/regional-cache';
-import doQueue from '@opennextjs/cloudflare/overrides/queue/do-queue';
+import memoryQueue from '@opennextjs/cloudflare/overrides/queue/memory-queue';
+import queueCache from '@opennextjs/cloudflare/overrides/queue/queue-cache';
 
 import type { OpenNextConfig } from '@opennextjs/cloudflare';
 
@@ -14,7 +15,7 @@ const cloudflareConfig = defineCloudflareConfig({
   incrementalCache: withRegionalCache(r2IncrementalCache, {
     mode: 'long-lived',
   }),
-  queue: doQueue,
+  queue: queueCache(memoryQueue),
   enableCacheInterception: true,
 });
 
