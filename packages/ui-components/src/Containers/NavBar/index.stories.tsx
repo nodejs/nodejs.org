2335563cb883bd79beca00c0f36b12c3bd8c12fe
Augustin Mauroy <97875033+AugustinMauroy@users.noreply.github.com>
2026-07-18 02:11:21 +0200

    fix(navbar): no item position (#9007)
    * fix(navbar): no item position
    
    * Update index.module.css

--- packages/ui-components/src/Containers/NavBar/index.stories.tsx
+++ packages/ui-components/src/Containers/NavBar/index.stories.tsx
@@ -1,3 +1,5 @@
+import Logo from '#ui/Common/NodejsLogo';
+import SearchModal from '#ui/Common/Search/Modal';
 import NavBar from '#ui/Containers/NavBar';
 
 import type { Meta as MetaObj, StoryObj } from '@storybook/react-webpack5';
@@ -5,17 +7,12 @@ import type { Meta as MetaObj, StoryObj } from '@storybook/react-webpack5';
 type Story = StoryObj<typeof NavBar>;
 type Meta = MetaObj<typeof NavBar>;
 
-const common = {
-  as: 'a',
-  Logo: 'a',
-  pathname: '/',
-
-  children: <a>Some other child</a>,
-} as const;
-
 export const Default: Story = {
   args: {
-    ...common,
+    as: 'a',
+    Logo,
+    pathname: '/',
+    children: <SearchModal client={null} placeholder="Search the docs..." />,
     navItems: [
       {
         text: 'Learn',
@@ -45,6 +42,55 @@ export const Default: Story = {
   },
 };
 
-export const NoNavItems: Story = { args: common };
+export const NoNavItems: Story = {
+  args: {
+    as: 'a',
+    Logo,
+    pathname: '/',
+    children: <SearchModal client={null} placeholder="Search the docs..." />,
+  },
+};
+
+export const NoLogo: Story = {
+  args: {
+    as: 'a',
+    pathname: '/',
+    children: <SearchModal client={null} placeholder="Search the docs..." />,
+    navItems: [
+      {
+        text: 'Learn',
+        link: '/',
+      },
+      {
+        text: 'About',
+        link: '/about',
+      },
+      {
+        text: 'Docs',
+        link: '/docs',
+      },
+      {
+        text: 'Download',
+        link: '/download',
+      },
+      {
+        text: 'Blog',
+        link: '/blog',
+      },
+      {
+        text: 'Certification',
+        link: 'https://openjsf.org/certification',
+      },
+    ],
+  },
+};
+
+export const NoLogoNoNavItems: Story = {
+  args: {
+    as: 'a',
+    pathname: '/',
+    children: <SearchModal client={null} placeholder="Search the docs..." />,
+  },
+};
 
 export default { component: NavBar } as Meta;
